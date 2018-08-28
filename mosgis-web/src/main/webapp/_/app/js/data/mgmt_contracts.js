7237a5b733e0f78819e0e68e932a36e522d9bed4
Dmitry Ovsyanko <do_@pochta.ru>
2018-08-28 09:52:43 +0300

    mgmt_contract ui FIX tasks/587169e7-9ef8-4b1c-a93b-629d8d3e22dd

--- mosgis-web/src/main/webapp/_/app/js/data/mgmt_contracts.js
+++ mosgis-web/src/main/webapp/_/app/js/data/mgmt_contracts.js
@@ -51,7 +51,7 @@ define ([], function () {
 
     return function (done) {
     
-        var layout = w2ui ['vocs_layout']
+        var layout = w2ui ['rosters_layout']
 
         if (layout) layout.unlock ('main')
 
