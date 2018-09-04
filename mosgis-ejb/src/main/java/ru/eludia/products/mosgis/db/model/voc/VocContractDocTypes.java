099a6ab854d36c3970729e5f17ff560f23b646c3
Dmitry Ovsyanko <do_@pochta.ru>
2018-09-04 10:14:58 +0300

    ctr doc types db tasks/60ae5fb3-f249-44b0-9963-02627debaa94

---
+++ mosgis-ejb/src/main/java/ru/eludia/products/mosgis/db/model/voc/VocContractDocTypes.java
@@ -0,0 +1,52 @@
+package ru.eludia.products.mosgis.db.model.voc;
+
+import ru.eludia.base.model.Type;
+import ru.eludia.base.model.Table;
+
+public class VocContractDocTypes extends Table {
+
+    public VocContractDocTypes () {
+        
+        super ("vc_contract_doc_types", "Типы документов-приложений к договорам");
+        
+        pk    ("id",           Type.INTEGER, 2, "Ключ");
+        col   ("label",        Type.STRING, null, "Наименование");
+        
+        data  (i.class);
+
+    }
+
+    public enum i {
+        
+        AGREEMENT_ATTACHMENT           ( 1, "Дополнительное соглашение"),
+        CHARTER                        ( 2, "Устав"),
+        COMMISSIONING_PERMIT_AGREEMENT ( 3, "Разрешение на ввод в эксплуатацию"),
+        CONTRACT                       ( 4, "Договор"),
+        CONTRACT_ATTACHMENT            ( 5, "Приложение к договору"),
+        PROTOCOL_BUILDING_OWNER        ( 6, "Решение органа управления застройщика"),
+        PROTOCOL_MEETING_BOARD         ( 7, "Протокол заседания правления"),
+        PROTOCOL_MEETING_OWNERS        ( 8, "Протокол собрания собственников"),
+        PROTOCOL_OK                    ( 9, "Протокол открытого конкурса"),
+        SIGNED_OWNERS                  (10, "Реестр собственников"),
+        
+        OTHER                          (99, "Прочие"); 
+                
+        int id;
+        String label;
+
+        public int getId () {
+            return id;
+        }
+
+        public String getLabel () {
+            return label;
+        }
+
+        private i (int id, String label) {
+            this.id = id;
+            this.label = label;
+        }
+
+    }
+    
+}
