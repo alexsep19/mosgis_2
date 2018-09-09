f8c22aeab14e546da14a15858d55ad5053980d0e
Dmitry Ovsyanko <do_@pochta.ru>
2018-09-09 14:18:05 +0300

    URLFilter: cache control http headers tuned up

--- mosgis-web/src/main/java/ru/eludia/products/mosgis/web/URLFilter.java
+++ mosgis-web/src/main/java/ru/eludia/products/mosgis/web/URLFilter.java
@@ -27,14 +27,12 @@ public class URLFilter implements Filter {
     }    
 
     @Override
-    public void doFilter (ServletRequest request, ServletResponse response,
-            FilterChain chain)
-            throws IOException, ServletException {
+    public void doFilter (ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
 
         RequestWrapper wrappedRequest = new RequestWrapper ((HttpServletRequest) request);
                         
         try {
-            ((HttpServletResponse) response).setHeader ("X-Mosgis-Version", ver);
+            wrappedRequest.setResponseHeaders ((HttpServletResponse) response);
             chain.doFilter (wrappedRequest, response);
         }
         catch (Throwable t) {
@@ -83,12 +81,56 @@ public class URLFilter implements Filter {
         filterConfig.getServletContext ().log (msg);        
     }
 
-    private static final Pattern reStatic = Pattern.compile ("/__\\w+(/.*)");
+    private static final Pattern RE_STATIC = Pattern.compile ("/__\\w+(/.*)");
 
     class RequestWrapper extends HttpServletRequestWrapper {
         
+        private boolean isRest = false;
+        private boolean isVersionedStatic = false;
+        private String result;
+
+        private static final String INDEX_HTML = "/index.html";
+        private static final long MS_IN_YEAR = 365 * 24 * 60 * 60 * 1000L;
+        
+        void setResponseHeaders (HttpServletResponse hr) {
+
+            if (isRest) 
+                hr.setHeader ("X-Mosgis-Version", ver);            
+            else 
+                hr.setDateHeader ("Expires", 
+                    isVersionedStatic ? System.currentTimeMillis () + MS_IN_YEAR : 
+                    0L
+                );
+
+        }
+
+        private boolean isRestRequest (String servletPath) {
+            return !"GET".equals (getMethod ()) || servletPath.startsWith ("/_rest");
+        }
+
+        private String rewriteStaticPath (String servletPath) {
+            Matcher m = RE_STATIC.matcher (servletPath);
+            return 
+                (isVersionedStatic = m.matches ()) ? "/_" + m.group (1) : 
+                INDEX_HTML;
+        }
+
+        private String calcResult (String servletPath) {
+            
+            switch (servletPath) {
+                case INDEX_HTML:     return servletPath;
+                case "/favicon.ico": return "/_mandatory_content/favicon.ico";
+                case "/robots.txt":  return "/_mandatory_content/robots.txt";
+                default: return 
+                    (isRest = isRestRequest (servletPath)) ? servletPath : 
+                    rewriteStaticPath (servletPath);
+            }            
+            
+        }
+        
         public RequestWrapper (HttpServletRequest request) {
             super (request);
+            result = calcResult (super.getServletPath ());
         }
 
         @Override
@@ -98,22 +140,19 @@ public class URLFilter implements Filter {
 
         @Override
         public String getServletPath () {
-            
-            String s = super.getServletPath ();
+            return result;
+        }
 
-            if (!"GET".equals (getMethod ())) return s;
-            
-            if (s.startsWith ("/_rest")) return s;
+        @Override
+        public String toString () {
+            return super.getServletPath () + " .. " + getServletPath () + ", isRest=" + isRest + ", isVersionedStatic=" + isVersionedStatic;
+        }
 
-            if ("/favicon.ico".equals (s)) return "/_mandatory_content/favicon.ico";
-            if ("/robots.txt".equals  (s)) return "/_mandatory_content/robots.txt";
-            
-            Matcher m = reStatic.matcher (s); 
-                        
-            if (m.matches ()) return "/_" + m.group (1);
-                        
-            return "/index.html"; 
-
+        @Override
+        public long getDateHeader (String name) {
+            return 
+                "If-Modified-Since".equals (name) ? 0L : // suppress 304 responses, always send content
+                super.getDateHeader (name);
         }
 
     }
