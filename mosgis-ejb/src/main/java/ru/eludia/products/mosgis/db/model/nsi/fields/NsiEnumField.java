9d1b6ca92e3ae66c432726272e9172eb488f0b8a
Dmitry Ovsyanko <do_@pochta.ru>
2018-09-21 17:21:21 +0300

    NsiCommonService: exportNsiItem IN PROGRESS

--- mosgis-ejb/src/main/java/ru/eludia/products/mosgis/db/model/nsi/fields/NsiEnumField.java
+++ mosgis-ejb/src/main/java/ru/eludia/products/mosgis/db/model/nsi/fields/NsiEnumField.java
@@ -1,5 +1,7 @@
 package ru.eludia.products.mosgis.db.model.nsi.fields;
 
+import java.util.List;
+import java.util.StringTokenizer;
 import java.util.stream.Collectors;
 import javax.json.JsonObject;
 import ru.gosuslugi.dom.schema.integration.nsi_base.NsiElementFieldType;
@@ -16,6 +18,26 @@ public class NsiEnumField extends NsiStringField {
             .getPosition ().stream ()
             .map (i -> i.getValue ())
             .collect (Collectors.joining ("|"));
+    }
+    
+    @Override
+    public NsiElementFieldType toDom (Object value) throws java.sql.SQLException {
+        
+        if (value == null) return null;
+        
+        NsiElementEnumFieldType result = new NsiElementEnumFieldType ();
+        result.setName (remark);
+        List<NsiElementEnumFieldType.Position> position = result.getPosition ();
+        StringTokenizer st = new StringTokenizer (value.toString (), "|");
+        while (st.hasMoreTokens ()) {
+            String t = st.nextToken ();
+            final NsiElementEnumFieldType.Position p = new NsiElementEnumFieldType.Position ();
+            p.setValue (t);
+            p.setGUID (t);
+            position.add (p);
+        }
+        return result;
+        
     }
 
 }
