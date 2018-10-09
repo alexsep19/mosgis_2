e11904a1883ed3171a435b34f9547be1117f096d
Dmitry Ovsyanko <do_@pochta.ru>
2018-10-09 17:01:22 +0300

    charter files roster tasks/a53f8c07-55e1-4a82-87ca-f95e8c2685a6

---
+++ mosgis-web/src/main/webapp/_/app/js/data/charter_docs.js
@@ -0,0 +1,69 @@
+define ([], function () {
+
+    $_DO.edit_charter_docs = function (e) {
+            
+        use.block ('charter_doc_popup')
+    
+    }    
+    
+    $_DO.create_charter_docs = function (e) {
+            
+        use.block ('charter_doc_new')
+    
+    }
+    
+    $_DO.download_charter_docs = function (e) {    
+    
+        var box = $('body')
+
+        var r = this.get (e.recid)
+
+        function label (cur, max) {return String (Math.round (100 * cur / max)) + '%'}
+
+        w2utils.lock (box, label (0, 1))
+
+        download ({
+
+            type:   'charter_docs', 
+            id:     e.recid,
+            action: 'download',
+
+        }, {}, {
+
+            onprogress: function (cur, max) {$('.w2ui-lock-msg').html ('<br><br>' + label (cur, max))},
+
+            onload: function () {w2utils.unlock (box)},
+
+        })
+    
+    }
+
+    $_DO.delete_charter_docs = function (e) {    
+    
+        if (!e.force) return
+    
+        $('.w2ui-message').remove ()
+
+        e.preventDefault ()
+        
+        query ({
+        
+            type:   'charter_docs', 
+            id:     w2ui [e.target].getSelection () [0],
+            action: 'delete',
+            
+        }, {}, reload_page)
+    
+    }
+
+    return function (done) {
+
+        w2ui ['topmost_layout'].unlock ('main')               
+
+        var data = clone ($('body').data ('data'))
+
+        done (data)
+
+    }
+
+})
--- mosgis-web/src/main/webapp/_/app/js/view/charter.js
+++ mosgis-web/src/main/webapp/_/app/js/view/charter.js
@@ -40,7 +40,7 @@ define ([], function () {
 
                         tabs: [
                             {id: 'charter_common',   caption: 'Общие'},
-//                            {id: 'charter_docs',     caption: 'Документы'},
+                            {id: 'charter_docs',     caption: 'Документы'},
                             {id: 'charter_objects',  caption: 'Объекты управления'},
                         ].filter (not_off),
 
