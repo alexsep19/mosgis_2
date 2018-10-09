4a42fb64f5c1076683337565fbfda4dcfe2e6754
Dmitry Ovsyanko <do_@pochta.ru>
2018-10-09 09:51:24 +0300

    charter objects app tasks/8223a1c8-3a90-4bba-bade-f4c2551fb5c0

---
+++ mosgis-ejb/src/main/java/ru/eludia/products/mosgis/rest/impl/CharterObjectsImpl.java
@@ -0,0 +1,94 @@
+package ru.eludia.products.mosgis.rest.impl;
+
+import java.util.logging.Logger;
+import javax.ejb.Stateless;
+import javax.json.JsonObject;
+import static ru.eludia.base.DB.HASH;
+import ru.eludia.base.Model;
+import ru.eludia.base.db.sql.gen.Select;
+import ru.eludia.products.mosgis.db.model.nsi.NsiTable;
+import ru.eludia.products.mosgis.db.model.tables.AdditionalService;
+import ru.eludia.products.mosgis.db.model.tables.Charter;
+//import ru.eludia.products.mosgis.db.model.tables.CharterFile;
+import ru.eludia.products.mosgis.rest.api.CharterObjectsLocal;
+import ru.eludia.products.mosgis.db.model.tables.CharterObject;
+import ru.eludia.products.mosgis.db.model.tables.CharterObjectLog;
+import ru.eludia.products.mosgis.db.model.voc.VocAction;
+import ru.eludia.products.mosgis.db.model.voc.VocBuildingAddress;
+//import ru.eludia.products.mosgis.db.model.voc.VocCharterDocType;
+import ru.eludia.products.mosgis.db.model.voc.VocGisStatus;
+import ru.eludia.products.mosgis.db.model.voc.VocOrganization;
+import ru.eludia.products.mosgis.rest.User;
+import ru.eludia.products.mosgis.rest.impl.base.BaseCRUD;
+import ru.eludia.products.mosgis.web.base.ComplexSearch;
+
+@Stateless
+public class CharterObjectsImpl extends BaseCRUD<CharterObject> implements CharterObjectsLocal  {
+
+    private static final Logger logger = Logger.getLogger (CharterObjectsImpl.class.getName ());    
+
+    @Override
+    public JsonObject getItem (String id) {return fetchData ((db, job) -> {
+        
+        final Model m = db.getModel ();
+        
+        final JsonObject item = db.getJsonObject (m
+            .get   (getTable (), id,          "AS root", "*"    )
+            .toOne (Charter.class,           "AS ctr",  "*"    ).on ()
+            .toOne (VocOrganization.class,    "AS org",  "label").on ("ctr.uuid_org")
+            .toOne (VocBuildingAddress.class, "AS fias", "label").on ("root.fiashouseguid=fias.houseguid")
+        );
+
+        job.add ("item", item);
+                
+        db.addJsonArrays (job,                
+                
+            NsiTable.getNsiTable (3).getVocSelect (),                
+                
+            VocAction.getVocSelect (),
+            
+//            m.select (VocCharterDocType.class, "id", "label").orderBy ("label"),
+            
+            m.select (VocGisStatus.class,       "id", "label").orderBy ("id"),
+            
+            m.select (AdditionalService.class, "uuid AS id", "label").orderBy ("label")
+                .and ("is_deleted", 0)
+                .and ("uuid_org", item.getString ("ctr.uuid_org"))
+
+        );
+
+    });}
+
+    @Override
+    public JsonObject select (JsonObject p, User u) {return fetchData ((db, job) -> {
+        
+        ComplexSearch s = new ComplexSearch (p.getJsonArray ("search"));
+        
+        if (!s.getFilters ().containsKey ("uuid_charter")) throw new IllegalStateException ("uuid_charter filter is not set");
+                
+        Select select = db.getModel ()
+            .select     (getTable (),              "AS root", "*", "uuid AS id")
+            .toOne      (VocBuildingAddress.class, "AS fias",      "label"                                       ).on ("root.fiashouseguid=fias.houseguid")
+//            .toMaybeOne (CharterFile.class,       "AS agreement", "agreementnumber AS no", "agreementdate AS dt").on ()
+            .toMaybeOne (CharterObjectLog.class,  "AS log",       "ts"                                          ).on ()
+            .where      ("is_deleted", 0)
+            .orderBy    ("fias.label")
+            .limit      (p.getInt ("offset"), p.getInt ("limit"));
+        
+        db.addJsonArrays (job, s.filter (select, ""));
+
+    });}
+    
+    @Override
+    public JsonObject doAnnul (String id, JsonObject p, User user) {return doAction ((db) -> {
+                
+        db.update (getTable (), HASH (
+            "uuid",           id,
+            "annulmentinfo",  p.getJsonObject ("data").getString ("annulmentinfo")
+        ));
+        
+        logAction (db, user, id, VocAction.i.ANNUL);
+                        
+    });}
+
+}
---
+++ mosgis-web/src/main/java/ru/eludia/products/mosgis/rest/resources/CharterObjects.java
@@ -0,0 +1,105 @@
+package ru.eludia.products.mosgis.rest.resources;
+
+import javax.annotation.security.RolesAllowed;
+import ru.eludia.products.mosgis.rest.misc.EJBResource;
+import javax.json.JsonObject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.InternalServerErrorException;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+import ru.eludia.products.mosgis.rest.ValidationException;
+import ru.eludia.products.mosgis.rest.api.CharterObjectsLocal;
+
+@Path ("charter_objects")
+public class CharterObjects extends EJBResource <CharterObjectsLocal> {
+    
+    private String getUserOrg () {
+
+        String userOrg = getUser ().getUuidOrg ();
+
+        if (userOrg == null) {
+            logger.warning ("User has no org set, access prohibited");
+            throw new ValidationException ("foo", "Доступ запрещён");
+        }
+
+        return userOrg;
+        
+    }    
+    
+    private void checkOrg (JsonObject item) {
+
+        String itemOrg = item.getString ("ctr.uuid_org", null);
+
+        if (itemOrg == null) throw new InternalServerErrorException ("Wrong MgmtCharter, no org: " + item);
+
+        String userOrg = getUserOrg ();
+
+        if (!userOrg.equals (itemOrg)) {
+            logger.warning ("Org mismatch: " + userOrg + " vs. " + itemOrg);
+            throw new ValidationException ("foo", "Доступ запрещён");
+        }
+
+    }
+
+    @POST
+    @Consumes (APPLICATION_JSON)
+    @Produces (APPLICATION_JSON)
+    public JsonObject select (JsonObject p) {
+        return back.select (p, getUser ());
+    }
+
+    @POST
+    @Path("create") 
+    @RolesAllowed ("nsi_20_1")
+    @Consumes (APPLICATION_JSON)
+    @Produces (APPLICATION_JSON)
+    public JsonObject doCreate (JsonObject p) {
+        return back.doCreate (p, getUser ());
+    }
+    
+    @POST
+    @Path("{id}/update") 
+    @Consumes (APPLICATION_JSON)
+    @Produces (APPLICATION_JSON)
+    public JsonObject doUpdate (@PathParam ("id") String id, JsonObject p) {
+        return back.doUpdate (id, p, getUser ());
+    }
+    
+    @POST
+    @Path("{id}/annul") 
+    @Consumes (APPLICATION_JSON)
+    @Produces (APPLICATION_JSON)
+    public JsonObject doAnnul (@PathParam ("id") String id, JsonObject p) {
+        return back.doAnnul (id, p, getUser ());
+    }
+
+    @POST
+    @Path("{id}/delete") 
+    @Produces (APPLICATION_JSON)
+    public JsonObject doDelete (@PathParam ("id") String id) {
+        return back.doDelete (id, getUser ());
+    }
+    
+    @POST
+    @Path("{id}") 
+    @Produces (APPLICATION_JSON)
+    public JsonObject getItem (@PathParam ("id") String id) { 
+        final JsonObject item = back.getItem (id);
+        if (!securityContext.isUserInRole ("admin") && !securityContext.isUserInRole ("nsi_20_4")) checkOrg (item.getJsonObject ("item"));
+        return item;
+    }
+    
+    @POST
+    @Path("{id}/log") 
+    @Consumes (APPLICATION_JSON)
+    @Produces (APPLICATION_JSON)
+    public JsonObject getLog (@PathParam ("id") String id, JsonObject p) {
+        final JsonObject item = back.getItem (id);
+        if (!securityContext.isUserInRole ("admin") && !securityContext.isUserInRole ("nsi_20_4")) checkOrg (item.getJsonObject ("item"));
+        return back.getLog (id, p, getUser ());
+    }    
+
+}
