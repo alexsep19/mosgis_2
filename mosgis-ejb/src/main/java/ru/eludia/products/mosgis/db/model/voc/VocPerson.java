2557a0d34d6237ee51987a25fdee1ccb5a1669b6
Mihail Mochajlo <mihail.mochajlo@gmail.com>
2018-10-16 16:42:39 +0300

    fields added for tasks/4a71eced-e3b0-472e-a079-9451d208b356

--- mosgis-ejb/src/main/java/ru/eludia/products/mosgis/db/model/voc/VocPerson.java
+++ mosgis-ejb/src/main/java/ru/eludia/products/mosgis/db/model/voc/VocPerson.java
@@ -14,23 +14,28 @@ public class VocPerson extends Table {
         
         pk  ("uuid",     Type.UUID,         NEW_UUID, "Ключ");
         
-        col ("is_female",      Type.BOOLEAN,      null,     "Пол");
-        col ("birthdate",      Type.DATE,         null,     "Дата рождения");
+        fk  ("uuid_org",       VocOrganization.class,       "Поставщик информации");
         
-        col ("snils",          Type.NUMERIC, 11,  null,     "СНИЛС");
+        col ("is_female",      Type.BOOLEAN,       null,     "Пол");
+        col ("birthdate",      Type.DATE,          null,     "Дата рождения");
         
-        col ("code_vc_nsi_95", Type.STRING,  20,  null,     "Код документа, удостоверяющего личность (НСИ 95)");
-        col ("series",         Type.STRING,  45,  null,     "Серия документа, удостоверяющего дичность");
-        col ("number_",        Type.STRING,  45,  null,     "Номер документа, удостоверяющего личность");
-        col ("issuedate",      Type.DATE,         null,     "Дата выдачи документа, удостоверяющего личность");
+        col ("snils",          Type.NUMERIC, 11,   null,     "СНИЛС");
         
-        col ("placebirth",     Type.STRING,  255, null,     "Место рождения");
+        col ("code_vc_nsi_95", Type.STRING,  20,   null,     "Код документа, удостоверяющего личность (НСИ 95)");
+        col ("series",         Type.STRING,  45,   null,     "Серия документа, удостоверяющего дичность");
+        col ("number_",        Type.STRING,  45,   null,     "Номер документа, удостоверяющего личность");
+        col ("issuedate",      Type.DATE,          null,     "Дата выдачи документа, удостоверяющего личность");
+        col ("document_name",  Type.STRING,  1200, null,     "Кем выдан документ, удостоверяющий личность");
+        
+        col ("placebirth",     Type.STRING,  255,  null,     "Место рождения");
         
         col ("surname",        Type.STRING,  256,           "Фамилия");
         col ("firstname",      Type.STRING,  256,           "Имя");
-        col ("patronymic",     Type.STRING,  256, null,     "Отчество");
+        col ("patronymic",     Type.STRING,  256,  null,     "Отчество");
         
         col ("sex",            Type.STRING,  1, new Virt("DECODE(IS_FEMALE, 1, 'F', 0, 'M', NULL)"), "Пол (ГИС)");
+        
+        key ("uuid_org", "uuid_org");
         
     }
     
