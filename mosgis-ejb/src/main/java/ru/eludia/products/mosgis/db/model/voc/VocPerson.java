ebf65db7e17ec11b1939e9b81e16a29f7deb71a8
Mihail Mochajlo <mihail.mochajlo@gmail.com>
2018-10-16 18:01:03 +0300

    remove some fields from trigger for tasks/05c9415c-a0da-44c4-a2aa-b141455ab0e7

--- mosgis-ejb/src/main/java/ru/eludia/products/mosgis/db/model/voc/VocPerson.java
+++ mosgis-ejb/src/main/java/ru/eludia/products/mosgis/db/model/voc/VocPerson.java
@@ -2,6 +2,7 @@ package ru.eludia.products.mosgis.db.model.voc;
 
 import ru.eludia.base.model.Table;
 import ru.eludia.base.model.Type;
+import ru.eludia.base.model.def.Bool;
 import static ru.eludia.base.model.def.Def.NEW_UUID;
 import ru.eludia.base.model.def.Virt;
 import ru.eludia.products.mosgis.db.model.nsi.NsiTable;
@@ -14,28 +15,30 @@ public class VocPerson extends Table {
         
         pk  ("uuid",     Type.UUID,         NEW_UUID, "Ключ");
         
-        fk  ("uuid_org",       VocOrganization.class,        "Поставщик информации");
+        fk  ("uuid_org",       VocOrganization.class,          "Поставщик информации");
         
-        col ("is_female",      Type.BOOLEAN,       null,     "Пол");
-        col ("birthdate",      Type.DATE,          null,     "Дата рождения");
+        col ("is_deleted",     Type.BOOLEAN,       Bool.FALSE, "1, если запись удалена; иначе 0");
         
-        col ("snils",          Type.NUMERIC, 11,   null,     "СНИЛС");
+        col ("is_female",      Type.BOOLEAN,       null,       "Пол");
+        col ("birthdate",      Type.DATE,          null,       "Дата рождения");
         
-        col ("code_vc_nsi_95", Type.STRING,  20,   null,     "Код документа, удостоверяющего личность (НСИ 95)");
-        col ("series",         Type.STRING,  45,   null,     "Серия документа, удостоверяющего дичность");
-        col ("number_",        Type.STRING,  45,   null,     "Номер документа, удостоверяющего личность");
-        col ("issuedate",      Type.DATE,          null,     "Дата выдачи документа, удостоверяющего личность");
-        col ("issuer",         Type.STRING,        null,     "Кем выдан документ, удостоверяющий личность");
+        col ("snils",          Type.NUMERIC, 11,   null,       "СНИЛС");
         
-        col ("placebirth",     Type.STRING,  255,  null,     "Место рождения");
+        col ("code_vc_nsi_95", Type.STRING,  20,   null,      "Код документа, удостоверяющего личность (НСИ 95)");
+        col ("series",         Type.STRING,  45,   null,      "Серия документа, удостоверяющего дичность");
+        col ("number_",        Type.STRING,  45,   null,      "Номер документа, удостоверяющего личность");
+        col ("issuedate",      Type.DATE,          null,      "Дата выдачи документа, удостоверяющего личность");
+        col ("issuer",         Type.STRING,        null,      "Кем выдан документ, удостоверяющий личность");
         
-        col ("surname",        Type.STRING,  256,            "Фамилия");
-        col ("firstname",      Type.STRING,  256,            "Имя");
-        col ("patronymic",     Type.STRING,  256,  null,     "Отчество");
+        col ("placebirth",     Type.STRING,  255,  null,      "Место рождения");
+        
+        col ("surname",        Type.STRING,  256,             "Фамилия");
+        col ("firstname",      Type.STRING,  256,             "Имя");
+        col ("patronymic",     Type.STRING,  256,  null,      "Отчество");
         
         col ("sex",            Type.STRING,  1, new Virt("DECODE(IS_FEMALE, 1, 'F', 0, 'M', NULL)"), "Пол (ГИС)");
         
-        fk  ("id_log",         VocPersonLog.class, null,     "Последнее событие редактирования");
+        fk  ("id_log",         VocPersonLog.class, null,      "Последнее событие редактирования");
         
         key ("uuid_org", "uuid_org");
         
