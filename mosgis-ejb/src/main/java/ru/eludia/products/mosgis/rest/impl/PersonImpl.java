f58dcc3de3b2822208aad6899b04fb02be92511e
Mihail Mochajlo <mihail.mochajlo@gmail.com>
2018-10-18 10:30:57 +0300

    person interface implementation and resource added for tasks/c28a801e-0eaf-4756-8e3c-3768e43931df

---
+++ mosgis-ejb/src/main/java/ru/eludia/products/mosgis/rest/impl/PersonImpl.java
@@ -0,0 +1,140 @@
+package ru.eludia.products.mosgis.rest.impl;
+
+import javax.annotation.Resource;
+import javax.ejb.Stateless;
+import javax.jms.Queue;
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonObjectBuilder;
+import javax.ws.rs.InternalServerErrorException;
+import ru.eludia.base.DB;
+import ru.eludia.base.db.sql.gen.Select;
+import ru.eludia.products.mosgis.db.model.MosGisModel;
+import ru.eludia.products.mosgis.db.model.nsi.NsiTable;
+import ru.eludia.products.mosgis.db.model.voc.VocAction;
+import ru.eludia.products.mosgis.db.model.voc.VocAsyncEntityState;
+import ru.eludia.products.mosgis.db.model.voc.VocOkei;
+import ru.eludia.products.mosgis.db.model.voc.VocOrganization;
+import ru.eludia.products.mosgis.db.model.voc.VocPerson;
+import ru.eludia.products.mosgis.db.model.voc.VocPersonLog;
+import ru.eludia.products.mosgis.ejb.ModelHolder;
+import ru.eludia.products.mosgis.rest.User;
+import ru.eludia.products.mosgis.rest.api.PersonLocal;
+import ru.eludia.products.mosgis.rest.impl.base.BaseCRUD;
+import ru.eludia.products.mosgis.web.base.ComplexSearch;
+import ru.eludia.products.mosgis.web.base.Search;
+import ru.eludia.products.mosgis.web.base.SimpleSearch;
+
+@Stateless
+public class PersonImpl extends BaseCRUD<VocPerson> implements PersonLocal {
+    
+    @Resource (mappedName = "mosgis.inNsiPersonQueue")
+    Queue queue;
+
+    @Override
+    public Queue getQueue () {
+        return queue;
+    }
+    
+    private void filterOffDeleted (Select select) {
+        select.and ("is_deleted", 0);
+    }
+    
+    private void applyComplexSearch (final ComplexSearch search, Select select) {
+
+        search.filter (select, "");
+        
+        if (!search.getFilters ().containsKey ("is_deleted")) filterOffDeleted (select);
+
+    }
+    
+    private void applySimpleSearch (final SimpleSearch search, Select select) {
+
+        filterOffDeleted (select);
+
+        final String searchString = search.getSearchString ();
+        
+        if (searchString == null || searchString.isEmpty ()) return;
+
+        select.and ("label_uc LIKE %?%", searchString.toUpperCase ());
+        
+    }
+    
+    private void applySearch (final Search search, Select select) {        
+
+        if (search instanceof SimpleSearch) {
+            applySimpleSearch  ((SimpleSearch) search, select);
+        }
+        else if (search instanceof ComplexSearch) {
+            applyComplexSearch ((ComplexSearch) search, select);
+        }
+        else {
+            filterOffDeleted (select);
+        }
+
+    }
+    
+    @Override
+    public JsonObject select (JsonObject p, User user) {return fetchData ((db, job) -> {
+
+        Select select = ModelHolder.getModel ().select (getTable (), "AS root", "*", "uuid AS id")
+            .toOne (VocOrganization.class, "AS org", "label").on ()
+            .toMaybeOne (VocPersonLog.class         ).on ()
+            .and ("uuid_org", user.getUuidOrg ())
+            .orderBy ("org.label")
+            .orderBy ("root.sortorder")
+            .limit (p.getInt ("offset"), p.getInt ("limit"));
+
+        applySearch (Search.from (p), select);
+
+        db.addJsonArrayCnt (job, select);
+
+    });}
+    
+    @Override
+    public JsonObject getItem (String id) {return fetchData ((db, job) -> {
+
+        job.add ("item", db.getJsonObject (ModelHolder.getModel ()
+            .get (getTable (), id, "*")
+            .toOne      (VocOrganization.class,        "label").on ()
+            .toMaybeOne (VocPersonLog.class        ).on ()
+        ));
+
+    });}
+    
+    @Override
+    public JsonObject getVocs () {
+        
+        JsonObjectBuilder jb = Json.createObjectBuilder ();
+        
+        VocAction.addTo (jb);
+        
+        final MosGisModel model = ModelHolder.getModel ();
+
+        try (DB db = model.getDb ()) {
+            
+            db.addJsonArrays (jb,
+                    
+                NsiTable.getNsiTable (95).getVocSelect (),
+                    
+                model
+                    .select (VocOkei.class, "code AS id", "national AS label")
+                    .orderBy ("national"),
+                    
+                model
+                    .select (VocOrganization.class, "uuid AS id", "label")                    
+                    .orderBy ("label")
+                    .and ("uuid", model.select (VocPerson.class, "uuid_org").where ("is_deleted", 0))
+
+            );
+
+        }
+        catch (Exception ex) {
+            throw new InternalServerErrorException (ex);
+        }
+
+        return jb.build ();
+        
+    }
+    
+}
