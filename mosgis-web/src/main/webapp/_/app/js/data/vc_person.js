a29c969908ce662d8e511e2a9d744881ecdfa867
Mihail Mochajlo <mihail.mochajlo@gmail.com>
2018-10-22 14:31:38 +0300

    added common page for tasks/7aa84872-b042-4f95-8fa0-6215bc34d9ae

---
+++ mosgis-web/src/main/webapp/_/app/js/data/vc_person.js
@@ -0,0 +1,40 @@
+define ([], function () {
+
+    $_DO.choose_tab_add_service = function (e) {
+
+        var name = e.tab.id
+                
+        var layout = w2ui ['topmost_layout']
+            
+        if (layout) {                
+            layout.content ('main', '');
+            layout.lock ('main', 'Загрузка...', true);
+        }
+            
+        localStorage.setItem ('vc_person.active_tab', name)
+            
+        use.block (name)
+            
+    }            
+
+    return function (done) {        
+
+        query ({type: 'vc_persons', part: 'vocs', id: undefined}, {}, function (data) {
+
+            add_vocabularies (data, data)
+
+            query ({type: 'vc_persons'}, {}, function (d) {
+            
+                data.item = d.item
+
+                $('body').data ('data', data)
+    
+                done (data)
+
+            })
+
+        }) 
+        
+    }
+
+})
---
+++ mosgis-web/src/main/webapp/_/app/js/data/vc_person_common.js
@@ -0,0 +1,104 @@
+define ([], function () {
+
+    var form_name = 'vc_person_common_form'
+    
+    $_DO.cancel_vc_person_common = function (e) {
+        
+        if (!confirm ('Отменить несохранённые правки?')) return
+
+        var data = w2ui [form_name].record
+        
+        query ({type: 'vc_persons'}, {}, function (data) {
+
+            data.__read_only = true
+
+            $_F5 (data)
+
+        })
+
+    }
+    
+    $_DO.edit_vc_person_common = function (e) {
+
+        var data = {item: w2ui [form_name].record}
+
+        data.__read_only = false
+        
+        var $form = w2ui [form_name]
+                
+        $_F5 (data)
+
+    }
+
+    $_DO.update_vc_person_common = function (e) {
+    
+        if (!confirm ('Сохранить изменения?')) return
+        
+        var f = w2ui [form_name]
+
+        var v = f.values ()
+                
+        var re = /^[А-ЯЁ][а-яё\-]*$/
+        
+        if (!v.surname) die ('surname', 'Укажите, пожалуйста, фамилию')
+        if (!re.test (v.surname)) die ('surname', 'Фамилия содержит не корректные символы')
+        if (!v.firstname) die ('firstname', 'Укажите, пожалуйста, имя')
+        if (!re.test (v.firstname)) die ('firstname', 'Имя содержит некорректные символы')
+        if (v.patronymic && (!re.test (v.patronymic) || !/[ач]$/.test (v.patronymic))) die ('patronymic', 'Отчество указано некорректно')
+
+        query ({type: 'vc_persons', action: 'update'}, {data: v}, reload_page)
+
+    }
+    
+    $_DO.delete_vc_person_common = function (e) {   
+        if (!confirm ('Удалить эту запись, Вы уверены?')) return        
+        query ({type: 'vc_persons', action: 'delete'}, {}, reload_page)
+    }
+    
+    $_DO.undelete_vc_person_common = function (e) {   
+        if (!confirm ('Восстановить эту запись, Вы уверены?')) return        
+        query ({type: 'vc_persons', action: 'undelete'}, {}, reload_page)
+    }
+    
+    $_DO.choose_tab_vc_person_common = function (e) {
+    
+        var name = e.tab.id
+                
+        var layout = w2ui ['passport_layout']
+            
+        if (layout) {                
+            layout.content ('main', '');
+            layout.lock ('main', 'Загрузка...', true);
+        }
+            
+        localStorage.setItem ('vc_person_common.active_tab', name)
+            
+        use.block (name)        
+    
+    }
+
+    return function (done) {        
+
+        w2ui ['topmost_layout'].unlock ('main')
+        
+        var data = clone ($('body').data ('data'))
+
+        data.active_tab = localStorage.getItem ('vc_person_common.active_tab') || 'vc_person_common_log'
+
+        data.__read_only = 1
+
+        if ($_USER.role.admin) data.item.org_label = data.item ['vc_orgs.label']
+        
+        data.item._can = $_USER.role.admin /*|| data.item.id_status == 10*/ ? {} : {
+            edit: 1 - data.item.is_deleted,
+            update: 1,
+            cancel: 1,
+            delete: 1 - data.item.is_deleted,
+//            undelete: data.item.is_deleted,
+        }
+
+        done (data)
+        
+    }
+
+})
