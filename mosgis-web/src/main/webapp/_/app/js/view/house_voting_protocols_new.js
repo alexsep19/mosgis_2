a04c5c52cad5682973840acb8ebb8464eb3a4265
Mihail Mochajlo <mihail.mochajlo@gmail.com>
2018-11-07 20:43:08 +0300

    popup new protocol for tasks/0990aaf0-e613-484f-bbdb-80d6ab7945b0

--- mosgis-web/src/main/webapp/_/app/js/view/house_voting_protocols_new.js
+++ mosgis-web/src/main/webapp/_/app/js/view/house_voting_protocols_new.js
@@ -4,43 +4,101 @@ define ([], function () {
         
         var name = 'house_voting_protocols_new_form'
         
+        function disabling (element, i, arr) {
+            element.val ('').prop ('disabled', true)
+            element.closest ('.w2ui-field').hide ()
+        }
+        
+        function enabling (element, i, arr) {
+            element.closest ('.w2ui-field').show ()
+            element.val ('').prop ('disabled', false)
+        }
+        
+        function sizing (element, arr) {
+            for (item in arr){
+                element.closest ('.w2ui-' + item).height (arr[item])
+            }
+        }
+        
+        function set_size (elements, sizes) {
+            
+        }
+        
         function recalc () {
+
+            var items = {'avoting': [$('#avoitingdate'),
+                                     $('#resolutionplace')],
+                         'meeting': [$('#meetingdate'),
+                                     $('#meetingtime'),
+                                     $('#votingplace')],
+                         'evoting': [$('#evotingdatebegin'),
+                                     $('#evotingtimebegin'),
+                                     $('#evotingdateend'),
+                                     $('#evotingtimeend'),
+                                     $('#discipline'),
+                                     $('#inforeview')],
+                         'meet_av': [$('#meeting_av_date'),
+                                     $('#meeting_av_time'),
+                                     $('#meeting_av_date_end'),
+                                     $('#meeting_av_res_place')]}
+
+            var sizes = {'avoting': {form: 290,
+                                     page: 270,
+                                     box: 330,
+                                     popup: 340},
+                         'meeting': {form: 330,
+                                     page: 310,
+                                     box: 360,
+                                     popup: 380},
+                         'evoting': {form: 435,//
+                                     page: 415,//
+                                     box: 455,//
+                                     popup: 485,
+                                     'form-box': 470},
+                         'meet_av': {form: 364,
+                                     page: 345,
+                                     box: 390,
+                                     popup: 415,
+                                     'form-box': 370}}
+
+            items['avoting'].forEach(disabling);
+            items['meeting'].forEach(disabling);
+            items['evoting'].forEach(disabling);
+            items['meet_av'].forEach(disabling);
 
             var v = w2ui [name].values ()
 
-            var on = v.form_ == 0 ? 1 : 0
-
-            var $avoitingdate = $('#avoitingdate')
-            var $avoitingdate_row = $avoitingdate.closest ('.w2ui-field')
-            
-            var $resolutionplace = $('#resolutionplace')
-            var $resolutionplace_row = $resolutionplace.closest ('.w2ui-field')
-
-            if (on) {
-                $avoitingdate_row.show ()
-                $resolutionplace_row.show ()
-                $avoitingdate.prop ('disabled', false).focus ()
-                $resolutionplace.prop ('disabled', false)
-            }
-            else {
-                $avoitingdate.val ('').prop ('disabled', true)
-                $resolutionplace.val ('').prop ('disabled', true)
-                $avoitingdate_row.hide ()
-                $resolutionplace_row.hide ()
+            switch (v.form_) {
+                case 0:
+                    items['avoting'].forEach(enabling);
+                    sizing (items['avoting'][0], sizes['avoting']);
+                    break;
+                case 1:
+                    items['meeting'].forEach(enabling);
+                    sizing (items['meeting'][0], sizes['meeting']);
+                    break;
+                case 2:
+                    items['evoting'].forEach(enabling);
+                    sizing (items['evoting'][0], sizes['evoting']);
+                    break;
+                case 3:
+                    items['meet_av'].forEach(enabling);
+                    sizing (items['meet_av'][0], sizes['meet_av']);
+                    break;
             }
                         
-            var o = {
-                form: 340,
-                page: 340,
-                //box: 216,
-                popup: 400,
-                //'form-box': 193,
-            }
-            
-            for (var k in o) {
-                $avoitingdate_row.closest ('.w2ui-' + k).height (o [k] + 30 * on)
-                $resolutionplace_row.closest ('.w2ui-' + k).height (o [k] + 30 * on)
-            }            
+//            var o = {
+//                form: 340,
+//                page: 340,
+//                //box: 216,
+//                popup: 400,
+//                //'form-box': 193,
+//            }
+//            
+//            for (var k in o) {
+//                $avoitingdate_row.closest ('.w2ui-' + k).height (o [k] + 30 * on)
+//                $resolutionplace_row.closest ('.w2ui-' + k).height (o [k] + 30 * on)
+//            }            
         }
         
         $(view).w2popup('open', {
