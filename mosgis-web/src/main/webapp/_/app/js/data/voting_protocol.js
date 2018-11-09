b42b850d7ee3663308c2d3570451de34b2cbc7ed
Mihail Mochajlo <mihail.mochajlo@gmail.com>
2018-11-09 10:21:13 +0300

    voting_protocol (data view html) added for tasks/0990aaf0-e613-484f-bbdb-80d6ab7945b0

---
+++ mosgis-web/src/main/webapp/_/app/js/data/voting_protocol.js
@@ -0,0 +1,40 @@
+define ([], function () {
+
+    $_DO.choose_tab_house_voting_protocol = function (e) {
+
+        var name = e.tab.id
+                
+        var layout = w2ui ['topmost_layout']
+            
+        if (layout) {                
+            layout.content ('main', '');
+            layout.lock ('main', 'Загрузка...', true);
+        }
+            
+        localStorage.setItem ('house_voting_protocol.active_tab', name)
+            
+        use.block (name)
+            
+    }            
+
+    return function (done) {        
+
+        query ({type: 'voting_protocols', part: 'vocs', id: undefined}, {}, function (data) {
+
+            add_vocabularies (data, data)
+
+            query ({type: 'voting_protocols'}, {}, function (d) {
+            
+                data.item = d.item
+
+                $('body').data ('data', data)
+    
+                done (data)
+
+            })
+
+        }) 
+        
+    }
+
+})
