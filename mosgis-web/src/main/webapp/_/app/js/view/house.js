e9331d86e9314c0dd2404d20fe5314d8321de1fc
Mihail Mochajlo <mihail.mochajlo@gmail.com>
2018-11-12 17:02:45 +0300

    tab move and rename for tasks/f84efbb1-f9f9-4fcb-bacf-154d32bfe761

--- mosgis-web/src/main/webapp/_/app/js/view/house.js
+++ mosgis-web/src/main/webapp/_/app/js/view/house.js
@@ -35,12 +35,12 @@ define ([], function () {
                             {id: 'house_address',  caption: 'Адрес'},
                             {id: 'house_passport', caption: 'Общие'},   // sic
                             {id: 'house_common',   caption: 'Паспорт'}, // sic
-                            {id: 'house_voting_protocols', caption: 'Протоколы ОСС', off: !voting_ptocols_view_permit()},
                             {id: 'house_constr',   caption: 'Конструктивные элементы', off: !data.item.is_condo},
                             {id: 'house_systems',  caption: 'Внутридомовые сети', off: !data.item.is_condo},
                             {id: 'house_premises', caption: 'Помещения', off: !data.item.is_condo},
                             {id: 'house_living_rooms', caption: 'Комнаты', off: (data.item.is_condo && !data.has_shared_premises_res)},
                             {id: 'house_property_documents', caption: 'Собственники'},
+                            {id: 'house_voting_protocols', caption: 'Общие собрания', off: !voting_ptocols_view_permit()},
                             {id: 'house_docs',     caption: 'Документы'},
                         ].filter (not_off),
 
