df6a1f5a742199222e31bc66b7dd0c325c5df0b2
Dmitry Ovsyanko <do_@pochta.ru>
2018-11-13 09:09:21 +0300

    work list IN PROGRESS tasks/af12851d-96c2-49f4-a828-8b9a447cdc1c

--- mosgis-ejb/src/main/java/ru/eludia/products/mosgis/db/model/tables/WorkingListItem.java
+++ mosgis-ejb/src/main/java/ru/eludia/products/mosgis/db/model/tables/WorkingListItem.java
@@ -14,7 +14,7 @@ public class WorkingListItem extends EnTable {
         UUID_WORKING_LIST    (WorkingList.class,      "Ссылка на перечень"),
         UUID_ORG_WORK        (OrganizationWork.class, "Ссылка на работу/услугу организации"),
         
-//        ID_LOG               (WorkingListLog.class,  "Последнее событие редактирования"),
+        ID_LOG               (WorkingListItemLog.class,  "Последнее событие редактирования"),
         INDEX_               (NUMERIC,  4, 0, null,   "Номер строки в перечне работ и услуг"),
         
         PRICE                (NUMERIC, 14, 4, null,   "Цена"),
@@ -33,7 +33,7 @@ public class WorkingListItem extends EnTable {
         public boolean isLoggable () {
             
             switch (this) {
-//                case ID_LOG:
+                case ID_LOG:
                 case UUID_WORKING_LIST:
                     return false;
                 default: 
@@ -50,79 +50,15 @@ public class WorkingListItem extends EnTable {
         cols   (c.class);
 
         key    ("ndx", c.UUID_WORKING_LIST, c.INDEX_);
-/*        
-        trigger ("BEFORE INSERT", 
-                
-              "BEGIN"
-
-            + " IF :NEW.uuid_contract_object IS NOT NULL THEN "
-                + "SELECT fiashouseguid INTO :NEW.fiashouseguid FROM tb_contract_objects WHERE uuid=:NEW.uuid_contract_object; "
-            + " END IF;"
-                      
-            + " IF :NEW.uuid_charter_object IS NOT NULL THEN "
-                + "SELECT fiashouseguid INTO :NEW.fiashouseguid FROM tb_charter_objects WHERE uuid=:NEW.uuid_charter_object; "
-            + " END IF;"
-                      
-            + "END;"
-                
-        );
         
-        trigger ("BEFORE INSERT OR UPDATE", ""
-                
-            + "DECLARE" 
-            + " PRAGMA AUTONOMOUS_TRANSACTION; "
-            + "BEGIN "                    
-                    
-            + "IF :NEW.is_deleted = 0 THEN "
+        trigger ("BEFORE INSERT OR UPDATE", ""                
+            + "BEGIN "
+            + " IF :NEW.price IS NOT NULL THEN "
+            + "  :NEW.totalcost := :NEW.price * NVL (:NEW.amount, 1) * NVL (:NEW.count, 1); "
+            + " END IF ; "
+            + "END;"
+        );
 
-                + "IF :NEW.uuid_contract_object IS NOT NULL THEN "
-                    + " FOR i IN ("
-                        + "SELECT "
-                        + " o.dt_from"
-                        + " , o.dt_to "
-                        + "FROM "
-                        + " tb_work_lists o "
-                        + "WHERE o.is_deleted = 0"
-                        + " AND o.uuid_contract_object = :NEW.uuid_contract_object "
-                        + " AND o.dt_to   >= :NEW.dt_to "
-                        + " AND o.dt_from <= :NEW.dt_from "
-                        + " AND o.uuid <> NVL(:NEW.uuid, '00') "
-                        + ") LOOP"
-                    + " raise_application_error (-20000, "
-                        + "'Для этого объекта уже зарегистртрован перечень работ с ' "
-                        + "|| TO_CHAR (i.dt_from, 'DD.MM.YYYY')"
-                        + "||' по '"
-                        + "|| TO_CHAR (i.dt_to, 'DD.MM.YYYY')"
-                        + "|| '. Операция отменена.'); "
-                    + " END LOOP; "
-                + "END IF; "                                        
-                
-                + "IF :NEW.uuid_charter_object IS NOT NULL THEN "
-                    + " FOR i IN ("
-                        + "SELECT "
-                        + " o.dt_from"
-                        + " , o.dt_to "
-                        + "FROM "
-                        + " tb_work_lists o "
-                        + "WHERE o.is_deleted = 0"
-                        + " AND o.uuid_charter_object = :NEW.uuid_charter_object "
-                        + " AND o.dt_to   >= :NEW.dt_to "
-                        + " AND o.dt_from <= :NEW.dt_from "
-                        + " AND o.uuid <> NVL(:NEW.uuid, '00') "
-                        + ") LOOP"
-                    + " raise_application_error (-20000, "
-                        + "'Для этого объекта уже зарегистртрован перечень работ с ' "
-                        + "|| TO_CHAR (i.dt_from, 'DD.MM.YYYY')"
-                        + "||' по '"
-                        + "|| TO_CHAR (i.dt_to, 'DD.MM.YYYY')"
-                        + "|| '. Операция отменена.'); "
-                    + " END LOOP; "
-                + "END IF; "                                        
-
-            + "END IF; "                                        
-                    
-        + "END;");        
-*/
     }
 
 }
