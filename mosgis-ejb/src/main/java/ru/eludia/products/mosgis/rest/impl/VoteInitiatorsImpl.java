6b16d8a9b1e0f5ba3c8f2f4372135fa44127227b
Mihail Mochajlo <mihail.mochajlo@gmail.com>
2018-11-15 13:08:05 +0300

    impl for tasks/28dde383-1050-4e27-99ef-c4f6176b780a

---
+++ mosgis-ejb/src/main/java/ru/eludia/products/mosgis/rest/impl/VoteInitiatorsImpl.java
@@ -0,0 +1,61 @@
+package ru.eludia.products.mosgis.rest.impl;
+
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonObjectBuilder;
+import javax.ws.rs.InternalServerErrorException;
+import ru.eludia.base.DB;
+import ru.eludia.products.mosgis.db.model.tables.VoteInitiator;
+import ru.eludia.products.mosgis.db.model.tables.VotingProtocol;
+import ru.eludia.products.mosgis.db.model.voc.VocAction;
+import ru.eludia.products.mosgis.db.model.voc.VocAsyncEntityState;
+import ru.eludia.products.mosgis.db.model.voc.VocOrganization;
+import ru.eludia.products.mosgis.ejb.ModelHolder;
+import ru.eludia.products.mosgis.rest.User;
+import ru.eludia.products.mosgis.rest.api.VoteInitiatorsLocal;
+import ru.eludia.products.mosgis.rest.impl.base.BaseCRUD;
+
+public class VoteInitiatorsImpl extends BaseCRUD<VoteInitiator> implements VoteInitiatorsLocal {
+    
+    @Override
+    public JsonObject getVocs () {
+        
+        JsonObjectBuilder jb = Json.createObjectBuilder ();
+        
+        VocAction.addTo (jb);
+        
+        try (DB db = ModelHolder.getModel ().getDb ()) {
+            
+            db.addJsonArrays (jb,
+                    
+                ModelHolder.getModel ()
+                    .select (VocOrganization.class, "uuid AS id", "label")
+                    .orderBy ("label")
+                    .and ("uuid", ModelHolder.getModel ().select (VotingProtocol.class, "uuid_org").where ("is_deleted", 0)),
+
+                ModelHolder.getModel ()
+                    .select (VocAsyncEntityState.class, "id", "label")
+                    .orderBy ("label")
+
+            );
+
+        }
+        catch (Exception ex) {
+            throw new InternalServerErrorException (ex);
+        }
+
+        return jb.build ();
+        
+    }
+
+    @Override
+    public JsonObject select(JsonObject p, User user) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public JsonObject getItem(String id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
