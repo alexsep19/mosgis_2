1d96adf632bd6574ffda6449598b67a5655a389c
Mihail Mochajlo <mihail.mochajlo@gmail.com>
2018-11-19 13:30:43 +0300

    impl create for tasks/28dde383-1050-4e27-99ef-c4f6176b780a

--- mosgis-ejb/src/main/java/ru/eludia/products/mosgis/rest/impl/VoteInitiatorsImpl.java
+++ mosgis-ejb/src/main/java/ru/eludia/products/mosgis/rest/impl/VoteInitiatorsImpl.java
@@ -1,5 +1,6 @@
 package ru.eludia.products.mosgis.rest.impl;
 
+import java.util.Map;
 import javax.ejb.Stateless;
 import javax.json.Json;
 import javax.json.JsonObject;
@@ -7,6 +8,7 @@ import javax.json.JsonObjectBuilder;
 import javax.ws.rs.InternalServerErrorException;
 import ru.eludia.base.DB;
 import ru.eludia.base.db.sql.gen.Select;
+import ru.eludia.base.model.Table;
 import ru.eludia.products.mosgis.db.model.tables.VoteInitiator;
 import ru.eludia.products.mosgis.db.model.tables.VoteInitiatorLog;
 import ru.eludia.products.mosgis.db.model.tables.VotingProtocol;
@@ -66,6 +68,21 @@ public class VoteInitiatorsImpl extends BaseCRUD<VoteInitiator> implements VoteI
         //applySearch (Search.from (p), select);
 
         db.addJsonArrayCnt (job, select);
+
+    });}
+    
+    @Override
+    public JsonObject doCreate (JsonObject p, User user) {return doAction ((db, job) -> {
+
+        final Table table = getTable ();
+
+        Map<String, Object> data = getData (p);
+
+        Object insertId = db.insertId (table, data);
+        
+        job.add ("id", insertId.toString ());
+        
+        logAction (db, user, insertId, VocAction.i.CREATE);
 
     });}
 
