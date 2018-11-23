0a5d995ae49a2c40a7a9ae07feac76cb4de7f557
Mihail Mochajlo <mihail.mochajlo@gmail.com>
2018-11-23 12:45:14 +0300

    disabling added for tasks/9692b7b4-0154-4756-84d0-d84028169446

--- mosgis-web/src/main/webapp/_/app/js/view/voting_protocol_vote_decision_lists_new.js
+++ mosgis-web/src/main/webapp/_/app/js/view/voting_protocol_vote_decision_lists_new.js
@@ -6,10 +6,48 @@ define ([], function () {
 
         var grid = w2ui ['voting_protocol_vote_decision_lists_grid']
 
+        function recalc () {
+
+            var tables = {'11.1': 'management_type_table', 
+                          '2.1': 'forming_fund_table'
+            }
+
+            function disable_block (table_name) {
+                var $table = $('#' + table_name)
+                $table.find('input').each (disable_element)
+                $table.find('label').each (disable_element)
+            }
+
+            function enable_block (table_name) {
+                var $table = $('#' + table_name)
+                $table.find('input').each (enable_element)
+                $table.find('label').each (enable_element)
+            }
+
+            function disable_element (i, element) {
+                $(element).prop ('disabled', true)
+            }
+
+            function enable_element (i, element) {
+                $(element).prop ('disabled', false)
+            }
+
+            for (var table in tables) {
+                disable_block (tables[table])
+            }
+
+            var v = w2ui [name].values ()
+
+            enable_block(tables[v.decisiontype_vc_nsi_63])
+
+            //enable_block (Object.keys(tables)[v.form_])
+
+        }
+
         $(view).w2popup('open', {
 
-            width  : 700,
-            height : 400,
+            width  : 650,
+            height : 350,
 
             title   : 'Новая повестка',
 
@@ -38,6 +76,10 @@ define ([], function () {
                                 {id: 1, text: "Решение не принято"},
                             ]}},
                         ],
+
+                        onChange: function (e) {if (e.target == "decisiontype_vc_nsi_63") e.done (recalc)},
+                        
+                        onRender: function (e) {e.done (setTimeout (recalc, 100))}
 
                     });
 
