f80f7d1f13a3d841547a5a94b470698638f81163
Mihail Mochajlo <mihail.mochajlo@gmail.com>
2018-11-26 14:29:43 +0300

    log info upd for tasks/9692b7b4-0154-4756-84d0-d84028169446

--- mosgis-web/src/main/webapp/_/app/js/view/vote_decision_list_common_log.js
+++ mosgis-web/src/main/webapp/_/app/js/view/vote_decision_list_common_log.js
@@ -16,13 +16,24 @@ define ([], function () {
 
             columnGroups : [
                 {span: 3, caption: 'Событие'},
-                {span: 7, caption: 'Значения полей'},
+                {span: 1, caption: '', master: true},
+                {span: 5, caption: 'Результаты голосования'},
             ], 
             
             columns: [                
                 {field: 'ts', caption: 'Дата/время',    size: 10, render: _ts},
                 {field: 'action', caption: 'Действие',    size: 10, voc: data.vc_actions},
                 {field: 'vc_users.label', caption: 'Оператор',    size: 10},
+
+                {field: 'decisiontype_vc_nsi_63', caption: 'Тип вопроса', size: 10, voc: data.vc_nsi_63},
+
+                {field: 'agree', caption: 'За', size: 4},
+                {field: 'against', caption: 'Против', size: 4},
+                {field: 'abstent', caption: 'Воздержались', size: 4},
+                {field: 'total', caption: 'Всего', size: 4},
+                {field: 'votingresume', caption: 'Итоги голосования', size: 4, 
+                    render: function (r) {return r.votingresume == "M" ? 'Решение принято' : 'Решение не принято'}
+                },
             ],
             
             url: '/mosgis/_rest/?type=vote_decision_lists&part=log&id=' + $_REQUEST.id,            
