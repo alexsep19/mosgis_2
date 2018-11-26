3c0d8eef0e1261445235b64de5b2dff9003975bd
Dmitry Ovsyanko <do_@pochta.ru>
2018-11-26 11:05:46 +0300

    ch payments log tasks/864eebf8-f3ea-4c6c-bcde-1fd82d44ac3e

---
+++ mosgis-web/src/main/webapp/_/app/js/view/charter_payment_common_log.js
@@ -0,0 +1,70 @@
+define ([], function () {
+
+    function _ddt (record, ind, col_ind, data) {
+        return data < 99 ? data : 'посл.'
+    }
+    
+    var nxt = {
+        0: 'тек.',
+        1: 'след.',
+    }
+
+    return function (data, view) {
+    
+        data = $('body').data ('data')
+        
+        $(w2ui ['passport_layout'].el ('main')).w2regrid ({ 
+
+            name: 'charter_common_log',
+
+            show: {
+                toolbar: true,
+                toolbarInput: false,
+                footer: true,
+            },     
+
+            columnGroups : [
+                {span: 3, caption: 'Событие'},
+                {span: 2, caption: 'Значения полей'},
+                {span: 4, caption: 'Запрос в ГИС ЖКХ'},
+            ], 
+            
+            columns: [                
+                {field: 'ts', caption: 'Дата/время',    size: 30, render: _ts},
+                {field: 'action', caption: 'Действие',    size: 30, voc: data.vc_actions},
+                {field: 'vc_users.label', caption: 'Оператор',    size: 30},
+
+                {field: 'payment_1', caption: 'Руб/м2 (для членов)', size: 10},
+                {field: 'payment_0', caption: 'Руб/м2 (для не членов)', size: 10},
+//                {field: 'annulmentinfo', caption: 'Аннулирование', size: 18},
+                {field: 'soap.ts', caption: 'Отправлено',    size: 30, render: _ts, attr: 'data-ref=1'},
+                {field: 'soap.ts_rp', caption: 'Обработано',    size: 30, render: _ts, attr: 'data-ref=1'},
+                {field: 'soap.id_status', caption: 'Статус',    size: 30, render: function (r, i, c, v) {return (
+                    r.action != 'approve' ? '' : 
+                    r ['soap.ts_rp']      ? 'Обработано' : 
+                    r ['soap.ts']         ? 'Ожидает ответа' : 
+                                            'Ожидает отправки'
+                )}},
+                {field: 'soap.err_text', caption: 'Ошибка',    size: 30},
+
+            ],
+            
+            url: '/mosgis/_rest/?type=charter_payments&part=log&id=' + $_REQUEST.id,            
+            
+            onClick: function (e) {
+            
+                var c = this.columns [e.column]
+                var r = this.get (e.recid)
+                
+                switch (c.field) {
+                    case 'soap.ts':    if (r.uuid_out_soap) return openTab ('/out_soap_rq/' + r.uuid_out_soap)
+                    case 'soap.ts_rp': if (r.uuid_message) return openTab ('/out_soap_rp/' + r.uuid_message)
+                }
+            
+            }
+
+        }).refresh ();
+
+    }
+
+})
