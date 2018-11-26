6497f6665a9366393230f4a06650f31b8dce5b97
Mihail Mochajlo <mihail.mochajlo@gmail.com>
2018-11-26 17:58:40 +0300

    files tables modified

--- mosgis-ejb/src/main/java/ru/eludia/products/mosgis/db/model/tables/VotingProtocolFile.java
+++ mosgis-ejb/src/main/java/ru/eludia/products/mosgis/db/model/tables/VotingProtocolFile.java
@@ -1,45 +1,47 @@
 package ru.eludia.products.mosgis.db.model.tables;
 
-import ru.eludia.base.model.Table;
+import ru.eludia.base.model.Col;
+import ru.eludia.base.model.Ref;
 import ru.eludia.base.model.Type;
 import static ru.eludia.base.model.def.Blob.EMPTY_BLOB;
-import ru.eludia.products.mosgis.db.model.voc.VocCharterObjectReason;
+import ru.eludia.products.mosgis.db.model.AttachTable;
+import ru.eludia.products.mosgis.db.model.EnColEnum;
 
-public class VotingProtocolFile extends Table {
+public class VotingProtocolFile extends AttachTable {
+    
+    public enum c implements EnColEnum {
+
+        UUID_PROTOCOL (VotingProtocol.class,    "Ссылка на объект протокола"),
+        ID_LOG        (VotingProtocolFileLog.class,  "Последнее событие редактирования")
+        ;
+
+        @Override
+        public Col getCol () {return col;}
+        private Col col;        
+        private c (Type type, Object... p) {col = new Col (this, type, p);}
+        private c (Class c,   Object... p) {col = new Ref (this, c, p);}
+
+        @Override
+        public boolean isLoggable () {
+            return false;
+        }
+
+    }
     
     public VotingProtocolFile () {
         
-        super ("tb_voting_protocol_files", "Файлы, приложенные к протоколу ОСС");
+        super  ("tb_voting_protocol_files", "Файлы, приложенные к протоколу ОСС");
         
-        pk  ("uuid", Type.UUID, "Ключ");
+        cols   (c.class);
         
-        ref ("uuid_voting_protocol", VotingProtocol.class, "Ссылка на протокол общего собрания собственников");
-        
-        col ("description", Type.STRING, 500, "Описание вложения");
-        
-        col ("label",                 Type.STRING,                           "Имя файла");
-        col ("mime",                  Type.STRING,              null,        "Тип содержимого");
-        col ("len",                   Type.INTEGER,             null,        "Размер, байт");
-        col ("body",                  Type.BLOB,                EMPTY_BLOB,  "Содержимое");
-        col ("attachmentguid",        Type.UUID,                null,        "Идентификатор сохраненного вложения");
-        col ("attachmenthash",        Type.BINARY, 32,          null,        "ГОСТ Р 34.11-94");
-        
-        col ("id_status",             Type.INTEGER, 1,                null,  "Статус");        
-        fk  ("id_log",                VotingProtocolFileLog.class,    null,  "Последнее событие редактирования");
-        
+        key    ("parent", c.UUID_PROTOCOL);
+        key    ("attachmentguid", AttachTable.c.ATTACHMENTGUID);
+       
         trigger ("BEFORE UPDATE", 
                 "BEGIN "
-                    + "IF :NEW.len IS NULL "
-                    + "THEN :NEW.len := -1; "
-                    + "END IF; "
-                        
-                    + "IF :NEW.id_status = 0 AND DBMS_LOB.GETLENGTH (:NEW.body) = :NEW.len "
-                    + "THEN BEGIN "
-                        + ":NEW.id_status := 1; "
-                    + "END; "
-                    + "END IF; "
-                + "END;");   
-        
+                    + CHECK_LEN
+                + "END;");        
+
     }
     
 }
--- mosgis-ejb/src/main/java/ru/eludia/products/mosgis/db/model/tables/VotingProtocolFileLog.java
+++ mosgis-ejb/src/main/java/ru/eludia/products/mosgis/db/model/tables/VotingProtocolFileLog.java
@@ -1,57 +1,17 @@
 package ru.eludia.products.mosgis.db.model.tables;
 
-import ru.eludia.base.model.Table;
-import ru.eludia.base.model.Type;
-import static ru.eludia.base.model.def.Def.NEW_UUID;
-import static ru.eludia.base.model.def.Def.NOW;
-import ru.eludia.products.mosgis.db.model.voc.VocAction;
-import ru.eludia.products.mosgis.db.model.voc.VocUser;
+import ru.eludia.products.mosgis.db.model.AttachTable;
+import ru.eludia.products.mosgis.db.model.EnTable;
+import ru.eludia.products.mosgis.db.model.GisFileLogTable;
 
-public class VotingProtocolFileLog extends Table {
+public class VotingProtocolFileLog extends GisFileLogTable {
     
     public VotingProtocolFileLog () {
         
-        super ("tb_voting_protocol_files__log", "История изменения файлов, приложенных к протоколам ОСС");
-        
-        pk     ("uuid",                  Type.UUID,                NEW_UUID,    "Ключ");
-        ref    ("action",                VocAction.class,                       "Действие");
-        fk     ("uuid_object",           VotingProtocolFile.class,              "Ссылка на запись");
-        col    ("ts",                    Type.TIMESTAMP,           NOW,         "Дата/время события");
-        fk     ("uuid_user",             VocUser.class,            null,        "Оператор");        
-        col    ("ts_start_sending",      Type.DATE,                null,        "Дата начала передачи");
-        
-        fk     ("uuid_out_soap",         OutSoap.class,            null,        "Последний запрос на импорт в ГИС ЖКХ");
-        col    ("uuid_message",          Type.UUID,                null,        "UUID запроса в ГИС ЖКХ");
-        
-        col    ("id_status",             Type.INTEGER, 1,          null,        "Статус");        
-        col    ("label",                 Type.STRING,              null,        "Имя файла");
-        col    ("mime",                  Type.STRING,              null,        "Тип содержимого");
-        col    ("len",                   Type.INTEGER,             null,        "Размер, байт");
-        col    ("body",                  Type.BLOB,                null,        "Содержимое");
-        col    ("description",           Type.TEXT,                null,        "Примечание");
-        
-        col    ("ts_error",              Type.TIMESTAMP,           null,        "Дата/время ошибки передачи");
-        col    ("err_text",              Type.STRING,              null,        "Текст ошибки");
-        
-        trigger ("BEFORE INSERT", 
-                "BEGIN "
-                        + "SELECT " +
-                            "id_status, " +
-                            "label, " +
-                            "mime," +
-                            "len, " +
-                            "body, " +
-                            "description" +
-                        " INTO " +               
-                            ":NEW.id_status, " +
-                            ":NEW.label, " +
-                            ":NEW.mime," +
-                            ":NEW.len, " +
-                            ":NEW.body, " +
-                            ":NEW.description" +
-                        " FROM tb_voting_protocol_files WHERE uuid=:NEW.uuid_object; "
-
-                + "END;");
-        
+        super ("tb_voting_protocol_files__log", "История редактирования файлов, приложенных к протоколу ОСС", VotingProtocolFile.class
+            , EnTable.c.class
+            , AttachTable.c.class
+            , VotingProtocolFile.c.class
+        );
     }
 }
