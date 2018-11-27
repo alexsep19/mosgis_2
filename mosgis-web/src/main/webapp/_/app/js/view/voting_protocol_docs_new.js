db5b27e9bf1e6cb1953581d5d4035198535b7e07
Mihail Mochajlo <mihail.mochajlo@gmail.com>
2018-11-27 14:00:18 +0300

    popup new for tasks/97f603bf-73c7-4cad-99e7-bddf86218561

---
+++ mosgis-web/src/main/webapp/_/app/js/view/voting_protocol_docs_new.js
@@ -0,0 +1,43 @@
+define ([], function () {
+
+    return function (data, view) {
+    
+        var name = 'voting_protocol_docs_new_form'
+
+        var contract = data.contract || data    
+
+        $(view).w2popup('open', {
+
+            width  : 605,
+            height : 280,
+
+            title   : 'Добавление документа',
+
+            onOpen: function (event) {
+
+                event.onComplete = function () {
+
+                    if (w2ui [name]) w2ui [name].destroy ()
+
+                    $('#w2ui-popup .w2ui-form').w2form ({
+
+                        name: name,
+
+                        fields : [
+                            {name: 'description',  type: 'textarea' },
+                            {name: 'files', type: 'file'},    
+                        ],
+
+                    });
+
+                    clickOn ($('#w2ui-popup button'), $_DO.update_voting_protocol_docs_new)
+
+                }
+
+            }
+
+        });
+
+    }
+
+});
