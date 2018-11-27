889ebd0d52cc50f8d98f548ce563c0a55e4368c7
Mihail Mochajlo <mihail.mochajlo@gmail.com>
2018-11-27 13:50:16 +0300

    popup added for tasks/97f603bf-73c7-4cad-99e7-bddf86218561

---
+++ mosgis-web/src/main/webapp/_/app/js/data/voting_protocol_docs_popup.js
@@ -0,0 +1,50 @@
+define ([], function () {
+
+    $_DO.download_voting_protocol_docs_popup = function (e) {    
+
+        var box = $('#w2ui-popup')
+
+        function label (cur, max) {return String (Math.round (100 * cur / max)) + '%'}
+
+        w2utils.lock (box, label (0, 1))
+
+        download ({
+
+            type:   'voting_protocol_docs', 
+            id:     w2ui ['voting_protocol_docs_popup_form'].record.id,
+            action: 'download',
+
+        }, {}, {
+
+            onprogress: function (cur, max) {$('.w2ui-lock-msg').html ('<br><br>' + label (cur, max))},
+
+            onload: function () {w2utils.unlock (box)},
+
+        })
+    
+    }
+
+
+    $_DO.update_voting_protocol_docs_popup = function (e) {
+    
+        var form = w2ui ['voting_potocol_docs_popup_form']
+        
+        var r = form.values ()
+        
+        query ({type: 'voting_protocol_docs', action: 'edit', id: form.record.id}, {data: r}, reload_page)
+
+    }
+
+    return function (done) {
+
+        var grid = w2ui ['voting_protocol_docs_grid']
+
+        var id = grid.getSelection () [0]
+
+        var data = grid.get (id)
+darn (data)
+        done (data)
+
+    }
+
+})
