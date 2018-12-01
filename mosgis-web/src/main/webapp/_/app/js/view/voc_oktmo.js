9301e051cc52e62a9d95dbff6f979f3793bd9351
Mihail Mochajlo <mihail.mochajlo@gmail.com>
2018-12-02 02:53:08 +0300

    ui fix for tasks/cb7cbcd2-b534-48c0-b334-09542d92f49b

--- mosgis-web/src/main/webapp/_/app/js/view/voc_oktmo.js
+++ mosgis-web/src/main/webapp/_/app/js/view/voc_oktmo.js
@@ -9,20 +9,24 @@ define ([], function () {
             name: 'voc_oktmo_grid',
 
             show: {
-                toolbar: false,
+                toolbar: true,
                 footer: false,
             },
 
+            columnGroups : [
+                {span: 3, caption: 'Код ОКТМО'},
+                {span: 5, caption: '', master: true},
+            ],
+
             columns: [                
-                {field: 'area_code', caption: 'Код района/города МО', size: 10},
-                {field: 'settlement_code', caption: 'Код поселения МО', size: 10},
-                {field: 'locality_code', caption: 'Код населенного пункта МО', size: 10},
-                {field: 'section_code', caption: 'Код раздела', size: 10},
-                {field: 'site_name', caption: 'Наименование территории', size: 10},
-                {field: 'add_info', caption: 'Дополнительная информация', size: 10},
-                {field: 'description', caption: 'Описание', size: 10},
-                {field: 'appr_date', caption: 'Дата утверждения', size: 10, render: _dt},
-                {field: 'adop_date', caption: 'Дата принятия', size: 10, render: _dt},
+                {field: 'area_code', caption: '', size: 2},
+                {field: 'settlement_code', caption: '', size: 2},
+                {field: 'locality_code', caption: '', size: 2},
+                {field: 'site_name', caption: 'Наименование территории', size: 50},
+                {field: 'add_info', caption: 'Дополнительная информация', size: 10, hidden: 1},
+                {field: 'description', caption: 'Описание', size: 10, hidden: 1},
+                {field: 'appr_date', caption: 'Дата утверждения', size: 10, render: _dt, hidden: 1},
+                {field: 'adop_date', caption: 'Дата принятия', size: 10, render: _dt, hidden: 1},
             ],
             
             url: '/mosgis/_rest/?type=voc_oktmo',
