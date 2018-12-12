aa5aee1e234fd7b51f405892993ddfc8b87947e8
Dmitry Ovsyanko <do_@pochta.ru>
2018-12-12 16:53:13 +0300

    pp doc type db tasks/b4131b61-9128-4083-b542-d3cdbfd013c9

---
+++ mosgis-ejb/src/main/java/ru/eludia/products/mosgis/db/model/voc/VocPublicPropertyContractFileType.java
@@ -0,0 +1,73 @@
+package ru.eludia.products.mosgis.db.model.voc;
+
+import javax.json.Json;
+import ru.eludia.base.model.Type;
+import javax.json.JsonArray;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonObject;
+import javax.json.JsonObjectBuilder;
+import ru.eludia.base.model.Col;
+import ru.eludia.base.model.ColEnum;
+import ru.eludia.base.model.Table;
+
+public class VocPublicPropertyContractFileType extends Table {
+    
+    private static final String TABLE_NAME = "vc_pp_ctr_file_types";
+    
+    public VocPublicPropertyContractFileType () {       
+        super (TABLE_NAME, "Типы документов, приложенных к ДПОИ");
+        cols  (c.class);        
+        pk    (c.ID);
+        data  (i.class);
+    }
+    
+    public enum c implements ColEnum {        
+        ID         (Type.NUMERIC, 1, "Идентификатор"),
+        LABEL      (Type.STRING,     "Наименование");        
+                                                                                    @Override public Col getCol () {return col;} private Col col; private c (Type type, Object... p) {col = new Col (this, type, p);}
+    }
+    
+    public enum i {
+
+        CONTRACT     (1, "Договор"),
+        ADDENDUM     (2, "Дополнительное соглашение"),
+        VOTING_PROTO (3, "Протокол собрания собственников")
+        ;
+                
+        int    id;
+        String label;
+
+        public int getId () {
+            return id;
+        }
+        
+        public String getLabel () {
+            return label;
+        }
+
+        private i (int id, String label) {
+            this.id = id;
+            this.label = label;            
+        }
+        
+        private JsonObject toJsonObject () {
+            return Json.createObjectBuilder ()
+                .add ("id",    id)
+                .add ("label", label)
+            .build ();
+        }
+
+        public static JsonArray toJsonArray () {            
+            JsonArrayBuilder builder = Json.createArrayBuilder ();                    
+            for (i value: i.values ()) builder.add (value.toJsonObject ());            
+            return builder.build ();            
+        }
+
+    }
+    
+    private static JsonArray jsonArray = i.toJsonArray ();    
+    public  static final void addTo (JsonObjectBuilder job) {
+        job.add (TABLE_NAME, jsonArray);
+    }
+    
+}
