8aed950f19248e0b34215497a7eab437e160ec15
Dmitry Ovsyanko <do_@pochta.ru>
2018-12-12 09:31:16 +0300

    vp validation tasks/80ec9dc9-64df-49ba-9a03-f685f6a55399

--- mosgis-ejb/src/main/java/ru/eludia/products/mosgis/db/model/tables/VotingProtocol.java
+++ mosgis-ejb/src/main/java/ru/eludia/products/mosgis/db/model/tables/VotingProtocol.java
@@ -91,11 +91,15 @@ public class VotingProtocol extends EnTable {
         trigger ("BEFORE UPDATE", 
                 
             "DECLARE "
+            + " cnt_files NUMBER;"
             + " uuid_init RAW(16);"
             + "BEGIN "
                 + "IF :NEW.is_deleted=0 AND :NEW.ID_PRTCL_STATUS <> :OLD.ID_PRTCL_STATUS AND :NEW.ID_PRTCL_STATUS=" + VocGisStatus.i.PENDING_RQ_PLACING.getId () + " THEN BEGIN "
 
                     + "IF :NEW.AVOTINGDATE > TRUNC(SYSDATE) THEN raise_application_error (-20000, 'Приём решений ещё не завершён. Операция отменена.'); END IF; "
+
+                    + " SELECT COUNT(*) INTO cnt_files FROM tb_voting_protocol_files WHERE is_deleted = 0 AND UUID_PROTOCOL=:NEW.uuid; "
+                    + " IF cnt_files=0 THEN raise_application_error (-20000, 'Файл протокола не загружен на сервер. Операция отменена.'); END IF; "
 
                     + " SELECT MIN(uuid) INTO uuid_init FROM tb_vote_initiators WHERE is_deleted = 0 AND UUID_PROTOCOL=:NEW.uuid AND UUID_ORG=:NEW.UUID_ORG; "
                     + " IF uuid_init IS NULL THEN raise_application_error (-20000, 'Вашей организации нет в списке инициаторов. Операция отменена.'); END IF; "
