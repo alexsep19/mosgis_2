66b251631336d551344b02d342bf58649d8f6b3c
Dmitry Ovsyanko <do_@pochta.ru>
2018-12-19 16:45:13 +0300

    pp ap app IN PROGRESS tasks.cheby.ru/tasks/81e40795-5436-4f54-b42d-e77a1f71fa38

---
+++ mosgis-ejb/src/main/java/ru/eludia/products/mosgis/rest/impl/AgreementPaymentImpl.java
@@ -0,0 +1,170 @@
+package ru.eludia.products.mosgis.rest.impl;
+
+import java.util.logging.Logger;
+import javax.annotation.Resource;
+import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+import javax.jms.Queue;
+import javax.json.JsonObject;
+import ru.eludia.base.Model;
+import ru.eludia.base.db.sql.gen.Operator;
+import ru.eludia.base.db.sql.gen.Select;
+import ru.eludia.products.mosgis.db.model.EnTable;
+import ru.eludia.products.mosgis.db.model.MosGisModel;
+import ru.eludia.products.mosgis.db.model.tables.Charter;
+import ru.eludia.products.mosgis.db.model.tables.AgreementPayment;
+import ru.eludia.products.mosgis.db.model.tables.AgreementPaymentLog;
+import ru.eludia.products.mosgis.db.model.tables.OutSoap;
+import ru.eludia.products.mosgis.db.model.voc.VocAction;
+import ru.eludia.products.mosgis.db.model.voc.VocGisStatus;
+import ru.eludia.products.mosgis.db.model.voc.VocOrganization;
+import ru.eludia.products.mosgis.ejb.ModelHolder;
+import ru.eludia.products.mosgis.rest.User;
+import ru.eludia.products.mosgis.rest.api.AgreementPaymentLocal;
+import ru.eludia.products.mosgis.rest.impl.base.BaseCRUD;
+import ru.eludia.products.mosgis.web.base.ComplexSearch;
+import ru.eludia.products.mosgis.web.base.Search;
+import ru.eludia.products.mosgis.web.base.SimpleSearch;
+
+@Stateless
+@TransactionAttribute(TransactionAttributeType.NOT_SUPPORTED)
+public class AgreementPaymentImpl extends BaseCRUD<AgreementPayment> implements AgreementPaymentLocal {
+
+    @Resource (mappedName = "mosgis.inHouseAgreementPaymentsQueue")
+    Queue queue;
+
+    @Override
+    public Queue getQueue () {
+        return queue;
+    }
+
+    private static final Logger logger = Logger.getLogger (AgreementPaymentImpl.class.getName ());    
+       
+    private void filterOffDeleted (Select select) {
+        select.and (EnTable.c.IS_DELETED, Operator.EQ, 0);
+    }
+
+    private void applyComplexSearch (final ComplexSearch search, Select select) {
+/*
+        search.filter (select, "");
+        if (!search.getFilters ().containsKey ("is_deleted")) filterOffDeleted (select);
+*/        
+    }
+    
+    private void applySimpleSearch (final SimpleSearch search, Select select) {
+/*
+        final String s = search.getSearchString ();
+        
+        if (s == null || s.isEmpty ()) return;
+        
+        final String uc = s.toUpperCase ();
+
+        select.andEither ("owner_label_uc LIKE %?%", uc).or ("label", s);
+*/
+    }
+    
+    private void applySearch (final Search search, Select select) {        
+
+        if (search instanceof ComplexSearch) {
+            applyComplexSearch ((ComplexSearch) search, select);
+        }
+        else {
+            if (search instanceof SimpleSearch) applySimpleSearch  ((SimpleSearch) search, select);
+            filterOffDeleted (select);        
+        }
+        
+    }
+    
+    @Override
+    public JsonObject select (JsonObject p, User user) {return fetchData ((db, job) -> {
+        
+        final Model m = ModelHolder.getModel ();
+
+        Select select = m.select (AgreementPayment.class, "*", "uuid AS id")
+            .orderBy (AgreementPayment.c.DATEFROM.lc () + " DESC")
+            .limit (p.getInt ("offset"), p.getInt ("limit"));
+
+        applySearch (Search.from (p), select);
+        
+        select.and (AgreementPayment.c.UUID_CTR.lc (), p.getJsonObject ("data").getString ("uuid_ctr"));
+
+        db.addJsonArrayCnt (job, select);
+
+    });}
+    
+    @Override
+    public JsonObject getItem (String id) {return fetchData ((db, job) -> {
+
+        final MosGisModel m = ModelHolder.getModel ();
+        
+        final JsonObject item = db.getJsonObject (m
+            .get (getTable (), id, "AS root", "*")
+            .toOne (Charter.class, "AS ctr", "*").on ()
+            .toOne (VocOrganization.class, "AS org", "label").on ("ctr.uuid_org")
+            .toMaybeOne (AgreementPaymentLog.class, "AS cpl").on ()
+            .toMaybeOne (OutSoap.class, "err_text").on ("cpl.uuid_out_soap=out_soap.uuid")
+        );
+
+        job.add ("item", item);
+        
+        VocGisStatus.addTo (job);
+        VocAction.addTo (job);
+
+    });}            
+    
+    @Override
+    protected void publishMessage (VocAction.i action, String id_log) {
+        
+        switch (action) {
+            case APPROVE:
+            case PROMOTE:
+            case REFRESH:
+            case TERMINATE:
+            case ANNUL:
+            case ROLLOVER:
+            case RELOAD:
+                super.publishMessage (action, id_log);
+            default:
+                return;
+        }
+        
+    }
+/*    
+    @Override
+    public JsonObject doApprove (String id, User user) {return doAction ((db) -> {
+
+        db.update (getTable (), HASH (
+            "uuid",           id,
+            "id_ctr_status",  VocGisStatus.i.PENDING_RQ_PLACING.getId ()
+        ));
+        
+        logAction (db, user, id, VocAction.i.APPROVE);
+
+        List<UUID> ids = new ArrayList<> ();        
+        
+        db.forEach (db.getModel ().select (AgreementPayment.class, "uuid_file_0", "uuid_file_1").where ("uuid", id), (rs) -> {
+            Object u = db.getValue (rs, 1);
+            if (u != null) ids.add ((UUID) u);
+            u = db.getValue (rs, 2);
+            if (u != null) ids.add ((UUID) u);
+        });
+        
+        for (UUID idFile: ids) {
+            
+            String idFileLog = db.insertId (AgreementPaymentFileLog.class, HASH (
+                "action", VocAction.i.APPROVE.getName (),
+                "uuid_object", idFile,
+                "uuid_user", user == null ? null : user.getId ()
+            )).toString ();
+            
+            db.update (AgreementPaymentFile.class, HASH (
+                "uuid",      idFile,
+                "id_log",    idFileLog
+            ));
+            
+        }        
+
+    });}    
+*/        
+}
