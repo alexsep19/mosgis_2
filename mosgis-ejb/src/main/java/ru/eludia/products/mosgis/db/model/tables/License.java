b865fdf3c9aef4edf6341e52d4dfe7ee70a62e84
Konstantin Nikolaev <knikolaev@cheby.ru>
2018-12-19 16:56:18 +0300

    Update Table

--- mosgis-ejb/src/main/java/ru/eludia/products/mosgis/db/model/tables/License.java
+++ mosgis-ejb/src/main/java/ru/eludia/products/mosgis/db/model/tables/License.java
@@ -1,7 +1,6 @@
 package ru.eludia.products.mosgis.db.model.tables;
 
 import ru.eludia.base.model.Col;
-import ru.eludia.base.model.ColEnum;
 import ru.eludia.base.model.Ref;
 import ru.eludia.base.model.Table;
 import ru.eludia.base.model.Type;
@@ -9,43 +8,61 @@ import ru.eludia.products.mosgis.db.model.voc.VocLicenseStatus;
 import ru.eludia.products.mosgis.db.model.voc.VocOrganization;
 import static ru.eludia.base.model.Type.DATE;
 import static ru.eludia.base.model.Type.STRING;
-import static ru.eludia.base.model.Type.UUID;
 import static ru.eludia.base.model.Type.INTEGER;
+import ru.eludia.products.mosgis.db.model.EnColEnum;
 
 public class License extends Table {
 
-    public enum c implements ColEnum {
+    public enum c implements EnColEnum {
         
-        LICENSEGUID                     (UUID,                    null,   "UUID лицензии в системе"),
+        LICENSEGUID                     (Type.UUID,               null,   "UUID лицензии в системе"),
         LICENSE_VERSION                 (INTEGER,                 null,   "Версия лицензии" ),
         ID_LOG                          (LicenseLog.class,        null,   "Последнее событие редактирования"), 
         LICENSE_NUMBER                  (STRING,            9,            "Номер лицензии"),
         LICENSE_REG_DATE                (DATE,                            "Дата регистрации лицензии"),   
         ID_STATUS                       (VocLicenseStatus.class,  null,   "Статус лицензии с точки зрения mosgis"),
         UUID_ORG_AUTHORITY              (VocOrganization.class,           "Наименование лицензирующего органа"),
-        REGION_FIAS_GUID                (UUID,                    null,   "Адрес осуществления лицензируемого вида деятельности (код по ФИАС)"),
+        REGION_FIAS_GUID                (Type.UUID,               null,   "Адрес осуществления лицензируемого вида деятельности (код по ФИАС)"),
         LICENSEABLE_TYPE_OF_ACTIVITY    (STRING,            2000,         "Лицензируемый вид деятельности с указанием выполняемых работ, оказываемых услуг, составляющих лицензируемый вид деятельности"),
         ADDITIONAL_INFORMATION          (STRING,            2000, null,   "Дополнительная информация"),
         UUID_ORG                        (VocOrganization.class,           "Лицензиат"); 
         
         @Override
-        public Col getCol () {return col;}
+        public Col getCol() {
+            return col;
+        }
+        private Col col;
+
+        private c(Type type, Object... p) {
+            col = new Col(this, type, p);
+        }
+
+        private c(Class c, Object... p) {
+            col = new Ref(this, c, p);
+        }
         
-        private Col col;        
-        private c (Type type, Object... p) {col = new Col (this, type, p);}
-        private c (Class c,   Object... p) {col = new Ref (this, c, p);}        
+        @Override
+        public boolean isLoggable() {
+            switch (this) {
+                case LICENSEGUID:
+                case REGION_FIAS_GUID:
+                    return false;
+                default:
+                    return true;
+            }
+        }
     
     }
     
     public License () {
 
-        super ("tb_licences", "Лицензии");
-        
-        cols  (c.class);
-        
-        pk    (c.LICENSEGUID);
-        key   ("uuid_org", c.UUID_ORG);
-    
-    }    
+            super ("tb_licences", "Лицензии");
+
+            cols  (c.class);
+
+            pk    (c.LICENSEGUID);
+            key   ("uuid_org", c.UUID_ORG);
+
+    }  
     
 }
--- mosgis-ejb/src/main/java/ru/eludia/products/mosgis/db/model/tables/LicenseLog.java
+++ mosgis-ejb/src/main/java/ru/eludia/products/mosgis/db/model/tables/LicenseLog.java
@@ -1,71 +1,25 @@
 package ru.eludia.products.mosgis.db.model.tables;
 
-import ru.eludia.base.model.Table;
-import ru.eludia.base.model.Type;
-import static ru.eludia.base.model.Type.DATE;
-import static ru.eludia.base.model.Type.STRING;
-import static ru.eludia.base.model.def.Def.NEW_UUID;
-import static ru.eludia.base.model.def.Def.NOW;
-import ru.eludia.products.mosgis.db.model.voc.VocAction;
-import ru.eludia.products.mosgis.db.model.voc.VocLicenseStatus;
-import ru.eludia.products.mosgis.db.model.voc.VocOrganization;
-import ru.eludia.products.mosgis.db.model.voc.VocUser;
+import java.util.UUID;
+import ru.eludia.products.mosgis.db.model.LogTable;
 
-public class LicenseLog extends Table {
+public class LicenseLog extends LogTable {
     
-    public LicenseLog() {
-        
-        super ("tb_licenses__log",                                 "История изменения лицензий");
-        
-        pk    ("uuid",                      Type.UUID,             NEW_UUID,            "Ключ");
-        
-        fk    ("uuid_object",               License.class,                              "Ссылка на запись");
-        col   ("ts",                        Type.TIMESTAMP,        NOW,                 "Дата/время события");
-        fk    ("uuid_user",                 VocUser.class,                      null,   "Оператор");
-        fk    ("uuid_out_soap",             OutSoap.class,                      null,   "Последний запрос на импорт в ГИС ЖКХ");
-        
-        col ("license_version",             Type.INTEGER,                       null, "Версия лицензии");
-        col ("license_number",              STRING,                9,                 "Номер лицензии");
-        col ("license_reg_date",            DATE,                                     "Дата регистрации лицензии");
-        fk  ("license_status",              VocLicenseStatus.class,             null, "Статус лицензии с точки зрения mosgis");
-        fk  ("licensing_authority",         VocOrganization.class,              null, "Наименование лицензирующего органа");
-        col ("region_fias_guid",            Type.UUID,             null,              "Адрес осуществления лицензируемого вида деятельности (код по ФИАС)");
-        col ("licenseable_type_of_activity",STRING,                             2000, "Лицензируемый вид деятельности с указанием выполняемых работ, оказываемых услуг, составляющих лицензируемый вид деятельности");
-        col ("additional_information",      STRING,                             2000, "Дополнительная информация");
-        fk  ("license_organization",        VocOrganization.class,              null, "Лицензиат"); 
+//    private static final ObjectFactory of = new ObjectFactory ();
 
-//        col   ("uuid_message",              Type.UUID,                          null,   "UUID запроса в ГИС ЖКХ");
-//        col   ("elementguid_new",           Type.UUID,                          null,   "Идентификатор новой версии существующего (в ГИС) элемента справочника");        
-//        col   ("is_deleted",                Type.BOOLEAN,                       null,   "1, если запись удалена; иначе 0");
-/*
-       trigger ("BEFORE INSERT", "BEGIN "
-               
-           + "SELECT"
-           + "       license_reg_date,              "
-           + "       license_organization,           "
-           + "       license_number,                 "
-           + "       licenseable_type_of_activity,  "
-           + "       license_version,            "
-           + "       additional_information,        "
-           + "       license_status,                 "
-           + "       region_fias_guid,              "
-           + "       licensing_authority             "
-           + "INTO "                
-           + "       :NEW.license_reg_date,              "
-           + "       :NEW.license_organization,           "
-           + "       :NEW.license_number,                 "
-           + "       :NEW.licenseable_type_of_activity,  "
-           + "       :NEW.license_version,            "
-           + "       :NEW.additional_information,        "
-           + "       :NEW.license_status,                 "
-           + "       :NEW.region_fias_guid,              "                   
-           + "       :NEW.licensing_authority             "
-           + " FROM "
-           + "  tb_licences                   "
-           + " WHERE uuid=:NEW.uuid_object; "
+    public LicenseLog () {
 
-       + "END;");      
-*/        
-    }    
+        super  ("tb_licences__log","Лицензии: история", License.class,
+                License.c.class
+        );
+
+        fk    ("uuid_out_soap", OutSoap.class, null, "Последний запрос на импорт в ГИС ЖКХ");
+    }
     
-}
+//    public static final RegOrgType regOrgType (UUID uuid) {
+//        final RegOrgType o = of.createRegOrgType ();
+//        o.setOrgRootEntityGUID (uuid.toString ());
+//        return o;
+//    }
+
+} 
