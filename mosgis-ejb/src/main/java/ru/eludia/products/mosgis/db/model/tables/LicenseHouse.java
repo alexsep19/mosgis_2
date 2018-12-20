3e572693f33af3f7375100588c8e1d3f8774b3bb
Aleksei Mukin <amukin@cheby.ru>
2018-12-21 00:35:59 +0300

    get new org

--- mosgis-ejb/src/main/java/ru/eludia/products/mosgis/db/model/tables/LicenseHouse.java
+++ mosgis-ejb/src/main/java/ru/eludia/products/mosgis/db/model/tables/LicenseHouse.java
@@ -11,29 +11,29 @@ import static ru.eludia.base.model.Type.STRING;
 
 public class LicenseHouse extends EnTable {
 
-    public enum Columns implements ColEnum {
+    public enum c implements ColEnum {
 
         FIASHOUSEGUID  (VocBuilding.class,         "Глобальный уникальный идентификатор дома по ФИАС"),
-        HOUSEADRESS    (STRING,       null,   "Адрес дома"),
+        HOUSEADDRESS   (STRING,            null,   "Адрес дома"),
         UUID_LICENSE   (License.class,     null,   "Лицензия"), 
         UUID_CONTRACT  (Contract.class,    null,   "Информация о договоре управления");
 
         @Override
         public Col getCol () {return col;}
         private Col col;        
-        private Columns (Type type, Object... p) {col = new Col (this, type, p);}
-        private Columns (Class c,   Object... p) {col = new Ref (this, c, p);} 
+        private c (Type type, Object... p) {col = new Col (this, type, p);}
+        private c (Class c,   Object... p) {col = new Ref (this, c, p);} 
         
     }
         
     public LicenseHouse () {
-        super ("tb_licence_houses", "Информация о доме в лицензии");
+        super ("tb_license_houses", "Информация о доме в лицензии");
         
-        cols   (Columns.class);
+        cols   (c.class);
         
-        key    ("fiashouseguid", LicenseHouse.Columns.FIASHOUSEGUID);
-        key    ("uuid_license",  LicenseHouse.Columns.UUID_LICENSE);
-        key    ("uuid_contract", LicenseHouse.Columns.UUID_CONTRACT);
+        key    ("fiashouseguid", LicenseHouse.c.FIASHOUSEGUID);
+        key    ("uuid_license",  LicenseHouse.c.UUID_LICENSE);
+        key    ("uuid_contract", LicenseHouse.c.UUID_CONTRACT);
 
     }
     
