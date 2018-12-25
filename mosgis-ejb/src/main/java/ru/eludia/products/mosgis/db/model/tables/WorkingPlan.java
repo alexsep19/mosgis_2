7c76842de27073539edcecf0378f9f3e968fed98
Dmitry Ovsyanko <do_@pochta.ru>
2018-12-25 13:33:54 +0300

    wp IN PROGRESS tasks/26591aea-ce55-40bf-bb8e-338713eb2a87

---
+++ mosgis-ejb/src/main/java/ru/eludia/products/mosgis/db/model/tables/WorkingPlan.java
@@ -0,0 +1,92 @@
+package ru.eludia.products.mosgis.db.model.tables;
+
+import ru.eludia.base.model.Col;
+import ru.eludia.base.model.Ref;
+import ru.eludia.base.model.Type;
+import ru.eludia.products.mosgis.db.model.EnColEnum;
+import ru.eludia.products.mosgis.db.model.EnTable;
+import ru.eludia.products.mosgis.db.model.voc.VocGisStatus;
+
+public class WorkingPlan extends EnTable {
+
+    public enum c implements EnColEnum {
+
+        UUID_WORKING_LIST    (WorkingList.class,     "Ссылка на перечень"),
+        ID_CTR_STATUS        (VocGisStatus.class,    VocGisStatus.DEFAULT, "Статус с точки зрения mosgis"),
+        ID_CTR_STATUS_GIS    (VocGisStatus.class,    VocGisStatus.DEFAULT, "Статус с точки зрения ГИС ЖКХ"),
+        YEAR                 (Type.NUMERIC, 4,       "Год")
+        ;
+
+        @Override
+        public Col getCol () {return col;}
+        private Col col;        
+        private c (Type type, Object... p) {col = new Col (this, type, p);}
+        private c (Class c,   Object... p) {col = new Ref (this, c, p);}
+
+        @Override
+        public boolean isLoggable () {
+            switch (this) {
+//                case ID_LOG:
+                case UUID_WORKING_LIST:
+                    return false;
+                default: 
+                    return true;
+            }
+        }
+
+    }
+
+    public WorkingPlan () {
+        super ("tb_work_plans", "Gkf работ и услуг на период");
+        cols   (c.class);
+        key    ("uuid_working_list", c.UUID_WORKING_LIST);
+    }
+    
+/*    
+    public enum Action {
+        
+        PLACING     (VocGisStatus.i.PENDING_RP_PLACING,   VocGisStatus.i.APPROVED, VocGisStatus.i.FAILED_PLACING),
+        ANNULMENT   (VocGisStatus.i.PENDING_RP_ANNULMENT, VocGisStatus.i.ANNUL,    VocGisStatus.i.FAILED_ANNULMENT)
+        ;
+        
+        VocGisStatus.i nextStatus;
+        VocGisStatus.i okStatus;
+        VocGisStatus.i failStatus;
+
+        private Action (VocGisStatus.i nextStatus, VocGisStatus.i okStatus, VocGisStatus.i failStatus) {
+            this.nextStatus = nextStatus;
+            this.okStatus = okStatus;
+            this.failStatus = failStatus;
+        }
+
+        public VocGisStatus.i getNextStatus () {
+            return nextStatus;
+        }
+
+        public VocGisStatus.i getFailStatus () {
+            return failStatus;
+        }
+
+        public VocGisStatus.i getOkStatus () {
+            return okStatus;
+        }
+
+        public static Action forStatus (VocGisStatus.i status) {
+            switch (status) {
+                case PENDING_RQ_PLACING:   return PLACING;
+                case PENDING_RQ_ANNULMENT: return ANNULMENT;
+                default: return null;
+            }            
+        }
+
+        public static Action forLogAction (VocAction.i a) {
+            switch (a) {
+                case APPROVE: return PLACING;
+                case ANNUL:   return ANNULMENT;
+                default: return null;
+            }
+        }
+
+    };
+*/
+}
