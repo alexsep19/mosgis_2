7c9bf6b0806d5598ec953787cc20647d11de400a
Mihail Mochajlo <mihail.mochajlo@gmail.com>
2018-12-27 13:04:42 +0300

    res premise restore operation for tasks/3d20353f-f0e6-4799-b313-6f2fff226f02

--- mosgis-api/src/main/java/ru/eludia/products/mosgis/rest/api/ResidentialPremisesLocal.java
+++ mosgis-api/src/main/java/ru/eludia/products/mosgis/rest/api/ResidentialPremisesLocal.java
@@ -1,9 +1,12 @@
 package ru.eludia.products.mosgis.rest.api;
 
 import javax.ejb.Local;
+import javax.json.JsonObject;
 import ru.eludia.products.mosgis.rest.api.base.PassportBackend;
 
 @Local
 public interface ResidentialPremisesLocal extends PassportBackend {
+    
+    public JsonObject doRestore (String id);
     
 }
