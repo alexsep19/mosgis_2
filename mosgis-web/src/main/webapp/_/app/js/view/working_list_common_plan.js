6eb065734109091f7faf2c91467ed4582f6520f3
Dmitry Ovsyanko <do_@pochta.ru>
2018-12-29 13:23:43 +0300

    wl editing FIX tasks/26591aea-ce55-40bf-bb8e-338713eb2a87

--- mosgis-web/src/main/webapp/_/app/js/view/working_list_common_plan.js
+++ mosgis-web/src/main/webapp/_/app/js/view/working_list_common_plan.js
@@ -1,8 +1,10 @@
 define ([], function () {
 
     return function (data, view) {
+    
+        var it = data.item
 
-        var is_own = data.item._can.edit_plan
+        var is_own = it._can.edit_plan
         
         var columns = [
             {field: 'index_', caption: '№', size: 1},
@@ -11,13 +13,31 @@ define ([], function () {
             {field: 'count', caption: 'К-во в перечне', size: 10},
             {field: 'cnt', caption: 'К-во в плане', size: 10},
         ]
+
+        for (var i = 0; i < 12; i ++) {
         
-        for (var i = 0; i < 12; i ++) columns.push ({
-            field: 'cnt_' + (i + 1), 
-            caption: w2utils.settings.shortmonths [i].toLowerCase (), 
-            size: 10,
-            editable: !is_own ? false : {type: 'int', min: 1},
-        })
+            var col = {
+                field: 'cnt_' + (i + 1), 
+                caption: w2utils.settings.shortmonths [i].toLowerCase (), 
+                size: 10
+            }
+            
+            if (is_own) {
+            
+                var m = i + 1
+            
+                var dt = data.plan.year + '-'
+                if (m < 10) dt += '0'
+                dt += m
+                dt += '-01'
+    
+                if (it.dt_from <= dt && dt <= it.dt_to) col.editable = {type: 'int', min: 1}
+
+            }
+        
+            columns.push (col)
+        
+        }
             
         $(w2ui ['passport_layout'].el ('main')).w2regrid ({ 
 
@@ -37,6 +57,7 @@ define ([], function () {
             onChange: $_DO.patch_working_list_common_plan,
             
             onContextMenu: !is_own ? null : function (e) {
+                if (!this.columns [e.column].editable) return
                 $_SESSION.set ('cell', {uuid: data.plan.uuid, uuid_working_list_item: e.recid, year: data.plan.year, month: e.column - 5})
                 use.block ('working_list_common_plan_dates_popup')
             },
