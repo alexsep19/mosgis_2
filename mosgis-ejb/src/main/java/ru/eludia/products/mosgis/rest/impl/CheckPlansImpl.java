64b0037f7f3386d5a9c60463252603dffc62e7cc
Mihail Mochajlo <mihail.mochajlo@gmail.com>
2019-01-10 11:39:45 +0300

    SELECT check plans for tasks/0020518d-83fb-4af6-9b56-5c8c2d4669df

--- mosgis-ejb/src/main/java/ru/eludia/products/mosgis/rest/impl/CheckPlansImpl.java
+++ mosgis-ejb/src/main/java/ru/eludia/products/mosgis/rest/impl/CheckPlansImpl.java
@@ -4,7 +4,9 @@ import javax.ejb.Stateless;
 import javax.ejb.TransactionAttribute;
 import javax.ejb.TransactionAttributeType;
 import javax.json.JsonObject;
+import ru.eludia.base.db.sql.gen.Select;
 import ru.eludia.products.mosgis.db.model.tables.CheckPlan;
+import ru.eludia.products.mosgis.ejb.ModelHolder;
 import ru.eludia.products.mosgis.rest.User;
 import ru.eludia.products.mosgis.rest.api.CheckPlansLocal;
 import ru.eludia.products.mosgis.rest.impl.base.BaseCRUD;
@@ -14,9 +16,13 @@ import ru.eludia.products.mosgis.rest.impl.base.BaseCRUD;
 public class CheckPlansImpl extends BaseCRUD<CheckPlan> implements CheckPlansLocal {
 
     @Override
-    public JsonObject select(JsonObject p, User user) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+    public JsonObject select(JsonObject p, User user) {return fetchData ((db, job) -> {
+        
+        Select select = ModelHolder.getModel ().select(getTable (), "AS root", "*", "uuid AS id");
+        
+        db.addJsonArrayCnt (job, select);
+        
+    });}
 
     @Override
     public JsonObject getItem(String id) {
