5f806d8284d107e644302aa9a88e178a871778b3
Mihail Mochajlo <mihail.mochajlo@gmail.com>
2019-01-10 13:59:23 +0300

    search by reg number for tasks/0020518d-83fb-4af6-9b56-5c8c2d4669df

--- mosgis-ejb/src/main/java/ru/eludia/products/mosgis/rest/impl/CheckPlansImpl.java
+++ mosgis-ejb/src/main/java/ru/eludia/products/mosgis/rest/impl/CheckPlansImpl.java
@@ -10,16 +10,58 @@ import ru.eludia.products.mosgis.ejb.ModelHolder;
 import ru.eludia.products.mosgis.rest.User;
 import ru.eludia.products.mosgis.rest.api.CheckPlansLocal;
 import ru.eludia.products.mosgis.rest.impl.base.BaseCRUD;
+import ru.eludia.products.mosgis.web.base.ComplexSearch;
+import ru.eludia.products.mosgis.web.base.Search;
+import ru.eludia.products.mosgis.web.base.SimpleSearch;
 
 @Stateless
 @TransactionAttribute(TransactionAttributeType.NOT_SUPPORTED)
 public class CheckPlansImpl extends BaseCRUD<CheckPlan> implements CheckPlansLocal {
 
+    private void filterOffDeleted (Select select) {
+        select.and ("is_deleted", 0);
+    }
+    
+    private void applyComplexSearch (final ComplexSearch search, Select select) {
+
+        search.filter (select, "");
+        
+        if (!search.getFilters ().containsKey ("is_deleted")) filterOffDeleted (select);
+
+    }
+    
+    private void applySimpleSearch (final SimpleSearch search, Select select) {
+
+        filterOffDeleted (select);
+
+        final String searchString = search.getSearchString ();
+        
+        if (searchString == null || searchString.isEmpty ()) return;
+
+        select.and ("uriregistrationplannumber LIKE %?%", searchString);
+        
+    }
+    
+    private void applySearch (final Search search, Select select) {        
+
+        if (search instanceof SimpleSearch) {
+            applySimpleSearch  ((SimpleSearch) search, select);
+        }
+        else if (search instanceof ComplexSearch) {
+            applyComplexSearch ((ComplexSearch) search, select);
+        }
+        else {
+            filterOffDeleted (select);
+        }
+
+    }
+    
     @Override
     public JsonObject select(JsonObject p, User user) {return fetchData ((db, job) -> {
         
-        Select select = ModelHolder.getModel ().select(getTable (), "AS root", "*", "uuid AS id")
-                .where ("is_deleted", 0);
+        Select select = ModelHolder.getModel ().select(getTable (), "AS root", "*", "uuid AS id");
+        
+        applySearch (Search.from (p), select);
         
         db.addJsonArrayCnt (job, select);
         
