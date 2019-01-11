a43f353e2e89c869d6c584192e8d570910d38824
Mihail Mochajlo <mihail.mochajlo@gmail.com>
2019-01-11 10:59:34 +0300

    tab level fix for tasks/c725540e-fd94-4236-aa4e-90b4272f672c

--- mosgis-web/src/main/webapp/_/app/js/view/main.js
+++ mosgis-web/src/main/webapp/_/app/js/view/main.js
@@ -21,8 +21,9 @@ define ([], function () {
                             {id: 'out_soap',     caption: 'Запросы сервиса', text: 'Запросы сервиса'},
                             {id: 'voc_nsi_list', caption: 'Справочники', text: 'Справочники'},
                             {id: 'rosters',      caption: 'Реестры', text: 'Реестры', tooltip: ''},
+                            {id: 'supervision', caption: 'Надзор', off: !($_USER.role.admin || $_USER.has_nsi_20 (4))},
 //                            {id: 'admin',        caption: 'Администрирование'},
-                        ],
+                        ].filter (not_off),
 
                         onClick: $_DO.choose_tab_main
 
