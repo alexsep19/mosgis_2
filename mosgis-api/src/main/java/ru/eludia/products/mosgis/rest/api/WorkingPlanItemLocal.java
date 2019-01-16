9ba3d4d611516242ec27269228901374b408ba11
Dmitry Ovsyanko <do_@pochta.ru>
2019-01-16 14:47:26 +0300

    fact working_plan_items rest resource tasks/221f9bfa-72aa-42bd-9694-5d800f2fda00

---
+++ mosgis-api/src/main/java/ru/eludia/products/mosgis/rest/api/WorkingPlanItemLocal.java
@@ -0,0 +1,10 @@
+package ru.eludia.products.mosgis.rest.api;
+
+import javax.ejb.Local;
+import javax.json.JsonObject;
+import ru.eludia.products.mosgis.rest.User;
+
+@Local
+public interface WorkingPlanItemLocal {
+    JsonObject select     (JsonObject p, User user);
+}
