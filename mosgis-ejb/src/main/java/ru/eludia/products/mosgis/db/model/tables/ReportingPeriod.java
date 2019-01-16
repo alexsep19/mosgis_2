75c5e411bf4056ff8bc1c0c3ebaa31c98c1b627a
Dmitry Ovsyanko <do_@pochta.ru>
2019-01-16 09:32:40 +0300

    fact tb_reporting_period added tasks.cheby.ru/tasks/221f9bfa-72aa-42bd-9694-5d800f2fda00

---
+++ mosgis-ejb/src/main/java/ru/eludia/products/mosgis/db/model/tables/ReportingPeriod.java
@@ -0,0 +1,37 @@
+package ru.eludia.products.mosgis.db.model.tables;
+
+import ru.eludia.base.model.Col;
+import ru.eludia.base.model.ColEnum;
+import ru.eludia.base.model.Ref;
+import ru.eludia.base.model.Type;
+import ru.eludia.products.mosgis.db.model.EnTable;
+
+public class ReportingPeriod extends EnTable {
+
+    public enum c implements ColEnum {
+
+        UUID_WORKING_PLAN      (WorkingPlan.class,           "Ссылка на план"),
+        MONTH                  (Type.NUMERIC, 2,             "Месяц"),
+        REPORTINGPERIODGUID    (Type.UUID,  null,            "Ссылка на период отчётности о выполненных работах")
+        ;
+
+        @Override
+        public Col getCol () {return col;}
+        private Col col;        
+        private c (Type type, Object... p) {col = new Col (this, type, p);}
+        private c (Class c,   Object... p) {col = new Ref (this, c, p);}
+
+    }
+
+    public ReportingPeriod () {
+        
+        super  ("tb_reporting_periods", "Периоды отчётности в планах работ и услуг");
+        cols   (c.class);        
+        unique ("uuid_working_plan", 
+            c.UUID_WORKING_PLAN, 
+            c.MONTH
+        );
+
+    }
+
+}
