2c3268f1e0302c27145792e10cab464037a4719f
Dmitry Ovsyanko <do_@pochta.ru>
2019-01-16 14:17:30 +0300

    fact reporting_period common tab tasks/221f9bfa-72aa-42bd-9694-5d800f2fda00

---
+++ mosgis-web/src/main/webapp/_/app/js/data/reporting_period_common.js
@@ -0,0 +1,54 @@
+define ([], function () {
+
+    var form_name = 'reporting_period_common_form'    
+/*    
+    $_DO.approve_reporting_period_common = function (e) {
+        if (!confirm ('Разместить эти данные в ГИС ЖКХ?')) return
+        query ({type: 'reporting_periods', action: 'approve'}, {}, reload_page)
+    }
+    
+    $_DO.alter_reporting_period_common = function (e) {
+        if (!confirm ('Открыть эту карточку на редактирование?')) return
+        query ({type: 'reporting_periods', action: 'alter'}, {}, reload_page)
+    }
+*/
+    $_DO.choose_tab_reporting_period_common = function (e) {
+    
+        var name = e.tab.id
+                        
+        var layout = w2ui ['passport_layout']
+
+        if (layout) {                
+            layout.content ('main', '');
+            layout.lock ('main', 'Загрузка...', true);
+        }
+
+        localStorage.setItem ('reporting_period_common.active_tab', name)
+
+        if (/^reporting_period_common_plan_/.test (name)) {
+            name = 'reporting_period_common_plan'
+        }
+
+        use.block (name)        
+    
+    }
+    
+    return function (done) {        
+
+        w2ui ['topmost_layout'].unlock ('main')
+        
+        var data = clone ($('body').data ('data'))
+
+        var it = data.item
+        
+        data.active_tab = localStorage.getItem ('reporting_period_common.active_tab') || 'reporting_period_planned_works'
+
+        data.__read_only = 1
+       
+        it.gis_status_label = 'проект...'//data.vc_gis_status [it.id_ctr_status]
+
+        done (data)
+        
+    }
+
+})
---
+++ mosgis-web/src/main/webapp/_/app/js/view/reporting_period_common.js
@@ -0,0 +1,74 @@
+define ([], function () {
+    
+    var form_name = 'reporting_period_common_form'
+    
+    return function (data, view) {
+        
+        $_F5 = function (data) {
+        
+            data.item.__read_only = data.__read_only
+            
+            var r = clone (data.item)
+            
+            var f = w2ui [form_name]
+            
+            f.record = r
+            
+            $('div[data-block-name=reporting_period_common] input').prop ({disabled: data.__read_only})
+
+            f.refresh ()
+
+        }
+
+        var layout = w2ui ['topmost_layout']
+
+        var $panel = $(layout.el ('main'))
+        
+        $panel.w2relayout ({
+        
+            name: 'passport_layout',
+            
+            panels: [
+                
+                {type: 'top', size: 130},
+                {type: 'main', size: 400, 
+                    tabs: {
+                        tabs: [
+                            {id: 'reporting_period_planned_works', caption: 'Информация об услугах и работах'},
+//                            {id: 'reporting_period_common_log', caption: 'История синхронизации'},
+                        ],
+                        onClick: $_DO.choose_tab_reporting_period_common
+                    }
+                },
+                
+            ],
+            
+            onRender: function (e) {
+                this.get ('main').tabs.click (data.active_tab)
+            },            
+
+        });
+        
+        var $panel = $(w2ui ['passport_layout'].el ('top'))
+                
+        fill (view, data.item, $panel)        
+                
+        $panel.w2reform ({ 
+        
+            name   : form_name,
+            
+            record : data.item,
+            
+            fields : [                     
+                {name: 'year', type: 'text'},
+            ],
+
+            focus: -1,
+            
+        })
+
+        $_F5 (data)        
+
+    }
+    
+})
