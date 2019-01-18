b65c9125634166d710e8a83ecf156badca24f77c
Mihail Mochajlo <mihail.mochajlo@gmail.com>
2019-01-18 12:40:47 +0300

    nsi 33 and 38 usage fixed

--- mosgis-ejb/src/main/java/ru/eludia/products/mosgis/db/model/tables/InfrastructureNsi3.java
+++ mosgis-ejb/src/main/java/ru/eludia/products/mosgis/db/model/tables/InfrastructureNsi3.java
@@ -1,11 +1,7 @@
 package ru.eludia.products.mosgis.db.model.tables;
 
-import ru.eludia.base.model.Col;
-import ru.eludia.base.model.Ref;
 import ru.eludia.base.model.Table;
 import ru.eludia.base.model.Type;
-import static ru.eludia.base.model.Type.STRING;
-import ru.eludia.products.mosgis.db.model.EnColEnum;
 
 public class InfrastructureNsi3 extends Table {
 
