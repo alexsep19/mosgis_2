ca82df94386f58afa3e213de9d738a23c09043d5
Mihail Mochajlo <mihail.mochajlo@gmail.com>
2019-01-18 11:17:26 +0300

    nsi_3 additional table fix for tasks/6c4f603f-7aa9-4445-b74d-3edb99fc43f4

--- mosgis-ejb/src/main/java/ru/eludia/products/mosgis/db/model/tables/InfrastructureNsi3.java
+++ mosgis-ejb/src/main/java/ru/eludia/products/mosgis/db/model/tables/InfrastructureNsi3.java
@@ -8,36 +8,14 @@ import static ru.eludia.base.model.Type.STRING;
 import ru.eludia.products.mosgis.db.model.EnColEnum;
 
 public class InfrastructureNsi3 extends Table {
-    
-    public enum c implements EnColEnum {
-        
-        UUID (Infrastructure.class, "Объект коммунальной инфраструктуры"),
-        CODE (STRING, 20,           "Ссылка на НСИ \"Вид коммунальной услуги\" (НСИ 3)")
-        
-        ;
-        
-        @Override
-        public Col getCol () {return col;}
-        private Col col;
-        private c (Type type, Object... p) {col = new Col (this, type, p);}
-        private c (Class c,   Object... p) {col = new Ref (this, c, p);}
 
-        @Override
-        public boolean isLoggable () {
-            return false;
-        }
-        
-    }
-    
     public InfrastructureNsi3 () {
         
         super ("tb_infrastructures_nsi_3", "Виды коммунальных услуг коммунальных инфраструктур");
         
-        cols (c.class);
-        
-        pk (c.UUID);
-        pk (c.CODE);
-        
+        pkref ("uuid",       Infrastructure.class,  "Объект коммунальной инфраструктуры");
+        pk    ("code",       Type.STRING,  20,      "Ссылка на НСИ \"Вид коммунальной услуги\" (НСИ 3)");
+
     }
-    
+
 }
