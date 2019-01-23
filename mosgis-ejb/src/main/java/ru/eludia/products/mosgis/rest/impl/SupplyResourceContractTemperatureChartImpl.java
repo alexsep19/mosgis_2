5764d8974f7c2f85825a7fb0c4606e1769267442
Maksim Gruzdev <mgruzdev@cheby.ru>
2019-01-23 16:16:45 +0300

    sr_ctr_t_charts tasks/7c8b7924-0b57-4719-bb3f-4a8ddc17a3b8

---
+++ mosgis-ejb/src/main/java/ru/eludia/products/mosgis/rest/impl/SupplyResourceContractTemperatureChartImpl.java
@@ -0,0 +1,93 @@
+package ru.eludia.products.mosgis.rest.impl;
+
+import java.util.logging.Logger;
+import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+import javax.json.JsonObject;
+import ru.eludia.base.DB;
+import ru.eludia.base.Model;
+import ru.eludia.base.db.sql.gen.Operator;
+import ru.eludia.base.db.sql.gen.Select;
+import ru.eludia.products.mosgis.db.model.EnTable;
+import ru.eludia.products.mosgis.db.model.MosGisModel;
+import ru.eludia.products.mosgis.db.model.tables.ActualSupplyResourceContract;
+import ru.eludia.products.mosgis.db.model.tables.SupplyResourceContractTemperatureChart;
+import ru.eludia.products.mosgis.ejb.ModelHolder;
+import ru.eludia.products.mosgis.rest.User;
+import ru.eludia.products.mosgis.rest.api.SupplyResourceContractTemperatureChartLocal;
+import ru.eludia.products.mosgis.rest.impl.base.BaseCRUD;
+import ru.eludia.products.mosgis.web.base.ComplexSearch;
+import ru.eludia.products.mosgis.web.base.Search;
+import ru.eludia.products.mosgis.web.base.SimpleSearch;
+
+@Stateless
+@TransactionAttribute(TransactionAttributeType.NOT_SUPPORTED)
+public class SupplyResourceContractTemperatureChartImpl extends BaseCRUD<SupplyResourceContractTemperatureChart> implements SupplyResourceContractTemperatureChartLocal {
+
+    private static final Logger logger = Logger.getLogger (SupplyResourceContractTemperatureChartImpl.class.getName ());
+
+    private void filterOffDeleted (Select select) {
+        select.and (EnTable.c.IS_DELETED, Operator.EQ, 0);
+    }
+
+    private void applyComplexSearch (final ComplexSearch search, Select select) {
+
+        search.filter (select, "");
+        if (!search.getFilters ().containsKey ("is_deleted")) filterOffDeleted (select);
+
+    }
+
+    private void applySimpleSearch (final SimpleSearch search, Select select) {
+
+        final String s = search.getSearchString ();
+
+//        if (s != null) select.and ("vc_nsi_3.label LIKE ?%", s.toUpperCase ().replace (' ', '%'));
+
+    }
+
+    private void applySearch (final Search search, Select select) {
+
+        if (search instanceof ComplexSearch) {
+            applyComplexSearch ((ComplexSearch) search, select);
+        }
+        else {
+            if (search instanceof SimpleSearch) applySimpleSearch  ((SimpleSearch) search, select);
+            filterOffDeleted (select);
+        }
+
+    }
+
+    @Override
+    public JsonObject select (JsonObject p, User user) {return fetchData ((db, job) -> {
+
+        final Model m = ModelHolder.getModel ();
+
+        Select select = m.select (SupplyResourceContractTemperatureChart.class, "*", "uuid AS id")
+//	    .where(SupplyResourceContractTemperatureChart.c.UUID_SR_CTR_OBJ.lc() + " IS NULL")
+            .orderBy (SupplyResourceContractTemperatureChart.c.OUTSIDETEMPERATURE.lc ())
+            .limit (p.getInt ("offset"), p.getInt ("limit"));
+
+        applySearch (Search.from (p), select);
+
+        JsonObject data = p.getJsonObject ("data");
+
+        String k = SupplyResourceContractTemperatureChart.c.UUID_SR_CTR.lc ();
+        String v = data.getString (k, null);
+        if (DB.ok (v)) select.and (k, v);
+
+        db.addJsonArrayCnt (job, select);
+    });}
+
+    @Override
+    public JsonObject getItem (String id) {return fetchData ((db, job) -> {
+
+        final MosGisModel m = ModelHolder.getModel ();
+
+        final JsonObject item = db.getJsonObject (m
+            .get (SupplyResourceContractTemperatureChart.class, id, "*")
+        );
+
+        job.add ("item", item);
+    });}
+}
