6014b6793d67c948b9d4e9b8b63d0bdecfb6e4b4
Dmitry Ovsyanko <do_@pochta.ru>
2019-01-24 11:04:30 +0300

    rp ws basic soap tasks/a884414f-0f23-4013-b641-36256a3686bf

--- mosgis-ejb/src/main/java/ru/eludia/products/mosgis/db/model/tables/ReportingPeriodLog.java
+++ mosgis-ejb/src/main/java/ru/eludia/products/mosgis/db/model/tables/ReportingPeriodLog.java
@@ -1,7 +1,13 @@
 package ru.eludia.products.mosgis.db.model.tables;
 
+import java.util.Map;
+import ru.eludia.base.DB;
+import ru.eludia.base.db.sql.gen.Get;
 import ru.eludia.products.mosgis.db.model.EnTable;
 import ru.eludia.products.mosgis.db.model.GisWsLogTable;
+import ru.eludia.products.mosgis.db.model.voc.VocOrganization;
+import ru.gosuslugi.dom.schema.integration.services.CompletedWorksByPeriodType;
+import ru.gosuslugi.dom.schema.integration.services.ImportCompletedWorksRequest;
 
 public class ReportingPeriodLog extends GisWsLogTable {
 
@@ -13,5 +19,63 @@ public class ReportingPeriodLog extends GisWsLogTable {
         );
         
     }
+    
+    public static ImportCompletedWorksRequest toCompletedWorksRequest (Map<String, Object> r) {
+        final ImportCompletedWorksRequest result = new ImportCompletedWorksRequest ();
+        result.setCompletedWorksByPeriod (toCompletedWorksByPeriodType (r));
+        return result;
+    }    
+    
+    private static CompletedWorksByPeriodType toCompletedWorksByPeriodType (Map<String, Object> r) {
+        final CompletedWorksByPeriodType result = DB.to.javaBean (CompletedWorksByPeriodType.class, r);
+        return result;
+    }
+    
+    public Get getForExport (String id) {
+
+        return (Get) getModel ()
+                
+            .get (this, id, "*")
+                
+            .toOne (ReportingPeriod.class, "AS r"
+                , ReportingPeriod.c.ID_CTR_STATUS.lc ()
+            ).on ()
+                
+            .toOne (WorkingPlan.class, "AS wp").on ()
+            .toOne (WorkingList.class, "AS l").on ()
+
+            .toMaybeOne (ContractObject.class, "AS co").on ()
+            .toMaybeOne (Contract.class, "AS ctr"
+                , "contractguid AS contractguid"
+            ).on ()
+            .toMaybeOne (VocOrganization.class, "AS o1"
+                , "orgppaguid AS orgppaguid_1"
+            ).on ("ctr.uuid_org=o1.uuid")
+
+            .toMaybeOne (CharterObject.class, "AS co").on ()
+            .toMaybeOne (Charter.class, "AS ch"
+                , "charterguid AS charterguid"
+            ).on ()
+            .toMaybeOne (VocOrganization.class, "AS o2"
+                , "orgppaguid AS orgppaguid_2"
+            ).on ("ch.uuid_org=o1.uuid")
+
+        ;
+        
+    }    
+/*    
+    public static void addPlannedWorksForExport (DB db, Map<String, Object> r) throws SQLException {
+
+        r.put ("refs", db.getList (db.getModel ()
+                
+//            .select (PublicPropertyContractVotingProtocol.class)
+//            .toOne  (VotingProtocol.class, VotingProtocol.c.VOTINGPROTOCOLGUID.lc () + " AS guid").and (EnTable.c.IS_DELETED.IS_DELETED.lc (), 0).on ()
+//            .where  (PublicPropertyContractVotingProtocol.c.UUID_CTR.lc (), r.get ("uuid_object"))
+//            .and    ("is_deleted", 0)
+
+        ));
+
+    }
+*/    
     
 }
