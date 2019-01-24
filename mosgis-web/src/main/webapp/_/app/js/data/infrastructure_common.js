7a7ac73a241fedbd0bcde45fac2abbbc8d2a775a
Mihail Mochajlo <mihail.mochajlo@gmail.com>
2019-01-24 12:49:51 +0300

    validation fix

--- mosgis-web/src/main/webapp/_/app/js/data/infrastructure_common.js
+++ mosgis-web/src/main/webapp/_/app/js/data/infrastructure_common.js
@@ -101,8 +101,7 @@ define ([], function () {
         var v = f.values ()
 
         var reg_year = /^[1-2][0-9]{3}$/
-
-        console.log (v)
+        var reg_int  = /^[1-9][0-9]*$/
 
         var elements_nsi_33 = {
             independentsource: {'1.1':1, '2.1':1, '5.1':1, '5.2':1},
@@ -120,7 +119,13 @@ define ([], function () {
         if (!v.code_vc_nsi_33) die ('code_vc_nsi_33', 'Укажите, пожалуйста, вид объекта')
         if (!v.oktmo) die ('oktmo_code', 'Укажите, пожалуйста, код ОКТМО')
         if (!v.comissioningyear) die ('comissioningyear', 'Укажите, пожалуйста, год ввода в эксплуатацию')
-        if (!reg_year.test (v.comissioningyear) || v.comissioningyear < 1600) die ('comissioningyear', 'Указано неверное значение года ввода в эксплуатацию') 
+        if (!reg_year.test (v.comissioningyear) || v.comissioningyear < 1600) die ('comissioningyear', 'Указано неверное значение года ввода в эксплуатацию')
+        if (v.countaccidents && !reg_int.test (v.countaccidents)) die ('countaccidents', 'Указано неверное значение количества аварий на 100 км сетей')
+        if (v.deterioration) {
+            var deterioration = parseFloat (v.deterioration)
+            if (isNaN (deterioration) || deterioration < 0 || deterioration > 100) die ('deterioration', 'Указано неверное значения уровня износа')
+            v.deterioration = deterioration
+        }
 
         Object.keys(elements_nsi_33).forEach ((value, index, array) => {
             if (elements_nsi_33[value][v.code_vc_nsi_33] && !v[value]) die (value, 'Указаны не все необходимые данные')
