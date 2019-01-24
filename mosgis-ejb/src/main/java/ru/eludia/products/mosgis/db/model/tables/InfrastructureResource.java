cb29432ccef22886d2abad576d564ed6c04ec370
Mihail Mochajlo <mihail.mochajlo@gmail.com>
2019-01-24 15:31:50 +0300

    resource tables added for tasks/032043af-9769-47c4-aa0b-5f9820b832f7

---
+++ mosgis-ejb/src/main/java/ru/eludia/products/mosgis/db/model/tables/InfrastructureResource.java
@@ -0,0 +1,58 @@
+package ru.eludia.products.mosgis.db.model.tables;
+
+import ru.eludia.base.model.Col;
+import ru.eludia.base.model.Ref;
+import ru.eludia.base.model.Type;
+import static ru.eludia.base.model.Type.NUMERIC;
+import static ru.eludia.base.model.Type.STRING;
+import ru.eludia.products.mosgis.db.model.EnColEnum;
+import ru.eludia.products.mosgis.db.model.EnTable;
+import ru.eludia.products.mosgis.db.model.voc.VocOkei;
+
+public class InfrastructureResource extends EnTable {
+    
+    public enum c implements EnColEnum {
+        
+        CODE_VC_NSI_2  (STRING, 20, "Ресурс (НСИ 2)"),
+        
+        SETPOWER       (NUMERIC, 9, 3, null, "Установленная мощность"),
+        SITINGPOWER    (NUMERIC, 9, 3, null, "Располагаемая мощность"),
+        
+        TOTALLOAD      (NUMERIC, 9, 3, null, "Присоединенная нагрузка"),
+        INDUSTRIALLOAD (NUMERIC, 9, 3, null, "Промышленность"),
+        SOCIALLOAD     (NUMERIC, 9, 3, null, "Социальная сфера"),
+        POPULATIONLOAD (NUMERIC, 9, 3, null, "Население"),
+        
+        OKEI           (VocOkei.class, "Единицы измерения (ОКЕИ)"),
+        
+        ID_LOG         (InfrastructureResourceLog.class, "Посленее событие редактирования")
+        
+        ;
+        
+        @Override
+        public Col getCol () {return col;}
+        private Col col;
+        private c (Type type, Object... p) {col = new Col (this, type, p);}
+        private c (Class c,   Object... p) {col = new Ref (this, c, p);}
+
+        @Override
+        public boolean isLoggable () {
+            switch (this) {
+                case ID_LOG:
+                    return false;
+                default:
+                    return true;
+            }
+        }
+        
+    }
+    
+    public InfrastructureResource () {
+        
+        super ("tb_infrastructure_resources", "Мощности ОКИ");
+        
+        cols (c.class);
+        
+    }
+    
+}
