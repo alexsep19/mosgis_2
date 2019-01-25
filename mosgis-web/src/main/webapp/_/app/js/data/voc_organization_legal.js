22f248de6a40722619c8f05ec2b60965c077c3b8
Dmitry Ovsyanko <do_@pochta.ru>
2019-01-25 11:20:53 +0300

    import button tasks/9c988446-f1a5-45dd-bd50-01dcce0096f4

--- mosgis-web/src/main/webapp/_/app/js/data/voc_organization_legal.js
+++ mosgis-web/src/main/webapp/_/app/js/data/voc_organization_legal.js
@@ -28,11 +28,32 @@ define ([], function () {
     return function (done) {
 
         query ({type: 'voc_organizations'}, {}, function (data) {
+            
+            data.nsi_20 = {}
+        
+            $.each (data.vc_orgs_nsi_20, function () {data.nsi_20 [this.code] = 1})
+            
+            if ($_USER.role.admin) {
 
+                data.import_types = [
+
+                    {id: "mgmt_contracts", label: "Импорт договоров управления", off: !data.nsi_20 [1]}
+
+                ].filter (not_off)
+                
+            }
+            
             add_vocabularies (data, {
                 vc_acc_req_status: 1,
                 vc_acc_req_types: 1,
             })
+            
+            var it = data.item
+            
+            it._can = {
+                refresh: 1,
+                import: data.import_types
+            }
 
             $('body').data ('data', data)
 
