5b363c02441f2684d6ab7d250ee0f62cbf8b8889
Dmitry Ovsyanko <do_@pochta.ru>
2019-01-25 15:37:18 +0300

    import jms tasks/9c988446-f1a5-45dd-bd50-01dcce0096f4

---
+++ mosgis-ejb/src/main/java/ru/eludia/products/mosgis/jms/gis/poll/GisPollOrgImportMgmtContracts.java
@@ -0,0 +1,81 @@
+package ru.eludia.products.mosgis.jms.gis.poll;
+
+import java.sql.SQLException;
+import java.util.Map;
+import java.util.UUID;
+import javax.ejb.ActivationConfigProperty;
+import javax.ejb.EJB;
+import javax.ejb.MessageDriven;
+import ru.eludia.base.DB;
+import ru.eludia.base.db.sql.gen.Get;
+import ru.eludia.products.mosgis.db.model.voc.VocOrganization;
+import ru.eludia.products.mosgis.ejb.ModelHolder;
+import ru.eludia.products.mosgis.jms.gis.poll.base.GisPollException;
+import ru.eludia.products.mosgis.jms.gis.poll.base.GisPollMDB;
+import ru.eludia.products.mosgis.jms.gis.poll.base.GisPollRetryException;
+import ru.gosuslugi.dom.schema.integration.house_management_service_async.Fault;
+import ru.eludia.products.mosgis.db.model.voc.VocOrganizationLog;
+import ru.eludia.products.mosgis.ejb.wsc.WsGisHouseManagementClient;
+import ru.gosuslugi.dom.schema.integration.house_management.GetStateResult;
+
+@MessageDriven(activationConfig = {
+ @ActivationConfigProperty(propertyName = "destinationLookup", propertyValue = "mosgis.outExportOrgMgmtContractsQueue")
+ , @ActivationConfigProperty(propertyName = "subscriptionDurability", propertyValue = "Durable")
+ , @ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Queue")
+})
+public class GisPollOrgImportMgmtContracts extends GisPollMDB {
+
+    @EJB
+    WsGisHouseManagementClient wsGisHouseManagementClient;
+
+    @Override
+    protected Get get (UUID uuid) {
+
+        return (Get) ModelHolder.getModel ().get (getTable (), uuid, "AS root", "*")
+            .toOne (VocOrganizationLog.class,     "AS log", "uuid", "action").on ("log.uuid_out_soap=root.uuid")
+            .toOne (VocOrganization.class, VocOrganization.c.ORGPPAGUID.lc () + " AS ppa").on ()
+        ;
+        
+    }
+
+    @Override
+    protected void handleOutSoapRecord (DB db, UUID uuid, Map<String, Object> r) throws SQLException {
+        
+        if (DB.ok (r.get ("is_failed"))) throw new IllegalStateException (r.get ("err_text").toString ());
+        
+        UUID orgPPAGuid = (UUID) r.get ("ppa");
+                        
+        try {            
+            GetStateResult state = getState (orgPPAGuid, r);            
+logger.info ("state=" + state);
+        }
+        catch (GisPollRetryException ex) {
+            return;
+        }
+        catch (GisPollException ex) {            
+            ex.register (db, uuid, r);
+        }
+        
+    }
+
+    private GetStateResult getState (UUID orgPPAGuid, Map<String, Object> r) throws GisPollRetryException, GisPollException {
+
+        GetStateResult rp;
+        
+        try {
+            rp = wsGisHouseManagementClient.getState (orgPPAGuid, (UUID) r.get ("uuid_ack"));
+        }
+        catch (Fault ex) {
+            throw new GisPollException (ex.getFaultInfo ());
+        }
+        catch (Throwable ex) {            
+            throw new GisPollException (ex);
+        }
+        
+        checkIfResponseReady (rp);
+        
+        return rp;
+        
+    }    
+    
+}
