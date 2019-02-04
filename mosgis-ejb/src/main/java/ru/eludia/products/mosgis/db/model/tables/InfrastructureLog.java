5531bfd28b3ea984ba121695c3f635f1ba81df76
Mihail Mochajlo <mihail.mochajlo@gmail.com>
2019-02-04 15:43:38 +0300

    IN PROGRESS for tasks/0da5ee87-ec9c-48da-8e1f-7294be866548

--- mosgis-ejb/src/main/java/ru/eludia/products/mosgis/db/model/tables/InfrastructureLog.java
+++ mosgis-ejb/src/main/java/ru/eludia/products/mosgis/db/model/tables/InfrastructureLog.java
@@ -4,16 +4,16 @@ import java.util.Map;
 import java.util.UUID;
 import ru.eludia.base.DB;
 import ru.eludia.base.db.sql.gen.Get;
+import ru.eludia.base.model.Table;
 import ru.eludia.products.mosgis.db.model.EnTable;
 import ru.eludia.products.mosgis.db.model.GisWsLogTable;
 import ru.eludia.products.mosgis.db.model.nsi.NsiTable;
 import ru.eludia.products.mosgis.db.model.voc.VocOrganization;
 import ru.gosuslugi.dom.schema.integration.infrastructure.ImportOKIRequest;
+import ru.gosuslugi.dom.schema.integration.infrastructure.InfrastructureType;
+import ru.gosuslugi.dom.schema.integration.nsi_base.NsiRef;
 
 public class InfrastructureLog extends GisWsLogTable {
-
-    
-
 
     public InfrastructureLog () {
         
@@ -37,18 +37,47 @@ public class InfrastructureLog extends GisWsLogTable {
     private static ImportOKIRequest.RKIItem.OKI toOKI (Map<String, Object> r) {
         final ImportOKIRequest.RKIItem.OKI result = DB.to.javaBean (ImportOKIRequest.RKIItem.OKI.class, r);
         result.setBase (NsiTable.toDom (r, "vc_nsi_39"));
+        result.setOKIType(toOKIType (r));
+        if ((long) r.get ("indefinitemanagement") == 1) result.setEndManagmentDate(null);
+        else result.setIndefiniteManagement(null);
+        return result;
+    }
+    
+    private static InfrastructureType.OKIType toOKIType(Map<String, Object> r) {
+        final ImportOKIRequest.RKIItem.OKI.OKIType result = DB.to.javaBean (ImportOKIRequest.RKIItem.OKI.OKIType.class, r);
+        NsiRef nsi_ref_33 = NsiTable.toDom (r, "vc_nsi_33");
+        result.setCode (nsi_ref_33.getCode ());
+        result.setGUID (nsi_ref_33.getGUID ());
+        result.setName (r.get ("vc_nsi_33").toString ());
+        if (r.containsKey ("vc_nsi_37")) result.setESubstationType (NsiTable.toDom (r, "vc_nsi_37"));
+        if (r.containsKey ("vc_nsi_38")) result.setPowerPlantType  (NsiTable.toDom (r, "vc_nsi_38"));
+        if (r.containsKey ("vc_nsi_34")) result.setWaterIntakeType (NsiTable.toDom (r, "vc_nsi_34"));
+        if (r.containsKey ("vc_nsi_40")) result.setFuelType        (NsiTable.toDom (r, "vc_nsi_40"));
+        if (r.containsKey ("vc_nsi_35")) result.setGasNetworkType  (NsiTable.toDom (r, "vc_nsi_35"));
         return result;
     }
     
     public Get getForExport (String id) {
         
+        final NsiTable nsi_33 = NsiTable.getNsiTable (33);
+        final NsiTable nsi_34 = NsiTable.getNsiTable (34);
+        final NsiTable nsi_35 = NsiTable.getNsiTable (35);
+        final NsiTable nsi_37 = NsiTable.getNsiTable (37);
+        final VocNsi38 nsi_38 = (VocNsi38) getModel ().get (VocNsi38.class);
         final NsiTable nsi_39 = NsiTable.getNsiTable (39);
+        final VocNsi40 nsi_40 = (VocNsi40) getModel ().get (VocNsi40.class);
         
         return (Get) getModel ()
-            .get   (this, id, "*")
-            .toOne (Infrastructure.class, "AS r", Infrastructure.c.ID_IS_STATUS.lc ()).on ()
-            .toOne (VocOrganization.class, "AS org", "orgppaguid").on ()
-            .toOne (nsi_39, nsi_39.getLabelField ().getfName () + " AS vc_nsi_39", "code", "guid").on ("(r.code_vc_nsi_39 = vc_nsi_39.code AND vc_nsi_39.isactual=1)");
+            .get        (this, id, "*")
+            .toOne      (Infrastructure.class, "AS r", Infrastructure.c.ID_IS_STATUS.lc ()).on ()
+            .toOne      (VocOrganization.class, "AS org", "orgppaguid").on ()
+            .toOne      (nsi_33, nsi_33.getLabelField ().getfName () + " AS vc_nsi_33", "code", "guid").on ("(r.code_vc_nsi_33 = vc_nsi_33.code AND vc_nsi_33.isactual=1)")
+            .toOne      (nsi_39, nsi_39.getLabelField ().getfName () + " AS vc_nsi_39", "code", "guid").on ("(r.code_vc_nsi_39 = vc_nsi_39.code AND vc_nsi_39.isactual=1)")
+            .toMaybeOne (nsi_37, nsi_37.getLabelField ().getfName () + " AS vc_nsi_37", "code", "guid").on ("(r.code_vc_nsi_37 = vc_nsi_37.code AND vc_nsi_37.isactual=1)")
+            .toMaybeOne (nsi_38, "label AS vc_nsi_38", "code", "guid").on ("(r.code_vc_nsi_38 = vc_nsi_38.code AND vc_nsi_38.isactual=1)")
+            .toMaybeOne (nsi_34, nsi_34.getLabelField ().getfName () + " AS vc_nsi_34", "code", "guid").on ("(r.code_vc_nsi_34 = vc_nsi_34.code AND vc_nsi_34.isactual=1)")
+            .toMaybeOne (nsi_40, "label AS vc_nsi_40", "code", "guid").on ("(r.code_vc_nsi_40 = vc_nsi_40.code AND vc_nsi_40.isactual=1)")
+            .toMaybeOne (nsi_35, nsi_35.getLabelField ().getfName () + " AS vc_nsi_35", "code", "guid").on ("(r.code_vc_nsi_35 = vc_nsi_35.code AND vc_nsi_35.isactual=1)");
     }
     
 }
--- mosgis-ejb/src/main/java/ru/eludia/products/mosgis/db/model/tables/VocNsi40.java
+++ mosgis-ejb/src/main/java/ru/eludia/products/mosgis/db/model/tables/VocNsi40.java
@@ -9,6 +9,7 @@ import ru.eludia.base.model.Ref;
 import ru.eludia.base.model.Type;
 import static ru.eludia.base.model.Type.BOOLEAN;
 import static ru.eludia.base.model.Type.STRING;
+import static ru.eludia.base.model.Type.UUID;
 import ru.eludia.base.model.def.Virt;
 import ru.eludia.products.mosgis.db.model.EnColEnum;
 import ru.eludia.products.mosgis.db.model.EnTable;
@@ -24,6 +25,8 @@ public class VocNsi40 extends EnTable {
         F_0EF1925149 (STRING, null, "Наименование топлива"),
         
         LABEL        (STRING, new Virt ("DECODE (\"F_0EF1925149\", NULL, \"F_8BB30AD91A\", \"F_8BB30AD91A\"||' ('|| \"F_0EF1925149\" ||')')"), "Полное наименование топлива"),
+        
+        GUID         (UUID, "Глобально-уникальный идентификатор элемента справочника"),
         
         ISACTUAL     (BOOLEAN, "Актуально")
         
