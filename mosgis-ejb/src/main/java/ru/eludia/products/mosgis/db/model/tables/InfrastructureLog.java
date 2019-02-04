281224ca59979fe45ea617591f7f649c6ca11f57
Mihail Mochajlo <mihail.mochajlo@gmail.com>
2019-02-04 12:18:22 +0300

    toImportOkiRequest added in log

--- mosgis-ejb/src/main/java/ru/eludia/products/mosgis/db/model/tables/InfrastructureLog.java
+++ mosgis-ejb/src/main/java/ru/eludia/products/mosgis/db/model/tables/InfrastructureLog.java
@@ -1,14 +1,21 @@
 package ru.eludia.products.mosgis.db.model.tables;
 
+import java.util.Map;
 import ru.eludia.products.mosgis.db.model.EnTable;
-import ru.eludia.products.mosgis.db.model.LogTable;
+import ru.eludia.products.mosgis.db.model.GisWsLogTable;
+import ru.gosuslugi.dom.schema.integration.infrastructure.ImportOKIRequest;
 
-public class InfrastructureLog extends LogTable {
-    
+public class InfrastructureLog extends GisWsLogTable {
+
     public InfrastructureLog () {
         
         super ("tb_infrastructures__log", "История редактирования объектов коммунальной инфраструктуры", Infrastructure.class, EnTable.c.class, Infrastructure.c.class);
         
+    }
+    
+    public static ImportOKIRequest toImportOKIRequest (Map<String, Object> r) {
+        final ImportOKIRequest result = new ImportOKIRequest ();
+        return result;            
     }
     
 }
