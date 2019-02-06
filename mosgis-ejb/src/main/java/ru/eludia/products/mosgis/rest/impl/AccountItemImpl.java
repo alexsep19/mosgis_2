cecfd64b1d6a3880fcb7949d52bfda38ba6136d8
Dmitry Ovsyanko <do_@pochta.ru>
2019-02-06 17:27:18 +0300

    acc item rest tasks/ac8f4f58-c93e-4443-8791-0f2c7131532d

---
+++ mosgis-ejb/src/main/java/ru/eludia/products/mosgis/rest/impl/AccountItemImpl.java
@@ -0,0 +1,52 @@
+package ru.eludia.products.mosgis.rest.impl;
+
+import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+import javax.json.JsonObject;
+import ru.eludia.base.db.sql.gen.Select;
+import ru.eludia.products.mosgis.db.model.tables.AccountItem;
+import ru.eludia.products.mosgis.db.model.voc.VocBuilding;
+import ru.eludia.products.mosgis.ejb.ModelHolder;
+import ru.eludia.products.mosgis.rest.User;
+import ru.eludia.products.mosgis.rest.api.AccountItemLocal;
+import ru.eludia.products.mosgis.rest.impl.base.BaseCRUD;
+
+@Stateless
+@TransactionAttribute(TransactionAttributeType.NOT_SUPPORTED)
+public class AccountItemImpl extends BaseCRUD<AccountItem> implements AccountItemLocal {
+
+    private void checkFilter (JsonObject data, AccountItem.c field, Select select) {
+        String key = field.lc ();
+        String value = data.getString (key, null);
+        if (value == null) return;
+        select.and (field, value);
+    }
+
+    @Override
+    public JsonObject select (JsonObject p, User user) {return fetchData ((db, job) -> {                
+
+        Select select = ModelHolder.getModel ().select (getTable (), "AS root", "*", "uuid AS id")
+//            .toMaybeOne (AccountItemLog.class               ).on ()
+            .toOne (VocBuilding.class, "AS addr", "label").on ()
+            .orderBy ("addr.label")
+            .limit (p.getInt ("offset"), p.getInt ("limit"));
+        
+        JsonObject data = p.getJsonObject ("data");
+        
+        checkFilter (data, AccountItem.c.UUID_ACCOUNT, select);
+
+        db.addJsonArrayCnt (job, select);
+
+    });}
+
+    @Override
+    public JsonObject getItem (String id) {return fetchData ((db, job) -> {
+
+        job.add ("item", db.getJsonObject (ModelHolder.getModel ()
+            .get (getTable (), id, "AS root", "*")
+        ));
+        
+    });}
+    
+}
