b3d91cde6cac89ad9dc0f3620edcf2d3ef8b7b5b
Mihail Mochajlo <mihail.mochajlo@gmail.com>
2019-02-07 17:14:29 +0300

    infrastructure update validation for tasks/0da5ee87-ec9c-48da-8e1f-7294be866548

--- mosgis-ejb/src/main/java/ru/eludia/products/mosgis/db/model/tables/InfrastructureNsi3.java
+++ mosgis-ejb/src/main/java/ru/eludia/products/mosgis/db/model/tables/InfrastructureNsi3.java
@@ -11,6 +11,29 @@ public class InfrastructureNsi3 extends Table {
         
         pkref ("uuid",       Infrastructure.class,  "Объект коммунальной инфраструктуры");
         pk    ("code",       Type.STRING,  20,      "Ссылка на НСИ \"Вид коммунальной услуги\" (НСИ 3)");
+        
+        trigger ("BEFORE INSERT OR UPDATE OR DELETE", ""
+                + "DECLARE "
+                    + "cnt NUMBER; "
+                + "BEGIN "
+                    + "SELECT COUNT(*) INTO cnt FROM tb_infrastructures infrastructure INNER JOIN vc_nsi_33 nsi33 ON (nsi33.code = infrastructure.code_vc_nsi_33 AND nsi33.is_object = 1); "
+                    + "IF cnt > 0 THEN BEGIN "
+                        + "SELECT COUNT(*) INTO cnt FROM tb_oki_resources res WHERE res.uuid_oki = :OLD.uuid; "
+                        + "IF cnt > 0 THEN "
+                            + "raise_application_error (-20000, 'К данному объекту привязаны характеристики мощностей объекта. Операция отменена'); "
+                        + "END IF; END; "
+                    + "ELSE BEGIN "
+                        + "SELECT COUNT(*) INTO cnt FROM tb_oki_tr_resources res WHERE res.uuid_oki = :OLD.uuid; "
+                        + "IF cnt > 0 THEN "
+                            + "raise_application_error (-20000, 'К данному объекту привязаны характеристики передачи коммунальных ресурсов. Операция отменена'); "
+                        + "END IF; "
+                        + "SELECT COUNT(*) INTO cnt FROM tb_oki_net_pieces net WHERE net.uuid_oki = :OLD.uuid; "
+                        + "IF cnt > 0 THEN "
+                            + "raise_application_error (-20000, 'К данному объекту привязаны участки сети. Операция отменена'); "
+                        + "END IF; "
+                    + "END; END IF; "
+                + "END; "
+        );
 
     }
 
