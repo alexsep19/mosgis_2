a8983b87435485f25d1a080368f764264f646140
Dmitry Ovsyanko <do_@pochta.ru>
2019-02-13 10:33:10 +0300

    md db tasks/e5d27ad9-ae00-432b-905d-d427abaf06e4

---
+++ mosgis-ejb/src/main/java/ru/eludia/products/mosgis/db/model/tables/MeteringDevice.java
@@ -0,0 +1,67 @@
+package ru.eludia.products.mosgis.db.model.tables;
+
+import ru.eludia.base.model.Col;
+import ru.eludia.base.model.Ref;
+import ru.eludia.base.model.Type;
+import ru.eludia.products.mosgis.db.model.EnColEnum;
+import ru.eludia.products.mosgis.db.model.EnTable;
+import ru.eludia.products.mosgis.db.model.voc.VocBuilding;
+import ru.eludia.products.mosgis.db.model.voc.VocGisStatus;
+import ru.eludia.products.mosgis.db.model.voc.VocOrganization;
+
+public class MeteringDevice extends EnTable {
+    
+    public enum c implements EnColEnum {
+        
+        UUID_ORG               (VocOrganization.class,                          "Организация, которая завела данный прибор в БД"),
+	FIASHOUSEGUID          (VocBuilding.class,                              "Глобальный уникальный идентификатор дома по ФИАС"),
+        
+        METERINGDEVICENUMBER   (Type.STRING,  50,                               "Заводской (серийный) номер ПУ"),
+        METERINGDEVICESTAMP    (Type.STRING,  100,                              "Марка ПУ"),
+        METERINGDEVICEMODEL    (Type.STRING,  100,                              "Модель ПУ"),
+        
+        INSTALLATIONDATE       (Type.DATE,           null,                      "Дата установки"),
+        COMMISSIONINGDATE      (Type.DATE,           null,                      "Дата ввода в эксплуатацию"),
+        
+        REMOTEMETERINGMODE     (Type.BOOLEAN,  Boolean.FALSE,                   "Наличие возможности дистанционного снятия показаний"),
+        REMOTEMETERINGINFO     (Type.STRING,  2000,                             "Информация о наличии возможности дистанционного снятия показаний ПУ указанием наименования установленной системы"),
+        
+        FIRSTVERIFICATIONDATE  (Type.DATE,           null,                      "Дата последней поверки"),
+
+        CODE_VC_NSI_16         (Type.STRING,  20,    null,                      "Межповерочный интервал (НСИ 16)"),
+
+        FACTORYSEALDATE        (Type.DATE,           null,                      "Дата опломбирования ПУ заводом-изготовителем"),
+
+        TEMPERATURESENSOR      (Type.BOOLEAN,  Boolean.FALSE,                   "Наличие датчиков температры"),
+        PRESSURESENSOR         (Type.BOOLEAN,  Boolean.FALSE,                   "Наличие датчиков давления"),
+        CONSUMEDVOLUME         (Type.BOOLEAN,  Boolean.FALSE,                   "ПУ предоставляет объем потребленного КР"),
+        
+        ID_CTR_STATUS          (VocGisStatus.class,    VocGisStatus.DEFAULT,    "Статус с точки зрения mosgis"),
+        ID_CTR_STATUS_GIS      (VocGisStatus.class,    VocGisStatus.DEFAULT,    "Статус с точки зрения ГИС ЖКХ"),
+
+///        ID_LOG                 (MeteringDeviceLog.class,                     "Последнее событие редактирования"),
+
+        ;
+
+        @Override public Col getCol () {return col;} private Col col; private c (Type type, Object... p) {col = new Col (this, type, p);} private c (Class c,   Object... p) {col = new Ref (this, c, p);}        
+        @Override
+        public boolean isLoggable () {
+            switch (this) {
+                case FIASHOUSEGUID:
+                case UUID_ORG:
+//                case ID_LOG:
+                    return false;
+                default:
+                    return true;
+            }
+        }        
+
+    }
+
+    public MeteringDevice () {        
+        super  ("tb_meters", "Приборы учёта");        
+        cols   (c.class);        
+        key    ("fiashouseguid", "fiashouseguid");        
+    }    
+
+}
