e47b3b62da8a0ff593b8d937c47d6e1b547ac148
Dmitry Ovsyanko <do_@pochta.ru>
2019-02-18 10:53:00 +0300

    md IN PROGRESS tasks/e5d27ad9-ae00-432b-905d-d427abaf06e4

--- mosgis-web/src/main/webapp/_/app/js/data/metering_device_new.js
+++ mosgis-web/src/main/webapp/_/app/js/data/metering_device_new.js
@@ -49,6 +49,79 @@ define ([], function () {
         }
 */        
     }
+    
+    function sample_by_premise (types, vw_premises) {
+    
+        var tmp = types.filter (function (i) {return i.clazz})
+        
+        var result = []
+
+        $.each (tmp, function () {
+
+            var t = this
+
+            $.each (vw_premises, function () {
+
+                if (this.class != t.clazz) return
+
+                if (t.code_vc_nsi_30 != this.code_vc_nsi_30) return
+
+                if (t.code_vc_nsi_30)
+
+                result.push ({
+                    id: t.id + '_' + this.id,
+                    label: t.label + ' ' + this.label
+                })
+
+            })          
+
+        }) 
+                
+        return result
+    
+    }
+    
+    function load_ctr_nsi2_and_show_popup (data, done) {    
+    
+        query ({type: 'contract_object_services', id: null}, {limit:10000, offset:0, search: [{field: "uuid_contract_object", operator: "is", value: data.cach.uuid}]}, function (d) {
+        
+            var mask = 0
+        
+            $.each (d.root, function () {
+                mask |= [1 << (this.code_vc_nsi_2 - 1)]
+            })
+            
+            data.vc_nsi_2.items = data.vc_nsi_2.items.filter (function (i) {return i.id & mask})
+
+            done (data)
+
+        }) 
+
+    }
+    
+    function load_ca_nsi2_and_show_popup (data, done) {
+
+        done (data)
+
+    }
+    
+    function load_nsi2_and_show_popup (data, done) {
+
+        $.each (data.types, function () {this.text = this.label})
+                
+        if (data.types.length) data.record.id_type = data.types [0].id
+        
+        var cach = data.cach
+        
+        if (!cach) die ('foo', 'Не найден объект управления для данного дома')
+        
+        if (cach ['ctr.uuid']) return load_ctr_nsi2_and_show_popup (data, done)
+        
+        if (cach ['ca.uuid']) return load_ca_nsi2_and_show_popup (data, done)
+        
+        if (!cach) die ('foo', 'Не опознан объект управления для данного дома')
+                            
+    }
 
     return function (done) {
 
@@ -60,80 +133,17 @@ define ([], function () {
 
         query ({type: 'metering_devices', id: null, part: 'vocs'}, {}, function (d) {
         
-            var types = d.vc_meter_types
-                .filter (function (i) {return i.is_condo == it.is_condo})
+            data.types = d.vc_meter_types.filter (function (i) {return i.is_condo == it.is_condo})
                 
-            add_vocabularies (d, d)
-            
-            for (k in d) data [k] = d [k]
-                
-            function go () {
-                $.each (types, function () {this.text = this.label})
-darn (types)                
-                data.types = types
-                if (types.length) data.record.id_type = types [0].id
-                done (data)            
-            }
-            
-            if (!it.is_condo) return go ()
-/*
-            var classes = null; 
-            
-            $.each (types, function () {
-                var c = this.clazz
-                if (!c) return
-                if (!classes) classes = {}
-                classes [c] = 0
-            })                        
-*/            
+            add_vocabularies (d, d); for (k in d) data [k] = d [k]
+                            
+            if (!it.is_condo) return load_nsi2_and_show_popup (data, done)
             
             query ({type: 'premises', id: null}, {data: {uuid_house: $_REQUEST.id}}, function (dd) {
-            
-                var tmp = types.filter (function (i) {return i.clazz})
-                
-                types = types.filter (function (i) {return !i.clazz})
-                
-                $.each (tmp, function () {
-                
-                    var t = this
-
-                    $.each (dd.vw_premises, function () {
-                    
-                        if (this.class != t.clazz) return
-                                                
-                        if (t.code_vc_nsi_30 != this.code_vc_nsi_30) return
-                        
-                        if (t.code_vc_nsi_30)
-                        
-                        types.push ({
-                            id: t.id + '_' + this.id,
-                            label: t.label + ' ' + this.label
-                        })
-
-                    })          
-                
-                }) 
-            
-/*            
-
-                $.each (dd.vw_premises, function () {
-                    var c = this.class
-darn (c)                    
-                    if (!c) return
-                    if (!(c in classes)) return
-                    classes [c] ++
-                })          
-                
-                types = types
-                    .filter (function (i) {
-                        var c = i.clazz
-                        return !c || classes [c]
-                    })
-                
-darn (classes)                
-*/
-
-                go ()
+                            
+                data.types = data.types.filter (function (i) {return !i.clazz}).concat (sample_by_premise (data.types, dd.vw_premises))
+                            
+                load_nsi2_and_show_popup (data, done)
             
             })                     
 
