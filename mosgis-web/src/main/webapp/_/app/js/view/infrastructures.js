a5c0f9c1f97f46380dd412b8999d5163bc0edc9f
Mihail Mochajlo <mihail.mochajlo@gmail.com>
2019-03-04 13:56:30 +0300

    remove Status field for tasks/c7c94c5e-7e19-4b54-835c-fd8fcfb59c0c

--- mosgis-web/src/main/webapp/_/app/js/view/infrastructures.js
+++ mosgis-web/src/main/webapp/_/app/js/view/infrastructures.js
@@ -58,7 +58,6 @@ define ([], function () {
                 {field: 'manageroki_label', caption: 'Правообладатель', size: 30},
                 {filed: 'endmanagmentdate', caption: 'Окончание управления', size: 18, render: _dt},
                 {field: 'uniquenumber', caption: 'Реестровый номер ГИС ЖКХ', size: 20},
-                {field: 'id_is_status', caption: 'Статус', size: 15, voc: data.vc_gis_status},
                 {field: 'id_is_status_gis', caption: 'Статус в ГИС ЖКХ', size: 15, voc: data.vc_gis_status},
             ],
 
