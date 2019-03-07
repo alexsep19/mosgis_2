ed7fc0f9582bdb54b99b6325b9f84e831b3d1ae6
Dmitry Ovsyanko <do_@pochta.ru>
2019-03-07 10:57:44 +0300

    ctr bank acc db refactoring tasks/4290f305-ca25-49bc-ba60-321dc1de0da2

--- mosgis-ejb/src/main/java/ru/eludia/products/mosgis/db/model/tables/MgmtContract.java
+++ mosgis-ejb/src/main/java/ru/eludia/products/mosgis/db/model/tables/MgmtContract.java
@@ -1,93 +1,41 @@
 package ru.eludia.products.mosgis.db.model.tables;
 
 import ru.eludia.base.model.View;
-import ru.eludia.base.model.Type;
+import ru.eludia.products.mosgis.db.model.EnTable;
 import ru.eludia.products.mosgis.db.model.voc.VocGisContractType;
-import ru.eludia.products.mosgis.db.model.voc.VocGisCustomerType;
 import ru.eludia.products.mosgis.db.model.voc.VocGisStatus;
-import ru.eludia.products.mosgis.db.model.voc.VocOrganization;
 
 public class MgmtContract extends View {
 
     public MgmtContract () {
         
-        super  ("tb_mgmt_contracts", "Страховые компании");
-
-        pk    ("uuid",                      Type.UUID,              "Ключ");
-        col   ("is_deleted",                Type.BOOLEAN,                    "1, если запись удалена; иначе 0");
-
-        fk    ("uuid_org",                  VocOrganization.class,                      "Договородержатель");
-        fk    ("uuid_org_customer",         VocOrganization.class,                      "Контрагент");
-        
-        fk    ("id_customer_type",          VocGisCustomerType.class,                 "Тип контрагента");
-        
-        fk    ("id_ctr_status",             VocGisStatus.class,          "Статус договора с точки зрения mosgis");
-        fk    ("id_ctr_status_gis",         VocGisStatus.class,          "Статус договора с точки зрения ГИС ЖКХ");
-        fk    ("id_ctr_state",              VocGisStatus.class,          "Состояние договора с точки зрения mosgis");
-        fk    ("id_ctr_state_gis",          VocGisStatus.class,          "Состояние договора с точки зрения ГИС ЖКХ");
-        
-        col   ("docnum",                    Type.STRING,                    "Номер договора");
-        col   ("signingdate",               Type.DATE,                          "Дата заключения");
-        col   ("effectivedate",             Type.DATE,                          "Дата вступления в силу");
-        col   ("plandatecomptetion",        Type.DATE,                          "Планируемая дата окончания");
-
-        col   ("automaticrolloveroneyear",  Type.BOOLEAN,            "1, если запись удалена; иначе 0");
- 
-        col   ("contractbase",              Type.STRING,  "Основание заключения договора");
-        col   ("code_vc_nsi_58",            Type.STRING,           20,   null,  "Ссылка на НСИ \"Основание заключения договора\" (реестровый номер 58)");
-
-        col   ("ddt_m_start",               Type.NUMERIC,          2,   null,  "Начало периода ввода показаний ПУ (1..31 — конкретное число; 99 — последнее число)");
-        col   ("ddt_m_start_nxt",           Type.BOOLEAN,          null, "1, если начало периода ввода показаний ПУ в следующем месяце; иначе 0");
-        col   ("ddt_m_end",                 Type.NUMERIC,          2,   null,  "Окончание периода ввода показаний ПУ (1..31 — конкретное число; 99 — последнее число)");
-        col   ("ddt_m_end_nxt",             Type.BOOLEAN,          null, "1, если окончание периода ввода показаний ПУ в следующем месяце; иначе 0");
-        col   ("ddt_d_start",               Type.NUMERIC,          2,   null,  "Срок представления (выставления) платежных документов для внесения платы за жилое помещение и (или) коммунальные услуги (1..30 — конкретное число; 99 — последнее число)");
-        col   ("ddt_d_start_nxt",           Type.BOOLEAN,          null, "1, если срок представления (выставления) платежных документов для внесения платы за жилое помещение и (или) коммунальные услуги в следующем месяце; иначе 0");
-        col   ("ddt_i_start",               Type.NUMERIC,          2,   null,  "Срок внесения платы за жилое помещение и (или) коммунальные услуги (1..30 — конкретное число; 99 — последнее число)");
-        col   ("ddt_i_start_nxt",           Type.BOOLEAN,          null, "1, если срок внесения платы за жилое помещение и (или) коммунальные услуги в следующем месяце; иначе 0");
-
-        fk    ("uuid_out_soap",             OutSoap.class,             null,    "Последний запрос на импорт в ГИС ЖКХ");
-        col   ("contractguid",              Type.UUID,                 null,    "UUID договора в ГИС ЖКХ");
-
-        fk    ("id_log",                    ContractLog.class,         null, "Последнее событие редактирования");
-        col   ("versionnumber",             Type.INTEGER,          10, null,    "Номер версии (по состоянию в ГИС ЖКХ)");
-        col   ("reasonofannulment",         Type.STRING,         1000, null,    "Причина аннулирования");
+        super ("tb_mgmt_contracts", "Договоры управления");        
+        cols  (EnTable.c.class);
+        cols  (Contract.c.class);
+        pk    (EnTable.c.UUID);
+        fk    ("id_ctr_state", VocGisStatus.class, "Состояние договора с точки зрения mosgis");
         
     }
 
     @Override
     public final String getSQL () {
+        
+        StringBuilder sb = new StringBuilder ("SELECT ");
+        
+        for (EnTable.c i: EnTable.c.values ()) {
+            sb.append ("t.");
+            sb.append (i.lc ());
+            sb.append (',');
+        }
+        
+        for (Contract.c i: Contract.c.values ()) {
+            sb.append ("t.");
+            sb.append (i.lc ());
+            sb.append (',');
+        }
 
-        return "SELECT " +
-            "t.uuid" +
-            ", t.is_deleted" +
-            ", t.uuid_org" +
-            ", t.uuid_org_customer" +
-            ", t.id_customer_type" +
-            ", t.id_ctr_status" +
-            ", t.id_ctr_status_gis" +
-            ", t.docnum" +
-            ", t.signingdate" +
-            ", t.effectivedate" +
-            ", t.plandatecomptetion" +
-            ", t.automaticrolloveroneyear" +
-            ", t.code_vc_nsi_58 contractbase" +
-            ", t.code_vc_nsi_58" +
-            ", t.contractguid"+
-            ", t.ddt_d_start"+
-            ", t.ddt_d_start_nxt"+
-            ", t.ddt_i_start"+
-            ", t.ddt_i_start_nxt"+
-            ", t.ddt_m_end"+
-            ", t.ddt_m_end_nxt"+
-            ", t.ddt_m_start"+
-            ", t.ddt_m_start_nxt"+
-            ", t.id_contract_type"+
-            ", t.id_ctr_state_gis"+
-            ", t.id_log"+
-            ", t.uuid_out_soap" +
-            ", t.versionnumber" +
-            ", t.reasonofannulment" +
-            ", CASE" +
+        return sb +
+            "CASE" +
             "    WHEN ID_CTR_STATUS IN (100, 110) THEN 80 " +
             "    WHEN UUID_OUT_SOAP IS NULL THEN 80 " +
             "    WHEN PLANDATECOMPTETION < TRUNC (SYSDATE) THEN 60" +
