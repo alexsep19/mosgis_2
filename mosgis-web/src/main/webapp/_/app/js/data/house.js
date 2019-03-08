759de5d30815ccd237e291f47a5a92b6ec8999d3
Dmitry Ovsyanko <do_@pochta.ru>
2019-03-08 17:53:00 +0300

    sr ctr acc tasks/7eec626b-c9da-4b86-8787-f5f45f5cff45

--- mosgis-web/src/main/webapp/_/app/js/data/house.js
+++ mosgis-web/src/main/webapp/_/app/js/data/house.js
@@ -86,7 +86,10 @@ define ([], function () {
 
             }
 
+            var is_own_srca = data.srca && data.srca.uuid_org == $_USER.uuid_org
+
             data.is_passport_editable = editable ()
+            data.is_premise_editable = data.is_passport_editable || is_own_srca
 
             data.non_editable_fields = [
                 '20168',
