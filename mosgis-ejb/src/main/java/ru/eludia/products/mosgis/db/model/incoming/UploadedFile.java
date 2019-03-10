db904e73ad68bcc5fcf908aae54654a5ce094fe1
Dmitry Ovsyanko <do_@pochta.ru>
2019-03-10 13:52:38 +0300

    file svc storing to db

---
+++ mosgis-ejb/src/main/java/ru/eludia/products/mosgis/db/model/incoming/UploadedFile.java
@@ -0,0 +1,32 @@
+package ru.eludia.products.mosgis.db.model.incoming;
+
+import ru.eludia.base.model.Col;
+import ru.eludia.base.model.ColEnum;
+import ru.eludia.base.model.Ref;
+import ru.eludia.base.model.Type;
+import ru.eludia.products.mosgis.db.model.EnTable;
+import static ru.eludia.base.model.def.Blob.EMPTY_BLOB;
+
+public class UploadedFile extends EnTable {
+    
+    public enum c implements ColEnum {
+
+        LABEL          (Type.STRING, 1024,                  "Имя файла"),
+//        LEN            (Type.INTEGER,                       "Размер"),
+        BODY           (Type.BLOB,              EMPTY_BLOB, "Содержимое"),
+        ;
+
+        @Override
+        public Col getCol () {return col;}
+        private Col col;        
+        private c (Type type, Object... p) {col = new Col (this, type, p);}
+        private c (Class c,   Object... p) {col = new Ref (this, c, p);}
+
+    }
+
+    public UploadedFile () {
+        super ("in_upl_files", "Файлы, загруженные через REST-сервис");
+        cols  (c.class);
+    }
+    
+}
--- mosgis-ejb/src/main/java/ru/eludia/products/mosgis/ws/rest/impl/filestore/FileStoreImpl.java
+++ mosgis-ejb/src/main/java/ru/eludia/products/mosgis/ws/rest/impl/filestore/FileStoreImpl.java
@@ -1,10 +1,18 @@
 package ru.eludia.products.mosgis.ws.rest.impl.filestore;
 
 import java.io.InputStream;
+import java.io.OutputStream;
+import java.sql.Blob;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.UUID;
 import javax.ejb.Stateless;
 import javax.ejb.TransactionAttribute;
 import javax.ejb.TransactionAttributeType;
+import ru.eludia.base.DB;
+import ru.eludia.products.mosgis.db.ModelHolder;
+import ru.eludia.products.mosgis.db.model.MosGisModel;
+import ru.eludia.products.mosgis.db.model.incoming.UploadedFile;
 import ru.eludia.products.mosgis.filestore.FileStoreLocal;
 
 @Stateless
@@ -12,8 +20,49 @@ import ru.eludia.products.mosgis.filestore.FileStoreLocal;
 public class FileStoreImpl implements FileStoreLocal {
 
     @Override
-    public UUID store (String name, InputStream is) {
-        return UUID.randomUUID ();
+    public UUID store (String name, InputStream in) {
+        
+        final MosGisModel m = ModelHolder.getModel ();
+        
+        try (DB db = m.getDb ()) {            
+
+            db.begin ();
+            
+            UUID uuid = (UUID) db.insertId (UploadedFile.class, DB.HASH (
+                UploadedFile.c.LABEL, name
+            ));
+            
+            try (PreparedStatement st = db.getConnection ().prepareStatement ("SELECT body FROM " + m.getName (UploadedFile.class) + " WHERE uuid = ? FOR UPDATE")) {
+
+                st.setString (1, uuid.toString ().replace ("-", "").toUpperCase ());
+
+                try (ResultSet rs = st.executeQuery ()) {
+
+                    if (rs.next ()) {
+
+                        Blob blob = rs.getBlob (1);
+                        byte [] buffer = new byte [8 * 1024];
+                        int len;
+
+                        try (OutputStream out = blob.setBinaryStream (1)) {
+                            while ((len = in.read (buffer)) > 0) out.write (buffer, 0, len);                        
+                        }
+
+                    }
+
+                }
+
+            }                    
+                        
+            db.commit ();
+        
+            return uuid;
+
+        }
+        catch (Exception ex) {
+            throw new IllegalStateException (ex);
+        }        
+              
     }
     
 }
