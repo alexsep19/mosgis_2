09bd217eabb893db1d67a0cb66584be9647afa00
Dmitry Ovsyanko <do_@pochta.ru>
2019-03-13 08:19:08 +0300

    RegOrgServiceCommonAsync test tasks/86e0c298-c218-4c8a-b70a-5b34c76237b0

--- mosgis-ejb/src/test/java/ru/eludia/products/mosgis/jms/ws/ExportOrgRegistryTest.java
+++ mosgis-ejb/src/test/java/ru/eludia/products/mosgis/jms/ws/ExportOrgRegistryTest.java
@@ -6,12 +6,13 @@ import ru.eludia.base.DB;
 import ru.eludia.products.mosgis.db.model.tables.base.BaseTest;
 import ru.gosuslugi.dom.schema.integration.base.BaseAsyncResponseType;
 import ru.gosuslugi.dom.schema.integration.organizations_registry_common.ExportOrgRegistryRequest;
+import ru.gosuslugi.dom.schema.integration.organizations_registry_common.GetStateResult;
 
 
 public class ExportOrgRegistryTest extends BaseTest {
     
     public ExportOrgRegistryTest () throws Exception {
-        jc = JAXBContext.newInstance (BaseAsyncResponseType.class);
+        jc = JAXBContext.newInstance (GetStateResult.class);
     }
 
     @Test (expected = Test.None.class)
@@ -19,7 +20,8 @@ public class ExportOrgRegistryTest extends BaseTest {
         
         ExportOrgRegistryRequest exportOrgRegistryRequest = new ExportOrgRegistryRequest ();
         final ExportOrgRegistryRequest.SearchCriteria searchCriteria = new ExportOrgRegistryRequest.SearchCriteria ();
-        searchCriteria.setOGRN ("1027736001281");
+//        searchCriteria.setOGRN ("5147746267939");
+        searchCriteria.setOGRNIP ("311500135000061");
         exportOrgRegistryRequest.getSearchCriteria ().add (searchCriteria);
         
         try (DB db = model.getDb ()) {            
