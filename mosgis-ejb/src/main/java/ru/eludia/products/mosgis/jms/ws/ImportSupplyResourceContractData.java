f40dcd889b33b09c4e91c7538eb50df7fef76df6
Dmitry Ovsyanko <do_@pochta.ru>
2019-03-18 15:04:37 +0300

    in ws sr ctr IN PROGRESS tasks/cebee63d-2bde-45d0-aef6-e2368d41a0af

--- mosgis-ejb/src/main/java/ru/eludia/products/mosgis/jms/ws/ImportSupplyResourceContractData.java
+++ mosgis-ejb/src/main/java/ru/eludia/products/mosgis/jms/ws/ImportSupplyResourceContractData.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 import java.util.logging.Level;
+import java.util.stream.Collectors;
 import javax.ejb.ActivationConfigProperty;
 import javax.ejb.MessageDriven;
 import javax.json.JsonObject;
@@ -12,13 +13,11 @@ import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import ru.eludia.base.DB;
 import ru.eludia.products.mosgis.db.ModelHolder;
-import ru.eludia.products.mosgis.db.model.EnTable;
 import ru.eludia.products.mosgis.db.model.MosGisModel;
 import ru.eludia.products.mosgis.db.model.tables.SupplyResourceContract;
 import ru.eludia.products.mosgis.db.model.tables.SupplyResourceContract.c;
+import ru.eludia.products.mosgis.db.model.tables.SupplyResourceContractSubject;
 import ru.eludia.products.mosgis.db.model.voc.VocAction;
-import ru.eludia.products.mosgis.db.model.voc.VocGisSupplyResourceContractCustomerType;
-import ru.eludia.products.mosgis.db.model.voc.VocPerson;
 import ru.eludia.products.mosgis.db.model.ws.WsMessages;
 import ru.eludia.products.mosgis.jms.base.WsMDB;
 import ru.eludia.products.mosgis.jms.ws.sr_ctr.CustomerSetter;
@@ -27,8 +26,6 @@ import ru.gosuslugi.dom.schema.integration.base.BaseAsyncResponseType;
 import ru.eludia.products.mosgis.ws.soap.impl.base.Fault;
 import ru.eludia.products.mosgis.ws.soap.tools.SOAPTools;
 import ru.gosuslugi.dom.schema.integration.base.CommonResultType;
-import ru.gosuslugi.dom.schema.integration.house_management.DRSOIndType;
-import ru.gosuslugi.dom.schema.integration.house_management.DRSORegOrgType;
 import ru.gosuslugi.dom.schema.integration.house_management.ImportSupplyResourceContractRequest;
 import ru.gosuslugi.dom.schema.integration.house_management.GetStateResult;
 import ru.gosuslugi.dom.schema.integration.house_management.ImportResult;
@@ -118,6 +115,36 @@ public class ImportSupplyResourceContractData extends WsMDB {
         if (i.getSupplyResourceContract () != null) return toImportSupplyResourceContract (r, contractGUID, i.getSupplyResourceContract ());
         throw new UnsupportedOperationException ("Не найден supplyResourceContract, такие запросы пока не поддерживаются");
     }
+    
+    private Map<String, Object> toMap (SupplyResourceContractType src) throws UnsupportedOperationException {
+        
+        final Map<String, Object> r;
+        
+        SupplyResourceContractType.IsNotContract isNotContract = src.getIsNotContract ();        
+        if (isNotContract != null) {
+            r = DB.to.Map (toJsonObject (isNotContract));
+            r.put (c.IS_CONTRACT.lc (), 0);
+        }
+        else {
+            throw new UnsupportedOperationException ("Не найден isNotContract, такие запросы пока не поддерживаются");
+        }
+        
+        addContractSubjects (r, src.getContractSubject ());
+        
+        return r;
+        
+    }
+    
+    private void addContractSubjects (Map<String, Object> r, List<SupplyResourceContractType.ContractSubject> contractSubject) {
+        r.put (SupplyResourceContractSubject.TABLE_NAME, contractSubject.stream ().map (t -> toMap (t)).collect (Collectors.toList ()));
+    }    
+    
+    private Map<String, Object> toMap (SupplyResourceContractType.ContractSubject cs) {
+        Map<String, Object> result = DB.to.Map (toJsonObject (cs));
+        result.put (SupplyResourceContractSubject.c.CODE_VC_NSI_3.lc (), cs.getServiceType ().getCode ());
+        result.put (SupplyResourceContractSubject.c.CODE_VC_NSI_239.lc (), cs.getMunicipalResource ().getCode ());
+        return result;
+    }
 
     private ImportResult.CommonResult.ImportSupplyResourceContract toImportSupplyResourceContract (Map<String, Object> wsr, String contractGUID, SupplyResourceContractType src) throws Exception {
         
@@ -125,20 +152,7 @@ public class ImportSupplyResourceContractData extends WsMDB {
 
         MosGisModel m = ModelHolder.getModel ();
         
-        final Map<String, Object> r;
-        
-        SupplyResourceContractType.IsNotContract isNotContract = src.getIsNotContract ();
-        if (isNotContract != null) {
-            final JsonObject jsonObject = toJsonObject (isNotContract);
-logger.info ("jsonObject=" + jsonObject);
-            r = DB.to.Map (jsonObject);
-            r.put (c.IS_CONTRACT.lc (), 0);
-        }
-        else {
-            throw new UnsupportedOperationException ("Не найден isNotContract, такие запросы пока не поддерживаются");
-        }        
-
-logger.info ("r=" + r);
+        Map<String, Object> r = toMap (src);        
 
         r.put (SupplyResourceContract.c.UUID_ORG.lc (), wsr.get (WsMessages.c.UUID_ORG.lc ()));
 
@@ -156,7 +170,15 @@ logger.info ("r=" + r);
             
             String idLog = m.createIdLogWs (db, m.get (SupplyResourceContract.class), (UUID) wsr.get ("uuid"), uuid, VocAction.i.CREATE);
             
-            result.setContractGUID (idLog);
+            result.setContractGUID (idLog);            
+            
+            List<Map<String, Object>> subjects = (List<Map<String, Object>>) r.get (SupplyResourceContractSubject.TABLE_NAME);
+            
+            for (Map<String, Object> i: subjects) {
+                
+                
+                
+            }
 
             return result;
 
