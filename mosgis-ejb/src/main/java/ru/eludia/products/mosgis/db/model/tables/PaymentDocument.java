569b23814cd3b66589a9c99ab3a7f8b729c384d2
Dmitry Ovsyanko <do_@pochta.ru>
2019-03-20 09:50:14 +0300

    pd db IN PROGRESS tasks/a82e2cb2-f1bb-4fd0-b56a-48ffd2d76d65

--- mosgis-ejb/src/main/java/ru/eludia/products/mosgis/db/model/tables/PaymentDocument.java
+++ mosgis-ejb/src/main/java/ru/eludia/products/mosgis/db/model/tables/PaymentDocument.java
@@ -6,11 +6,9 @@ import ru.eludia.base.model.Type;
 import ru.eludia.base.model.def.Virt;
 import ru.eludia.products.mosgis.db.model.EnColEnum;
 import ru.eludia.products.mosgis.db.model.EnTable;
-import ru.eludia.products.mosgis.db.model.voc.VocAccountType;
 import ru.eludia.products.mosgis.db.model.voc.VocGisStatus;
 import ru.eludia.products.mosgis.db.model.voc.VocOrganization;
 import ru.eludia.products.mosgis.db.model.voc.VocPaymentDocumentType;
-import ru.eludia.products.mosgis.db.model.voc.VocPerson;
 
 public class PaymentDocument extends EnTable {
 
@@ -18,53 +16,34 @@ public class PaymentDocument extends EnTable {
 
     public enum c implements EnColEnum {
         
-        UUID_ORG               (VocOrganization.class, null, "Организация, которая создала данный платёжный документ"),
-        UUID_ACCOUNT           (Account.class,               "Лицевой счёт"),
+        UUID_ORG                      (VocOrganization.class, null, "Организация, которая создала данный платёжный документ"),
+        UUID_ACCOUNT                  (Account.class,               "Лицевой счёт"),
 
-        ID_TYPE                (VocPaymentDocumentType.class, VocPaymentDocumentType.DEFAULT,  "Тип ЛС"),
+        ID_TYPE                       (VocPaymentDocumentType.class, VocPaymentDocumentType.DEFAULT,  "Тип ЛС"),
 
-        YEAR                   (Type.NUMERIC, 4,             "Год"),
-        MONTH                  (Type.NUMERIC, 4,             "Месяц"),
+        YEAR                          (Type.NUMERIC, 4,             "Год"),
+        MONTH                         (Type.NUMERIC, 4,             "Месяц"),
         
-        PAYMENTDOCUMENTNUMBER  (Type.STRING,  30,    null,   "Номер платежного документа, по которому внесена плата, присвоенный такому документу исполнителем в целях осуществления расчетов по внесению платы"),
+        PAYMENTDOCUMENTNUMBER         (Type.STRING,  30,    null,   "Номер платежного документа, по которому внесена плата, присвоенный такому документу исполнителем в целях осуществления расчетов по внесению платы"),
         
+        DEBTPREVIOUSPERIODS           (Type.NUMERIC, 10, 2, null,   "Задолженность за предыдущие периоды, руб."), 
+        ADVANCEBLLINGPERIOD           (Type.NUMERIC, 10, 2, null,   "Аванс на начало расчетного периода, руб."), 
         
-
-/*        
+        PAYMENTSTAKEN_DT              (Type.DATE,           null,   "Дата последнего учтенного платежа"),
+        PAYMENTSTAKEN                 (Type.NUMERIC,  2, 0, new Virt  ("EXTRACT(DAY FROM\"PAYMENTSTAKEN_DT\")"),   "Учтены платежи, поступившие до указанного числа расчетного периода включительно"),
         
-	UUID_CONTRACT          (Contract.class,               null, "Ссылка на договор"),
-        UUID_CHARTER           (Charter.class,                null, "Ссылка на устав"),
-        UUID_SR_CONTRACT       (SupplyResourceContract.class, null, "Ссылка на договор поставки ресурсов"),
+        TOTALPAYABLEBYCHARGEINFO      (Type.NUMERIC, 13, 2, null,   "Сумма к оплате за расчетный период по услугам, руб."),        
+        TOTALBYPENALTIESANDCOURTCOSTS (Type.NUMERIC, 13, 2, null,   "Итого к оплате по неустойкам и судебным издержкам, руб."),        
+        TOTALPAYABLEBYPD              (Type.NUMERIC, 13, 2, null,   "Итого к оплате за расчетный период всего, руб."),        
+        SUBSIDIESCOMPENSATION_        (Type.NUMERIC, 10, 2, null,   "Субсидии, компенсации и иные меры соц. поддержки граждан, руб."),               
+        TOTALPAYABLEBYPDWITH_DA       (Type.NUMERIC, 13, 2, null,   "Итого к оплате за расчетный период c учетом задолженности/переплаты, руб."),
+        PAIDCASH                      (Type.NUMERIC, 13, 2, null,   "Оплачено денежных средств, руб."),
 
-        ACCOUNTNUMBER          (Type.STRING,  30,    null,  "Причина закрытия (НСИ 22)"),
+        DATEOFLASTRECEIVEDPAYMENT     (Type.DATE,           null,   "Дата последней поступившей оплаты"),
+        LIMITINDEX                    (Type.NUMERIC,  5, 2, null,   "Предельный (максимальный) индекс изменения размера платы граждан за коммунальные услуги в муниципальном образовании, %"),
         
-        LIVINGPERSONSNUMBER    (Type.NUMERIC, 4,     null,  "Количество проживающих"),
-        TOTALSQUARE            (Type.NUMERIC, 25, 4, null,  "Общая площадь жилого помещения"),
-        RESIDENTIALSQUARE      (Type.NUMERIC, 25, 4, null,  "Жилая площадь"),
-        HEATEDAREA             (Type.NUMERIC, 25, 4, null,  "Отапливаемая площадь"),
-        
-        CODE_VC_NSI_22         (Type.STRING,  20,    null,                                  "Причина закрытия (НСИ 22)"),
-        CLOSEREASON            (Type.STRING,  20,    new Virt  ("''||\"CODE_VC_NSI_22\""),  "Причина закрытия (НСИ 22)"),
-        CLOSEDATE              (Type.DATE,           null,                                  "Дата закрытия"),
-
-        DESCRIPTION            (Type.STRING,  250,   null,                                  "Примечание (для закрытия)"),
-  
-        ISRENTER               (Type.BOOLEAN,          null,  "1, если является нанимателем; 0, если не является нанимателем"),
-        ISACCOUNTSDIVIDED      (Type.BOOLEAN,          null,  "1, если лицевые счета на помещение(я) разделены; 0, если лицевые счета на помещение(я) не разделены"),
-
-        IS_CUSTOMER_ORG        (Type.BOOLEAN,          Boolean.FALSE,  "1, если плательщик — юридическое лицо; 0, если физическое"),
-
-        UUID_ORG_CUSTOMER      (VocOrganization.class, null, "Организация. ЮЛ/ИП/ОП"),
-        UUID_PERSON_CUSTOMER   (VocPerson.class,       null, "Физическое лицо/индивидуальный предприниматель."),
-        
-        ACCOUNTGUID            (Type.UUID,             null,    "Идентификатор ЛС в ГИС ЖКХ (при обновлении данных ЛС)"),        
-
-        ID_LOG                 (AccountLog.class,                    "Последнее событие редактирования"),
-        
-*/        
-
-        ID_CTR_STATUS          (VocGisStatus.class,    VocGisStatus.DEFAULT, "Статус с точки зрения mosgis"),
-        ID_CTR_STATUS_GIS      (VocGisStatus.class,    VocGisStatus.DEFAULT, "Статус с точки зрения ГИС ЖКХ"),
+        ID_CTR_STATUS                 (VocGisStatus.class,    VocGisStatus.DEFAULT, "Статус с точки зрения mosgis"),
+        ID_CTR_STATUS_GIS             (VocGisStatus.class,    VocGisStatus.DEFAULT, "Статус с точки зрения ГИС ЖКХ"),
         
         ;
 
