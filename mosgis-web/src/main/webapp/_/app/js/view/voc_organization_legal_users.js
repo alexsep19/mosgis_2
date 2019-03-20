7079f9f872ec20a12d839c6484b4c5dfaa744189
a.solovyev <andcode@rambler.ru>
2019-03-20 16:28:43 +0300

    tasks/921be6e5-800f-4f58-8ec0-c7f19ec98d63

--- mosgis-web/src/main/webapp/_/app/js/view/voc_organization_legal_users.js
+++ mosgis-web/src/main/webapp/_/app/js/view/voc_organization_legal_users.js
@@ -1,21 +1,5 @@
 define ([], function () {
 
-    var b = ['lock', 'unlock']
-
-    function recalcToolbar (e) {e.done (function () {
-
-        var g = w2ui ['voc_organizations_grid']
-
-        var t = g.toolbar
-
-        t.disable (b[0]);
-        t.disable (b[1]);
-                
-        if (g.getSelection ().length != 1) return
-        t.enable (b [g.get (g.getSelection () [0]).is_locked])
-
-    })}
-
     return function (data, view) {
     
         $(w2ui ['topmost_layout'].el ('main')).w2regrid ({ 
@@ -30,13 +14,6 @@ define ([], function () {
                 footer: true,
             },     
             
-            toolbar: {
-                items: [
-                    {type: 'button', id: 'lock', caption: 'Заблокировать', onClick: $_DO.lock_voc_user, disabled: true, off: !$_USER.role.admin},
-                    {type: 'button', id: 'unlock', caption: 'Разблокировать', onClick: $_DO.unlock_voc_user, disabled: true, off: !$_USER.role.admin},
-                ].filter (not_off),
-            }, 
-
             searches: [            
                 {field: 'is_locked', caption: 'Блокировка', type: 'enum', options: {items: [
                     {id: "0", text: "Активные"},
@@ -62,9 +39,7 @@ define ([], function () {
             onDelete:   $_DO.delete_voc_organization_legal_users,
             
             onRefresh: function (e) {e.done (color_data_mandatory)},
-            
-            onSelect: recalcToolbar,
-            onUnselect: recalcToolbar,
+            onColumnOnOff: function (e) {e.done (color_data_mandatory)},
             
         }).refresh ();
 
--- mosgis-web/src/main/webapp/_/app/js/view/voc_users.js
+++ mosgis-web/src/main/webapp/_/app/js/view/voc_users.js
@@ -1,5 +1,21 @@
 define ([], function () {
 
+    var b = ['lock', 'unlock']
+
+    function recalcToolbar (e) {e.done (function () {
+
+        var g = w2ui ['voc_organizations_grid']
+
+        var t = g.toolbar
+
+        t.disable (b[0]);
+        t.disable (b[1]);
+                
+        if (g.getSelection ().length != 1) return
+        t.enable (b [g.get (g.getSelection () [0]).is_locked])
+
+    })}
+
     return function (data, view) {
     
         data = $('body').data ('data')
@@ -16,17 +32,29 @@ define ([], function () {
                 footer: true,
             },               
 
+            toolbar: {
+                items: [
+                    {type: 'button', id: 'lock', caption: 'Заблокировать', onClick: $_DO.lock_voc_user, disabled: true, off: !$_USER.role.admin},
+                    {type: 'button', id: 'unlock', caption: 'Разблокировать', onClick: $_DO.unlock_voc_user, disabled: true, off: !$_USER.role.admin},
+                ].filter (not_off),
+            }, 
+
             searches: [            
                 {field: 'label_uc',  caption: 'ФИО',  type: 'text'},
                 {field: 'login_uc',  caption: 'Login',  type: 'text'},
                 {field: 'code_vc_nsi_20', caption: 'Полномочия', type: 'enum', options: {items: data.vc_nsi_20.items}},
                 {field: 'uuid_org', caption: 'Организация', type: 'list', options: {items: data.vc_orgs.items}},
-            ],
+                {field: 'is_locked', caption: 'Блокировка', type: 'enum', options: {items: [
+                    {id: "0", text: "Активные"},
+                    {id: "1", text: "Заблокированные"},
+                ]}},
+            ].filter (not_off),
 
             columns: [                
                 {field: 'label', caption: 'ФИО',    size: 30},
                 {field: 'login', caption: 'login',  size: 20},
                 {field: 'vc_orgs.label', caption: 'Огранизация',  size: 100},
+                {field: 'lockreason', caption: 'Причина запрета',  size: 100, hidden: true},
             ],
             
             postData: {data: {uuid_org: $_REQUEST.id}},
@@ -48,6 +76,12 @@ define ([], function () {
             },
 
             onDelete:   $_DO.delete_voc_users,
+            
+            onRefresh: function (e) {e.done (color_data_mandatory)},
+            onColumnOnOff: function (e) {e.done (color_data_mandatory)},
+            
+            onSelect: recalcToolbar,
+            onUnselect: recalcToolbar,
 
         }).refresh ();
 
