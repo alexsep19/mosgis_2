1b87879fd3212e7ce9db3c4e122e3fce0376eb93
Dmitry Ovsyanko <do_@pochta.ru>
2019-03-22 14:22:37 +0300

    nsi337 log tab tasks/d343ebd1-2824-471f-b20c-4331b9c0f2c4

---
+++ mosgis-web/src/main/webapp/_/app/js/view/general_needs_municipal_resource_common_log.js
@@ -0,0 +1,58 @@
+define ([], function () {
+
+    return function (data, view) {
+    
+        data = $('body').data ('data')
+        
+        $(w2ui ['passport_layout'].el ('main')).w2regrid ({ 
+
+            name: 'general_needs_municipal_resource_common_log',
+
+            show: {
+                toolbar: true,
+                toolbarInput: false,
+                footer: true,
+            },     
+
+            columnGroups : [
+                {span: 3, caption: 'Событие'},
+                {span: 5, caption: 'Значения полей'},
+                {span: 3, caption: 'Запрос в ГИС ЖКХ'},
+            ], 
+            
+            columns: [                
+                {field: 'ts', caption: 'Дата/время',    size: 30, render: _ts},
+                {field: 'action', caption: 'Действие',    size: 30, voc: data.vc_actions},
+                {field: 'vc_users.label', caption: 'Оператор',    size: 30},
+
+                {field: 'parentcode',  caption: 'Раздел',     size: 20, voc: data.parents},
+                {field: 'sortorder', caption: '№п/п', size: 5},
+                {field: 'generalmunicipalresourcename', caption: 'Наименование', size: 50},
+                {field: 'code_vc_nsi_2',  caption: 'Ресурс',     size: 25, voc: data.vc_nsi_2},
+                {field: 'okei',  caption: 'Ед. изм.',     size: 10, voc: data.vc_okei},
+                
+                {field: 'soap.ts', caption: 'Отправлено',    size: 30, render: _ts, attr: 'data-ref=1'},
+                {field: 'soap.ts_rp', caption: 'Обработано',    size: 30, render: _ts, attr: 'data-ref=1'},
+                {field: 'soap.err_text', caption: 'Ошибка',    size: 30},
+
+            ],
+            
+            url: '/mosgis/_rest/?type=general_needs_municipal_resources&part=log&id=' + $_REQUEST.id,            
+            
+            onClick: function (e) {
+            
+                var c = this.columns [e.column]
+                var r = this.get (e.recid)
+                
+                switch (c.field) {
+                    case 'soap.ts':    if (r.uuid_out_soap) return openTab ('/out_soap_rq/' + r.uuid_out_soap)
+                    case 'soap.ts_rp': if (r.uuid_message) return openTab ('/out_soap_rp/' + r.uuid_message)
+                }
+            
+            }
+
+        }).refresh ();
+
+    }
+
+})
