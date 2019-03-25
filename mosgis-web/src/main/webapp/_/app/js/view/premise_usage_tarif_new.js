fad75de4d2f121f2c0418bb6df3313e9445e8fb5
Maksim Gruzdev <mgruzdev@cheby.ru>
2019-03-25 14:32:32 +0300

    tf ui new tasks/3679903e-0360-46c4-9e92-ffbb750092e3

---
+++ mosgis-web/src/main/webapp/_/app/js/view/premise_usage_tarif_new.js
@@ -0,0 +1,63 @@
+define ([], function () {
+
+    var name = 'premise_usage_tarif_new_form'
+
+    return function (data, view) {
+
+        $(fill (view, data.record)).w2uppop ({}, function () {
+
+            $('#w2ui-popup .w2ui-form').w2reform ({
+
+                name: name,
+
+                record: data.record,
+
+                fields : [
+                    {name: 'name', type: 'textarea'},
+                    {name: 'datefrom', type: 'date'},
+                    {name: 'dateto', type: 'date'},
+                    {name: 'price', type: 'float', options: {min: 0}},
+                    {name: 'oktmo', type: 'enum', hint: 'ОКТМО', options: {
+                        maxDropWidth: 800,
+                        renderItem: function (i, idx, remove) {
+                            return i.code + remove
+                        },
+                        url: '/mosgis/_rest/?type=voc_oktmo',
+                        openOnFocus: true,
+                        filter: false,
+                        cacheMax: 50,
+                        postData: {
+                            offset: 0,
+                            limit: 50
+                        },
+                        onRequest: function(e) {
+                            e.postData = {
+                                search: [
+                                    {field: 'code', operator: 'contains', value: e.postData.search}
+                                ],
+                                searchLogic: 'AND'
+                            }
+                        },
+                        onLoad: function (e) {
+                            e.data = {
+                                status: "success",
+                                records: e.data.content.vc_oktmo.map(function (i) {
+                                    return {
+                                        id: i.id,
+                                        code: i.code,
+                                        text: i.code + ' ' + i.site_name
+                                    }
+                                })
+                            }
+                        }
+                    }},
+                ],
+
+                focus: 0
+            })
+
+       })
+
+    }
+
+})
