b46d15d53168a29f953046994762317bfe1ea016
Maksim Gruzdev <mgruzdev@cheby.ru>
2019-03-28 17:00:42 +0300

    tf check uniq period tasks/3679903e-0360-46c4-9e92-ffbb750092e3

--- mosgis-ejb/src/main/java/ru/eludia/products/mosgis/db/model/tables/TarifDiff.java
+++ mosgis-ejb/src/main/java/ru/eludia/products/mosgis/db/model/tables/TarifDiff.java
@@ -50,10 +50,33 @@ public class TarifDiff extends EnTable  {
 
     public TarifDiff () {
 
-	super  (TABLE_NAME, "Тарифы: значения критериев дифференциации тарифов");
+	super  (TABLE_NAME, "Тарифы: значения критериев дифференциации");
 
 	cols   (c.class);
 
 	key    (c.UUID_TF);
+
+	trigger("BEFORE INSERT OR UPDATE", ""
+	    + "DECLARE"
+	    + " PRAGMA AUTONOMOUS_TRANSACTION; "
+	    + "BEGIN "
+	    + " IF :NEW.is_deleted = 0 THEN BEGIN "
+		+ " FOR i IN ("
+		    + "SELECT "
+		    + " pu_tf.name label "
+		    + "FROM "
+		    + TABLE_NAME + " o "
+		    + "LEFT JOIN " + PremiseUsageTarif.TABLE_NAME + " pu_tf ON pu_tf.uuid = o.uuid_tf "
+		    + "WHERE o.is_deleted = 0 "
+		    + " AND o.uuid <> :NEW.uuid "
+		    + " AND o.code_diff = :NEW.code_diff "
+		    + " AND o.uuid_tf   = :NEW.uuid_tf "
+		+ ") LOOP"
+		    + " raise_application_error (-20000, "
+		    + "'Указанный критерий дифференциации уже есть в ' || i.label "
+		    + "); "
+		+ " END LOOP; "
+	    + " END; END IF; "
+	    + "END;");
     }
 }
