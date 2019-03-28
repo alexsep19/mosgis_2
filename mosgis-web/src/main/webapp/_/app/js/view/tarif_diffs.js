2e490df8fcc6dcedf706f28d877f50ab077c4f7d
Maksim Gruzdev <mgruzdev@cheby.ru>
2019-03-28 16:33:55 +0300

    tf diff ro tasks/61c43b87-c2e6-432e-9865-2d0ebecb4ff3

--- mosgis-web/src/main/webapp/_/app/js/view/tarif_diffs.js
+++ mosgis-web/src/main/webapp/_/app/js/view/tarif_diffs.js
@@ -133,7 +133,7 @@ define ([], function () {
                 e.xhr.responseText = JSON.stringify (data)
             },
 
-            onDblClick: $_DO.edit_tarif_diffs,
+            onDblClick: data._can.edit? $_DO.edit_tarif_diffs : null,
 
             onAdd: $_DO.create_tarif_diffs,
 
