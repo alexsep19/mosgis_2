alexsep19/mosgis_2

commit b3a8c032ad61c5d05665d7040dadef891d1ae02c

ws client logging: always both messages

mosgis-ejb/src/main/java/ru/eludia/products/mosgis/ws/soap/tools/LoggingOutMessageHandler.java
@@ -85,8 +85,10 @@ public abstract class LoggingOutMessageHandler extends BaseLoggingMessageHandler
 
         SOAPMessage msg = messageContext.getMessage ();
         
-        MessageInfo messageInfo = new MessageInfo (messageContext);                        
-        
+        MessageInfo messageInfo = new MessageInfo (messageContext);        
+        byte [] bytes = toBytes (msg);
+        String s = getLoggedMessge (messageInfo, bytes, getCharSetName (msg));
+            
         if (messageInfo.isOut) {
             
             HeaderType rh = createRequestHeader (messageContext);
@@ -94,39 +96,26 @@ public abstract class LoggingOutMessageHandler extends BaseLoggingMessageHandler
             rh.setMessageGUID (getMessageGUID (messageContext).toString ());
             
             try {
-
                 AbstactServiceAsync.setRequestHeader (msg, rh);
-                                
             }
             catch (Exception ex) {
                 Logger.getLogger (LoggingOutMessageHandler.class.getName()).log (Level.SEVERE, null, ex);
             }
             
-            byte [] bytes = toBytes (msg);
-        
-            String s = getLoggedMessge (messageInfo, bytes, getCharSetName (msg));
-
             if (!"getState".equals (messageInfo.operation)) store (messageInfo, rh, s);
 
         }
         else {
             
-            if ("getState".equals (messageInfo.operation)) {
-                
-                byte [] bytes = toBytes (msg);
-
-                String s = getLoggedMessge (messageInfo, bytes, getCharSetName (msg));
-
-                ResultHeader resultHeader = AbstactServiceAsync.getResultHeader (msg);
-                
-                update (resultHeader, s);
-            
+            if ("getState".equals (messageInfo.operation)) {                
+                ResultHeader resultHeader = AbstactServiceAsync.getResultHeader (msg);                
+                update (resultHeader, s);            
             }            
             
         }
 
         return true;
-        
+
     }
-        
+
 }

commit 59e2fb8d736b94fd655fe8c8edcf71a6724893bd

LoggingOutMessageHandler FIX (outgoing header logging was broken since 	b3a8c03)

mosgis-ejb/src/main/java/ru/eludia/products/mosgis/ws/soap/tools/LoggingOutMessageHandler.java
@@ -85,8 +85,8 @@ public abstract class LoggingOutMessageHandler extends BaseLoggingMessageHandler
         SOAPMessage msg = messageContext.getMessage ();
         
         MessageInfo messageInfo = new MessageInfo (messageContext);        
-        byte [] bytes = toBytes (msg);
-        String s = getLoggedMessge (messageInfo, bytes, getCharSetName (msg));
+        
+        final boolean isGetState = "getState".equals (messageInfo.operation);
             
         if (messageInfo.isOut) {
             
@@ -98,19 +98,20 @@ public abstract class LoggingOutMessageHandler extends BaseLoggingMessageHandler
                 SOAPTools.setRequestHeader (msg, rh);
             }
             catch (Exception ex) {
-                Logger.getLogger (LoggingOutMessageHandler.class.getName()).log (Level.SEVERE, null, ex);
+                logger.log (Level.SEVERE, null, ex);
             }
-            
-            if (!"getState".equals (messageInfo.operation)) store (messageInfo, rh, s);
+
+            final String xml = getLoggedMessge (messageInfo, toBytes (msg), getCharSetName (msg)); // MUST be called AFTER setRequestHeader; side effect: logs out the message
+
+            if (!isGetState) store (messageInfo, rh, xml);
 
         }
         else {
-            
-            if ("getState".equals (messageInfo.operation)) {                
-                ResultHeader resultHeader = SOAPTools.getResultHeader (msg);                
-                update (resultHeader, s);            
-            }            
-            
+
+            final String xml = getLoggedMessge (messageInfo, toBytes (msg), getCharSetName (msg)); // side effect: logs out the message
+
+            if (isGetState) update (SOAPTools.getResultHeader (msg), xml);            
+
         }
 
         return true;