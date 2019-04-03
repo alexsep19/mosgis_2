4f01a1882d61131da6730dd83488f544ac8ea5bd
Dmitry Ovsyanko <do_@pochta.ru>
2019-04-03 12:43:22 +0300

    pd power ui IN PROGRESS tasks/5bd696e4-d37e-4d95-bf53-e4bb6706c6af

---
+++ mosgis-web/src/main/webapp/_/app/js/data/payment_document_common_comp_cost.js
@@ -0,0 +1,111 @@
+define ([], function () {
+
+    $_DO.patch_payment_document_common_comp_cost = function (e) {
+
+        var grid = this
+
+        var col = grid.columns [e.column]
+
+        var v = normalizeValue (e.value_new, col.editable.type)
+
+        var data = {}; 
+
+        data [col.field] = v == null ? null : String (v)
+
+        var nsi331_ba = e.recid.split ('_')
+
+        data.code_vc_nsi_331 = nsi331_ba [0]
+        data.uuid_bnk_acct   = nsi331_ba [1]
+
+        grid.lock ()
+
+        query ({type: 'payment_documents', action: 'patch_comp_cost'}, {data: data}, function (d) {
+            grid.unlock ()
+            grid.refresh ()
+        })
+
+    }
+
+    return function (done) {
+
+        var layout = w2ui ['passport_layout']
+
+        if (layout) layout.unlock ('main')
+
+        var data = $('body').data ('data')                
+
+        query ({type: 'payment_documents', part: 'charge_info'}, {}, function (d) {
+
+            var ba = {}
+
+            $.each (d.root, function () {
+            
+                var id = this.uuid_bnk_acct
+
+                ba [id] = {
+                    id: id,
+                    label: this ['ba.label'],
+                    org_label: this ['org_bank_acct.label'],
+                }
+
+            })
+            
+            ba = Object.values (ba).sort (function (a, b) {return a.label < b.label ? -1 : 1})        
+        
+            var lines = [{recid: 'total', label: 'Итого'}]
+            
+            var idx = {}
+            
+            $.each (data.vc_nsi_331.items, function () {
+            
+                var nsi_331 = this
+
+                $.each (ba, function () {                
+                
+                    var id = nsi_331.id + '_' + this.id
+                
+                    lines.push (idx [id] = {
+                        recid: id,
+                        label: nsi_331.label,
+                        acct: this.label,
+                        org_label: this.org_label,
+                        id_type: 1,
+                    })                                
+                
+                })            
+                
+            })
+            
+            query ({type: 'payment_documents', part: 'penalties'}, {}, function (dd) {
+
+                $.each (dd.root, function () {                
+
+                    var r = idx [this.code_vc_nsi_331 + '_' + this.uuid_bnk_acct]
+
+                    r.totalpayable = this.totalpayable
+                    r.cause = this.cause
+
+                })
+
+                data.lines = lines
+
+                done (data)
+            
+            });
+            
+            
+/*            
+            $.each (dia2w2uiRecords (d.root), function () {
+                
+                if (!this.uuid_m_m_service) return
+
+                lines.push (this)
+            
+            })
+*/        
+
+        }) 
+
+    }
+
+})
