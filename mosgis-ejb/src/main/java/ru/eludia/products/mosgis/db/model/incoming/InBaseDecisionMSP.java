b781ab51d108b498da2c93a85d6c6302c31041e5
Maksim Gruzdev <mgruzdev@cheby.ru>
2019-04-04 17:37:21 +0300

    nsi302 mdb tasks/46be4383-4559-4065-bcb8-1a4d9351c3c5

---
+++ mosgis-ejb/src/main/java/ru/eludia/products/mosgis/db/model/incoming/InBaseDecisionMSP.java
@@ -0,0 +1,50 @@
+package ru.eludia.products.mosgis.db.model.incoming;
+
+import ru.eludia.base.model.Col;
+import ru.eludia.base.model.ColEnum;
+import ru.eludia.base.model.Ref;
+import ru.eludia.base.model.Table;
+import ru.eludia.base.model.Type;
+import static ru.eludia.base.model.def.Def.NEW_UUID;
+import static ru.eludia.base.model.def.Def.NOW;
+import ru.eludia.products.mosgis.db.model.tables.OutSoap;
+import ru.eludia.products.mosgis.db.model.voc.VocOrganization;
+
+public class InBaseDecisionMSP extends Table {
+
+    public enum c implements ColEnum {
+        
+        UUID          (Type.UUID,      NEW_UUID, "Ключ"),
+        TS            (Type.TIMESTAMP, NOW,      "Дата/время записи в БД"),
+        UUID_ORG      (VocOrganization.class,    "Организация-инициатор импорта"),
+        UUID_OUT_SOAP (OutSoap.class, null,      "Импорт")
+        
+        ;
+        
+        @Override
+        public Col getCol() {
+            return col;
+        }
+        private Col col;
+
+        private c(Type type, Object... p) {
+            col = new Col(this, type, p);
+        }
+
+        private c(Class c, Object... p) {
+            col = new Ref(this, c, p);
+        }
+
+    }
+    
+    public InBaseDecisionMSP () {
+        
+        super ("in_base_dec_msp", "Запросы на импорт НСИ 302");
+        
+        cols  (c.class);
+        
+        pk    (c.UUID);
+        
+    }
+    
+}
