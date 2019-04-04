alexsep19/mosgis_2

commit 8cb016b32c0fa05d56cd9ef92dc811294664aec4

tasks/35c966eb-9214-4df7-943c-83b0a4be43f7

mosgis-web/src/main/webapp/_/app/js/view/rc_contract_common.js
@@ -6,6 +6,11 @@ define ([], function () {
         it.is_bnk_acct_visible = it.is_billing || it.id_ctr_status > 10
     }
 
+    function check_is_bnk_acct_visible (it) {
+        it.is_annuled = it.id_ctr_status == 110
+        it.is_terminated = it.id_ctr_status == 100
+    }
+
     function recalc () {
 
         var f = w2ui [form_name]
@@ -180,6 +185,10 @@ define ([], function () {
                 {name: 'completiondate', type: 'date'},
                 {name: 'uuid_org', type: 'text', hidden: true},
                 {name: 'label_org', type: 'text'},
+                
+                {name: 'reason_of_annulment', type: 'text'},
+                {name: 'terminate', type: 'text'},
+                {name: 'reason_of_termination', type: 'text'},
 
                 {name: 'is_accounts', type: 'list', options: {items: data.voc_bool}},
                 {name: 'is_invoices', type: 'list', options: {items: data.voc_bool}},

mosgis-web/src/main/webapp/_/app/js/data/rc_contract_common.js
@@ -66,7 +66,11 @@ define ([], function () {
     }
 
     $_DO.annul_rc_contract_common = function (e) {
-        use.block ('supply_resource_contract_annul_popup')
+        use.block ('rc_contract_annul_popup')
+    }
+    
+    $_DO.terminate_rc_contract_common = function (e) {
+        use.block ('rc_contract_terminate_popup')
     }
     
     $_DO.set_bank_acct_rc_contract_common = function (e) {   

mosgis-api/src/main/java/ru/eludia/products/mosgis/rest/api/RcContractLocal.java
@@ -2,10 +2,13 @@ package ru.eludia.products.mosgis.rest.api;
 
 import javax.ejb.Local;
 import javax.json.JsonObject;
-import ru.eludia.products.mosgis.rest.User;
 import ru.eludia.products.mosgis.rest.api.base.CRUDBackend;
 
 @Local
 public interface RcContractLocal extends CRUDBackend {
     JsonObject getVocs   (JsonObject p);
+    JsonObject doApprove(String id);
+    JsonObject doAlter(String id);
+    JsonObject doAnnul(String id, JsonObject p);
+    JsonObject doTerminate(String id, JsonObject p);
 }

mosgis-ejb/src/main/java/ru/eludia/products/mosgis/db/model/tables/RcContract.java
@@ -29,7 +29,7 @@ public class RcContract extends EnTable {
 	SIGNINGDATE          (Type.DATE, "Дата заключения"),
 	EFFECTIVEDATE        (Type.DATE, "Дата вступления в силу"),
 	COMPLETIONDATE       (Type.DATE, null, "Дата окончания плановая"),
-	TERMINATE            (Type.DATE, null, "Дата растрожения"),
+	TERMINATE            (Type.DATE, null, "Дата расторжения"),
 
 	DT_FROM              (Type.DATE, null, "Дата начала действия договора"),
 	DT_TO                (Type.DATE, null, "Дата окончания действия договора"),
@@ -51,6 +51,9 @@ public class RcContract extends EnTable {
 
         UUID_BNK_ACCT        (BankAccount.class,     "Платёжные реквизиты"),
 
+        REASON_OF_ANNULMENT  (Type.STRING, null, "Причина аннулирования"),
+        REASON_OF_TERMINATION(Type.STRING, null, "Причина расторжения"),
+        
         ;
 
         @Override

mosgis-ejb/src/main/java/ru/eludia/products/mosgis/ws/rest/impl/RcContractImpl.java
@@ -1,14 +1,20 @@
 package ru.eludia.products.mosgis.ws.rest.impl;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.logging.Logger;
 import javax.ejb.Stateless;
 import javax.ejb.TransactionAttribute;
 import javax.ejb.TransactionAttributeType;
 import javax.json.JsonObject;
 import ru.eludia.base.DB;
+import static ru.eludia.base.DB.HASH;
 import ru.eludia.base.Model;
 import ru.eludia.base.db.sql.gen.Operator;
 import ru.eludia.base.db.sql.gen.Select;
+import ru.eludia.base.model.def.Bool;
 import ru.eludia.products.mosgis.db.model.EnTable;
 import ru.eludia.products.mosgis.db.model.MosGisModel;
 import ru.eludia.products.mosgis.db.model.tables.ActualBankAccount;
@@ -22,6 +28,15 @@ import ru.eludia.products.mosgis.db.model.voc.VocGisStatus;
 import ru.eludia.products.mosgis.db.model.voc.VocOrganization;
 import ru.eludia.products.mosgis.db.model.voc.VocRcContractServiceTypes;
 import ru.eludia.products.mosgis.db.ModelHolder;
+import ru.eludia.products.mosgis.db.model.nsi.NsiTable;
+import ru.eludia.products.mosgis.db.model.tables.ActualSomeContractObject;
+import ru.eludia.products.mosgis.db.model.tables.CharterLog;
+import ru.eludia.products.mosgis.db.model.tables.CharterObject;
+import ru.eludia.products.mosgis.db.model.tables.CharterObjectLog;
+import ru.eludia.products.mosgis.db.model.tables.CharterObjectService;
+import ru.eludia.products.mosgis.db.model.tables.CharterObjectServiceLog;
+import ru.eludia.products.mosgis.db.model.tables.RcContractObject;
+import ru.eludia.products.mosgis.db.model.voc.VocBuildingAddress;
 import ru.eludia.products.mosgis.rest.User;
 import ru.eludia.products.mosgis.rest.api.RcContractLocal;
 import ru.eludia.products.mosgis.ws.rest.impl.base.BaseCRUD;
@@ -125,5 +140,96 @@ public class RcContractImpl extends BaseCRUD<RcContract> implements RcContractLo
         VocGisStatus.addLiteTo (job);
 	VocAction.addTo(job);
 	VocRcContractServiceTypes.addTo(job);
+        db.addJsonArrays (job, NsiTable.getNsiTable(54).getVocSelect ());
+    });}
+    
+    @Override
+    public JsonObject doApprove (String id) {return doAction ((DB db) -> {
+          
+        db.update (getTable (), HASH (
+            "uuid", id,
+            "id_ctr_status", 40
+        ));
+        
+        final Model m = db.getModel();
+        
+        List<Map<String, Object>> idsRcContractObject = db.getList (m.select(RcContractObject.class, "uuid" )
+            .where(RcContractObject.c.UUID_RC_CTR, id)
+            .and(RcContractObject.c.ID_CTR_STATUS, 10));
+        
+        List<Map<String, Object>> newIdsRcContractObject = new ArrayList<>();
+        
+        idsRcContractObject.forEach(rec -> {
+            rec.put(RcContractObject.c.ID_CTR_STATUS.lc(), 40);
+            newIdsRcContractObject.add(rec);
+        });
+
+        db.update (RcContractObject.class, newIdsRcContractObject);
+
+    });}
+    
+    @Override
+    public JsonObject doAlter (String id) {return doAction ((DB db) -> {
+          
+        db.update (getTable (), HASH (
+            "uuid", id,
+            "id_ctr_status", 11
+        ));
+
+    });}
+    
+    @Override
+    public JsonObject doAnnul (String id, JsonObject p) {return doAction ((DB db) -> {
+        
+        JsonObject data = p.getJsonObject ("data");
+        
+        db.update (getTable (), getData (p,
+            "uuid", id,
+            "id_ctr_status", 110,
+            "reason_of_annulment", data.getString ("reason_of_annulment", null)
+        ));
+        
+        final Model m = db.getModel();
+        
+        List<Map<String, Object>> idsRcContractObject = db.getList (m.select(RcContractObject.class, "uuid" )
+            .where(RcContractObject.c.UUID_RC_CTR, id));
+        
+        List<Map<String, Object>> newIdsRcContractObject = new ArrayList<>();
+        
+        idsRcContractObject.forEach(rec -> {
+            rec.put(RcContractObject.c.ID_CTR_STATUS.lc(), 110);
+            newIdsRcContractObject.add(rec);
+        });
+
+        db.update (RcContractObject.class, newIdsRcContractObject);
+        
+    });}
+    
+    @Override
+    public JsonObject doTerminate (String id, JsonObject p) {return doAction ((DB db) -> {
+        
+        JsonObject data = p.getJsonObject ("data");
+        
+        db.update (getTable (), getData (p,
+            "uuid", id,
+            "id_ctr_status", 100,
+            "terminate", data.getString ("date_of_termination", null),    
+            "reason_of_termination", data.getString ("reason_of_termination", null)
+        ));
+        
+        final Model m = db.getModel();
+        
+        List<Map<String, Object>> idsRcContractObject = db.getList (m.select(RcContractObject.class, "uuid" )
+            .where(RcContractObject.c.UUID_RC_CTR, id));
+        
+        List<Map<String, Object>> newIdsRcContractObject = new ArrayList<>();
+        
+        idsRcContractObject.forEach(rec -> {
+            rec.put(RcContractObject.c.DT_TO.lc(), data.getString ("date_of_termination", null));
+            newIdsRcContractObject.add(rec);
+        });
+
+        db.update (RcContractObject.class, newIdsRcContractObject);
+        
     });}
 }

mosgis-web/src/main/java/ru/eludia/products/mosgis/rest/resources/RcContracts.java
@@ -127,4 +127,42 @@ public class RcContracts extends EJBResource <RcContractLocal> {
     public JsonObject getVocs (JsonObject p) {
         return back.getVocs (p);
     }
+    
+    @POST
+    @Path("{id}/approve")
+    @Produces (APPLICATION_JSON)
+    public JsonObject doApprove (@PathParam ("id") String id) {
+        final JsonObject item = getInnerItem (id);
+        checkOrg (item);
+        return back.doApprove (id);
+    }
+    
+    @POST
+    @Path("{id}/alter")
+    @Produces (APPLICATION_JSON)
+    public JsonObject doAlter (@PathParam ("id") String id) {
+        final JsonObject item = getInnerItem (id);
+        checkOrg (item);
+        return back.doAlter (id);
+    }
+    
+    @POST
+    @Path("{id}/annul")
+    @Consumes (APPLICATION_JSON)
+    @Produces (APPLICATION_JSON)
+    public JsonObject doAnnul (@PathParam ("id") String id, JsonObject p) {
+        final JsonObject item = back.getItem (id, getUser ());
+        checkOrg (item.getJsonObject ("item"));
+        return back.doAnnul (id, p);
+    }
+    
+    @POST
+    @Path("{id}/terminate")
+    @Consumes (APPLICATION_JSON)
+    @Produces (APPLICATION_JSON)
+    public JsonObject doTerminate (@PathParam ("id") String id, JsonObject p) {
+        final JsonObject item = back.getItem (id, getUser ());
+        checkOrg (item.getJsonObject ("item"));
+        return back.doTerminate (id, p);
+    }
 }

mosgis-web/src/main/webapp/_/app/html/rc_contract_annul_popup.html
@@ -1,0 +1,36 @@
+<span>
+
+	<style>
+
+        label[data-block-name=rc_contract_annul_popup] {
+        	width: 150px;
+        	margin-right: 10px;
+        }
+
+        textarea[data-block-name=rc_contract_annul_popup] {
+        	width: 350px;
+        	height: 90px;
+        }
+
+	</style>
+
+	<div class="w2ui-reset w2ui-form" style="height:185px ">
+
+        <div class="w2ui-page page-0">
+
+            <div class="w2ui-field">
+                <label>Причина аннулирования</label>
+                <div>
+                	<textarea name=reason_of_annulment></textarea>
+                </div>
+            </div>
+
+        </div>
+
+        <div class="w2ui-buttons">
+			<button class=w2ui-btn name=update>Сохранить</button>
+        </div>
+
+	</div>
+
+</span>

mosgis-web/src/main/webapp/_/app/html/rc_contract_common.html
@@ -113,8 +113,24 @@
                         </div>
 
                     </td>
+                    <td valign=top >
 
-                    <td valign=top>
+                        <div class="w2ui-field" data-on=is_annuled>
+                            <label>Причина</label>
+                            <div>
+                                <span data-text=reason_of_annulment maxlength=255 />
+                            </div>
+                        </div>
+                        <div class="w2ui-field" data-on=is_terminated>
+                            <label>Дата расторжения</label>
+                            <div>
+                                <span data-text=terminate data-date />
+                            </div>
+                        </div>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="left-block" width=1 valign=top>
 
                         <div class="w2ui-field">
                             <label>Дата вступления в силу</label>
@@ -124,7 +140,16 @@
                         </div>
 
                     </td>
+                    <td valign=top data-on=is_terminated>
 
+                        <div class="w2ui-field">
+                            <label>Основание расторжения</label>
+                            <div>
+                                <span data-text=reason_of_termination maxlength=255 />
+                            </div>
+                        </div>
+
+                    </td>
                 </tr>
                 <tr>
 
@@ -247,7 +272,12 @@
 
         <div class="w2ui-buttons">
             <span>
-
+                
+                <button class=w2ui-btn name=approve   data-on=__read_only>Утвердить</button>
+                <button class=w2ui-btn name=alter   data-on=__read_only>Изменить</button>
+                <button class=w2ui-btn name=annul   data-on=__read_only>Аннулировать</button>
+                <button class=w2ui-btn name=terminate   data-on=__read_only>Расторгнуть</button>
+                
                 <button class=w2ui-btn name=edit   data-on=__read_only>Редактировать</button>
                 <button class=w2ui-btn name=update data-off=__read_only>Сохранить</button>
                 <button class=w2ui-btn name=cancel data-off=__read_only>Отменить</button>

commit c0cf4f875fd11b659753dbb230387966a5653bff

rc ui FIX (was broken since 8cb016b32c0fa05d56cd9ef92dc811294664aec4) tasks/b4d43e7f-9bb0-4f70-88ab-632683eec1d0

mosgis-web/src/main/webapp/_/app/js/view/rc_contract_common.js
@@ -6,11 +6,6 @@ define ([], function () {
         it.is_bnk_acct_visible = it.is_billing || it.id_ctr_status > 10
     }
 
-    function check_is_bnk_acct_visible (it) {
-        it.is_annuled = it.id_ctr_status == 110
-        it.is_terminated = it.id_ctr_status == 100
-    }
-
     function recalc () {
 
         var f = w2ui [form_name]
@@ -95,6 +90,8 @@ define ([], function () {
     return function (data, view) {
     
         var it = data.item
+        it.is_annuled = it.id_ctr_status == 110
+        it.is_terminated = it.id_ctr_status == 100
         
         check_is_bnk_acct_visible (it)
 