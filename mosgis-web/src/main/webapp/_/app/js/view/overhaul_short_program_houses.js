f03e286bfd92f23a8d08db7611b7707d19fe7342
Mihail Mochajlo <mihail.mochajlo@gmail.com>
2019-04-10 10:56:14 +0300

    house part added for tasks/2b81cd04-d29b-45f7-86c4-52748c3c06f2

---
+++ mosgis-web/src/main/webapp/_/app/js/view/overhaul_short_program_houses.js
@@ -0,0 +1,52 @@
+define ([], function () {
+
+    return function (data, view) {
+    
+        data = $('body').data ('data')
+
+        function canEdit () {
+            
+            if (!data.item.is_deleted && ($_USER.role.nsi_20_7 || $_USER.role.admin)) {
+                switch (data.item.last_succesfull_status) {
+                    case  10:
+                    case -22:
+                        return true
+                }
+            }
+            return false
+
+        }
+        
+        $(w2ui ['topmost_layout'].el ('main')).w2regrid ({ 
+
+            name: 'overhaul_short_program_houses_grid',
+
+            show: {
+                toolbar: true,
+                toolbarInput: false,
+                toolbarAdd: canEdit (),
+                toolbarDelete: canEdit (),
+                footer: true,
+            },
+
+            columns: [
+                {field: 'address', caption: 'Адрес', size: 10},
+                {field: 'oktmo', caption: 'Код ОКТМО', size: 10},
+            ],
+            
+            url: '/_back/?type=overhaul_short_program_houses',
+
+            postData: {program_uuid: $_REQUEST.id},
+
+            onAdd: $_DO.create_overhaul_short_program_house,
+            onDelete: $_DO.delete_overhaul_short_program_house,
+            
+            onDblClick: function (e) {
+                openTab ('/overhaul_short_program_house/' + e.recid)
+            },
+
+        }).refresh ();
+
+    }
+
+})
