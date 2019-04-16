2b50864a42ce9f264560aff1433ba2035eb7c6a2
Alex Sepity <a.sepityi@rc.center>
2019-04-16 14:26:10 +0300

    tasks/00683632-0fb8-4c74-a80f-6b07d6cd0320

--- mosgis-ejb/src/main/java/ru/eludia/products/mosgis/jms/xl/ParseMeteringValuesMDB.java
+++ mosgis-ejb/src/main/java/ru/eludia/products/mosgis/jms/xl/ParseMeteringValuesMDB.java
@@ -1,6 +1,7 @@
 package ru.eludia.products.mosgis.jms.xl;
 
 import java.sql.SQLException;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -15,7 +16,9 @@ import ru.eludia.base.DB;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import ru.eludia.products.mosgis.db.model.EnTable;
 import ru.eludia.products.mosgis.db.model.incoming.xl.lines.InXlMeteringDevice;
+import ru.eludia.products.mosgis.db.model.incoming.xl.lines.InXlMeteringValues;
 import ru.eludia.products.mosgis.db.model.tables.MeteringDevice;
+import ru.eludia.products.mosgis.db.model.tables.MeteringDeviceValue;
 import ru.eludia.products.mosgis.jms.xl.base.XLMDB;
 import ru.eludia.products.mosgis.jms.xl.base.XLException;
 
@@ -28,79 +31,84 @@ public class ParseMeteringValuesMDB extends XLMDB {
        
     private static final int N_COL_UUID = 33;
     private static final int N_COL_ERR  = 34;
+    public static final Date NULL_DATE = new Date(0);
 
-//    protected void addMeters (XSSFSheet sheet, UUID parent, DB db, Map<Integer, Integer> resourceMap) throws SQLException {
-//
-//        for (int i = 2; i <= sheet.getLastRowNum (); i ++) {
-//            
-//            final XSSFRow row = sheet.getRow (i);
-//
-//            if (row.getCell (0) == null) continue;
-//            
-//            UUID uuid = (UUID) db.insertId (InXlMeteringDevice.class, InXlMeteringDevice.toHash (parent, i, row, resourceMap));            
-//
-//            try {
-//                
-//                db.update (InXlMeteringDevice.class, DB.HASH (
-//                    EnTable.c.UUID, uuid,
-//                    EnTable.c.IS_DELETED, 0
-//                ));
-//                
-//                db.update (MeteringDevice.class, DB.HASH (
-//                    EnTable.c.UUID, uuid,
-//                    EnTable.c.IS_DELETED, 0
-//                ));
-//                
-//                setCellStringValue (row, N_COL_UUID, uuid.toString ());
-//
-//            }
-//            catch (SQLException e) {
-//
-//                String s = getErrorMessage (e);
-//
-//                db.update (InXlMeteringDevice.class, DB.HASH (
-//                    EnTable.c.UUID,           uuid,
-//                    InXlMeteringDevice.c.ERR, s
-//                ));
-//                
-//                setCellStringValue (row, N_COL_UUID, "");
-//                setCellStringValue (row, N_COL_ERR, s);
-//                
-//            }
-//            
-//        }
-//        
-//    }   
+    protected void addValues (XSSFSheet sheet, UUID parent, DB db/*, Map<Integer, Integer> resourceMap*/) throws SQLException {
 
-//    private boolean checkMeterLines (XSSFSheet sheet, DB db, UUID parent) throws SQLException {
-//        
-//        List<Map<String, Object>> brokenLines = db.getList (db.getModel ()
-//            .select (InXlMeteringDevice.class, "*")
-//            .where (InXlMeteringDevice.c.UUID_XL, parent)
-//            .where (EnTable.c.IS_DELETED, 1)
-//        );
-//        
-//        if (brokenLines.isEmpty ()) return true;
-//        
-//        for (Map<String, Object> brokenLine: brokenLines) {
-//            XSSFRow row = sheet.getRow ((int) DB.to.Long (brokenLine.get (InXlMeteringDevice.c.ORD.lc ())));
-//            XSSFCell cell = row.getLastCellNum () < 9 ? row.createCell (9) : row.getCell (9);
-//            cell.setCellValue (brokenLine.get (InXlMeteringDevice.c.ERR.lc ()).toString ());
-//        }
-//
-//        return false;
-//        
-//    }
+        Date dateValue = NULL_DATE;
+        Date datePeriod = NULL_DATE;
+        
+        for (int i = 1; i <= sheet.getLastRowNum (); i ++) {
+            
+            final XSSFRow row = sheet.getRow (i);
+
+            if (row.getCell (1) == null) continue;
+            
+            UUID uuid = (UUID) db.insertId (InXlMeteringValues.class, InXlMeteringValues.toHash (parent, i, row, dateValue, datePeriod));            
+
+            try {
+                
+                db.update (InXlMeteringValues.class, DB.HASH (
+                    EnTable.c.UUID, uuid,
+                    EnTable.c.IS_DELETED, 0
+                ));
+                
+                db.update (MeteringDeviceValue.class, DB.HASH (
+                    EnTable.c.UUID, uuid,
+                    EnTable.c.IS_DELETED, 0
+                ));
+                
+                setCellStringValue (row, N_COL_UUID, uuid.toString ());
+
+            }
+            catch (SQLException e) {
+
+                String s = getErrorMessage (e);
+
+                db.update (InXlMeteringValues.class, DB.HASH (
+                    EnTable.c.UUID,           uuid,
+                    InXlMeteringValues.c.ERR, s
+                ));
+                
+                setCellStringValue (row, N_COL_UUID, "");
+                setCellStringValue (row, N_COL_ERR, s);
+                
+            }
+            
+        }
+        
+    }   
+
+    private boolean checkMeterLines (XSSFSheet sheet, DB db, UUID parent) throws SQLException {
+        
+        List<Map<String, Object>> brokenLines = db.getList (db.getModel ()
+            .select (InXlMeteringValues.class, "*")
+            .where (InXlMeteringValues.c.UUID_XL, parent)
+            .where (EnTable.c.IS_DELETED, 1)
+        );
+        
+        if (brokenLines.isEmpty ()) return true;
+        
+        for (Map<String, Object> brokenLine: brokenLines) {
+            XSSFRow row = sheet.getRow ((int) DB.to.Long (brokenLine.get (InXlMeteringValues.c.ORD.lc ())));
+            XSSFCell cell = row.getLastCellNum () < 8 ? row.createCell (7) : row.getCell (7);
+            if (cell == null) cell = row.createCell (7);
+            cell.setCellValue (brokenLine.get (InXlMeteringValues.c.ERR.lc ()).toString ());
+        }
+
+        return false;
+        
+    }
 
     @Override
     protected void completeOK (DB db, UUID parent, XSSFWorkbook wb) throws SQLException {
         
         super.completeOK (db, parent, wb);
         
-//        db.update (MeteringDevice.class, DB.HASH (
-//            MeteringDevice.c.UUID_XL, parent,
-//            EnTable.c.IS_DELETED, 0
-//        ), MeteringDevice.c.UUID_XL.lc ());
+        db.update (MeteringDeviceValue.class, DB.HASH (
+            MeteringDevice.c.UUID_XL, parent,
+            EnTable.c.IS_DELETED, 0
+        ), MeteringDevice.c.UUID_XL.lc ());
 
     }
 
@@ -109,10 +117,10 @@ public class ParseMeteringValuesMDB extends XLMDB {
         
         super.completeFail (db, parent, wb);
         
-//        db.delete (db.getModel ()
-//            .select (MeteringDevice.class)
-//            .where  (MeteringDevice.c.UUID_XL, parent)
-//        );            
+        db.delete (db.getModel ()
+            .select (MeteringDeviceValue.class)
+            .where  (MeteringDeviceValue.c.UUID_XL, parent)
+        );            
         
     }
         
@@ -133,17 +141,19 @@ public class ParseMeteringValuesMDB extends XLMDB {
     @Override
     protected void processLines (XSSFWorkbook wb, UUID uuid, DB db) throws Exception {
         
-//        boolean isOk = true;        
+        boolean isOk = true;        
 //        
 //        final XSSFSheet sheetAddResources = wb.getSheet ("Доп. комм. ресурсы");        
 //        Map<Integer, Integer> resourceMap = toResourceMap (sheetAddResources);
 //        
-//        final XSSFSheet sheetMeters = wb.getSheet ("Сведения о ПУ");        
+        final XSSFSheet sheetValues = wb.getSheet ("Импорт показаний");        
+//        Map<Integer, Integer> resourceMap = toResourceMap (sheetMeters);
 //        addMeters (sheetMeters, uuid, db, resourceMap);
-//        
-//        if (!checkMeterLines (sheetMeters, db, uuid)) isOk = false;
-//
-//        if (!isOk) throw new XLException ();
+        addValues (sheetValues, uuid, db);
+        
+        if (!checkMeterLines (sheetValues, db, uuid)) isOk = false;
+
+        if (!isOk) throw new XLException ();
 
     }
 
