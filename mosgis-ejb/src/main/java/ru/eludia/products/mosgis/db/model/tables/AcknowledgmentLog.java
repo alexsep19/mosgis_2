8fd5bb309cd0c5286133758ecf22d6e5dbe85cdc
Dmitry Ovsyanko <do_@pochta.ru>
2019-04-23 09:47:54 +0300

    ask soap IN PROGRESS tasks/4b5fe81e-226b-41ab-9093-f84e38551b69

--- mosgis-ejb/src/main/java/ru/eludia/products/mosgis/db/model/tables/AcknowledgmentLog.java
+++ mosgis-ejb/src/main/java/ru/eludia/products/mosgis/db/model/tables/AcknowledgmentLog.java
@@ -2,11 +2,13 @@ package ru.eludia.products.mosgis.db.model.tables;
 
 import java.sql.SQLException;
 import java.util.Map;
+import java.util.UUID;
 import ru.eludia.base.DB;
 import ru.eludia.base.Model;
 import ru.eludia.products.mosgis.db.model.EnTable;
 import ru.eludia.products.mosgis.db.model.GisWsLogTable;
 import ru.gosuslugi.dom.schema.integration.bills.ImportAcknowledgmentRequest;
+import ru.gosuslugi.dom.schema.integration.payments_base.AcknowledgmentRequestInfoType;
 
 public class AcknowledgmentLog extends GisWsLogTable {
 
@@ -25,35 +27,45 @@ public class AcknowledgmentLog extends GisWsLogTable {
         
         final Map<String, Object> r = db.getMap (m                
                 
-            .get (AcknowledgmentLog.class, id, "*")
+            .get (AcknowledgmentLog.class, id)
                 
             .toOne (Acknowledgment.class, "AS r"
                 , EnTable.c.UUID.lc ()
             ).on ()
                 
-//            .toOne (PaymentDocument.class
-//            ).on ()
+            .toOne (PaymentDocument.class
+                , PaymentDocument.c.PAYMENTDOCUMENTID.lc () + " AS paymentdocumentid"
+                , PaymentDocument.c.YEAR.lc () + " AS year"
+                , PaymentDocument.c.MONTH.lc () + " AS month"
+            ).on ()
                 
             .toOne (Payment.class
-                , Payment.c.ORDERGUID.lc ()
+                , Payment.c.ORDERGUID.lc () + " AS notif_guid"
             ).on ()
                 
         );
-        
-        r.put ("notificationsoforderexecutionguid", r.get (Payment.c.ORDERGUID.lc ()));
-        
+
+        r.put ("notificationsoforderexecutionguid", r.get ("notif_guid"));
+
         return r;
                 
     }
         
     public static ImportAcknowledgmentRequest toImportAcknowledgment (Map<String, Object> r) {
         final ImportAcknowledgmentRequest result = DB.to.javaBean (ImportAcknowledgmentRequest.class, r);
-        result.getAcknowledgmentRequestInfo ().add (to (r));
+        result.getAcknowledgmentRequestInfo ().add (toAcknowledgmentRequestInfo (r));
         return result;
     }    
     
-    private static ImportAcknowledgmentRequest.AcknowledgmentRequestInfo to (Map<String, Object> r) {
+    private static ImportAcknowledgmentRequest.AcknowledgmentRequestInfo toAcknowledgmentRequestInfo (Map<String, Object> r) {
         final ImportAcknowledgmentRequest.AcknowledgmentRequestInfo result = DB.to.javaBean (ImportAcknowledgmentRequest.AcknowledgmentRequestInfo.class, r);
+        result.setPaymentDocumentAck (toPaymentDocumentAck (r));
+        result.setTransportGUID (UUID.randomUUID ().toString ());
+        return result;
+    }
+    
+    private static AcknowledgmentRequestInfoType.PaymentDocumentAck toPaymentDocumentAck (Map<String, Object> r) {
+        final AcknowledgmentRequestInfoType.PaymentDocumentAck result = DB.to.javaBean (AcknowledgmentRequestInfoType.PaymentDocumentAck.class, r);        
         return result;
     }
 
