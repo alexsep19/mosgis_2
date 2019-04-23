8b03211ea2b75c1d719ddd57edf6cadb2cdeef38
Aleksei Mukin <amukin@cheby.ru>
2019-04-23 16:29:03 +0300

    add mosgis as sender tasks/0dd69a1c-1cc2-4dec-9988-3020a608d503

--- mosgis-ejb/src/main/java/ru/eludia/products/mosgis/db/model/tables/Sender.java
+++ mosgis-ejb/src/main/java/ru/eludia/products/mosgis/db/model/tables/Sender.java
@@ -1,15 +1,21 @@
 package ru.eludia.products.mosgis.db.model.tables;
 
+import static ru.eludia.base.model.def.Bool.FALSE;
+
+import java.util.UUID;
+
 import ru.eludia.base.model.Col;
 import ru.eludia.base.model.Ref;
 import ru.eludia.base.model.Type;
-import static ru.eludia.base.model.def.Bool.FALSE;
 import ru.eludia.products.mosgis.db.model.EnColEnum;
 import ru.eludia.products.mosgis.db.model.EnTable;
 
 public class Sender extends EnTable {
     
-    public enum c implements EnColEnum {
+    public static final UUID MOS_GIS_UUID = UUID.fromString("00000000-0000-0000-0000-000000000000");
+    public static final UUID MOS_GIS_SALT = UUID.fromString("06b0804e-4459-497c-8add-c99fbc79e8b6");
+	
+	public enum c implements EnColEnum {
 
         ID_LOG               (SenderLog.class,    "Последнее событие редактирования"),
         LABEL_FULL           (Type.STRING, null,  "Наименование системы"),
@@ -25,7 +31,7 @@ public class Sender extends EnTable {
         public Col getCol () {return col;}
         private Col col;        
         private c (Type type, Object... p) {col = new Col (this, type, p);}
-        private c (Class c,   Object... p) {col = new Ref (this, c, p);}
+        private c (Class<?> c,   Object... p) {col = new Ref (this, c, p);}
 
         @Override
         public boolean isLoggable () {
@@ -43,6 +49,12 @@ public class Sender extends EnTable {
         super ("tb_senders", "Поставщики данных");                
         cols   (c.class);        
         unique ("login", "login");
+        
+        //Поставщик информации МосГИС
+        item(EnTable.c.UUID, MOS_GIS_UUID, 
+        		c.LABEL_FULL, "МосГИС", 
+        		c.LABEL, "МосГИС", 
+        		c.SALT, MOS_GIS_SALT);
     }
     
 }
