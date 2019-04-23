7876a16b968e50e038971b3b3e583c94750ded3b
Maksim Gruzdev <mgruzdev@cheby.ru>
2019-04-23 13:38:40 +0300

    ba db tasks/da72c62e-de6b-4c05-9333-f373e5db3f03

--- mosgis-ejb/src/main/java/ru/eludia/products/mosgis/db/model/tables/BankAccount.java
+++ mosgis-ejb/src/main/java/ru/eludia/products/mosgis/db/model/tables/BankAccount.java
@@ -26,6 +26,9 @@ public class BankAccount extends EnTable {
         OPENDATE               (Type.DATE,             null, "Дата открытия/изменения реквизитов"),
         CLOSEDATE              (Type.DATE,             null, "Дата закрытия"),
 
+	IS_ROKR                (Type.BOOLEAN,  null, "1, если является счетом регионального оператора, иначе 0"),
+	UUID_CRED_ORG          (VocOrganization.class, null, "Кредитная организация"),
+
 	ID_CTR_STATUS          (VocGisStatus.class,    VocGisStatus.DEFAULT,    "Статус с точки зрения mosgis"),
 	ID_CTR_STATUS_GIS      (VocGisStatus.class,    VocGisStatus.DEFAULT,    "Статус с точки зрения ГИС ЖКХ"),
 
@@ -78,8 +81,32 @@ public class BankAccount extends EnTable {
                         + ") LOOP"
                     + " raise_application_error (-20000, 'Номер расчетного счета выбранной кредитной организации уже используется ' || i.label || '. Операция отменена.'); "
                     + " END LOOP; "
-                                
-                + "END; END IF; "
+
+		    + "IF :NEW.is_rokr = 1 THEN "
+			+ " FOR i IN ("
+			    + "SELECT "
+			    + " o.uuid "
+			    + " , o.opendate "
+			    + " , o.closedate "
+			    + " , org.label "
+			    + "FROM "
+			    + TABLE_NAME + "  o "
+			    + " LEFT JOIN " + VocOrganization.TABLE_NAME +  " org ON o.UUID_ORG = org.uuid"
+			    + " WHERE o.is_deleted = 0"
+			    + " AND o.uuid <> :NEW.UUID "
+			    + " AND o.is_rokr = 1 "
+			    + " AND (o.opendate  < :NEW.closedate OR :NEW.closedate IS NULL) "
+			    + " AND (o.closedate > :NEW.opendate OR o.closedate IS NULL) "
+			    + ") LOOP"
+			+ " raise_application_error (-20000, 'Уже есть счет регионального оператора ' || i.label "
+			+ "   || ', действующий с ' "
+			+ "   || TO_CHAR (i.opendate, 'DD.MM.YYYY') "
+			+ "   || CASE WHEN i.closedate IS NULL THEN NULL ELSE ' по ' || TO_CHAR (i.closedate, 'DD.MM.YYYY') END "
+			+ "   || '. Разрешен только один счет регионального оператора, действующий в один период времени.'); "
+			+ " END LOOP; "
+		    + " END IF; "
+
+                + "END; END IF; " // IF :NEW.is_deleted=0
                                                                             
                 + "IF :OLD.is_deleted=0 AND :NEW.is_deleted=1 THEN BEGIN "
                                 
