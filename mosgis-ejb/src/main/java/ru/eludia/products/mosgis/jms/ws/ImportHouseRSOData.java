372e7f6bbb0601aa34f11956ffe398275812e5d8
Aleksei Mukin <amukin@cheby.ru>
2019-04-29 17:10:36 +0300

    import house by RSO IN_PROGRESS tasks/a33acf5e-ca2a-4184-aeb1-08896fda57e9

--- mosgis-ejb/src/main/java/ru/eludia/products/mosgis/jms/ws/ImportHouseRSOData.java
+++ mosgis-ejb/src/main/java/ru/eludia/products/mosgis/jms/ws/ImportHouseRSOData.java
@@ -13,15 +13,26 @@ import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 
 import ru.eludia.base.DB;
+import ru.eludia.base.db.sql.gen.Select;
+import ru.eludia.base.db.util.TypeConverter;
+import ru.eludia.products.mosgis.db.model.EnTable;
+import ru.eludia.products.mosgis.db.model.nsi.NsiTable;
+import ru.eludia.products.mosgis.db.model.tables.Entrance;
 import ru.eludia.products.mosgis.db.model.tables.House;
+import ru.eludia.products.mosgis.db.model.tables.NonResidentialPremise;
+import ru.eludia.products.mosgis.db.model.tables.ResidentialPremise;
 import ru.eludia.products.mosgis.db.model.voc.VocBuilding;
 import ru.eludia.products.mosgis.db.model.voc.VocUnom;
 import ru.eludia.products.mosgis.db.model.voc.VocUnomStatus;
+import ru.eludia.products.mosgis.db.model.ws.WsMessages;
 import ru.eludia.products.mosgis.jms.base.WsMDB;
+import ru.eludia.products.mosgis.util.StringUtils;
 import ru.eludia.products.mosgis.ws.soap.impl.base.Errors;
 import ru.eludia.products.mosgis.ws.soap.impl.base.Fault;
 import ru.eludia.products.mosgis.ws.soap.tools.SOAPTools;
 import ru.gosuslugi.dom.schema.integration.base.BaseAsyncResponseType;
+import ru.gosuslugi.dom.schema.integration.base.OKTMORefType;
+import ru.gosuslugi.dom.schema.integration.house_management.GKNEGRPKeyRSOType;
 import ru.gosuslugi.dom.schema.integration.house_management.GetStateResult;
 import ru.gosuslugi.dom.schema.integration.house_management.HouseBasicRSOType;
 import ru.gosuslugi.dom.schema.integration.house_management.HouseBasicUpdateRSOType;
@@ -29,11 +40,16 @@ import ru.gosuslugi.dom.schema.integration.house_management.ImportHouseRSOReques
 import ru.gosuslugi.dom.schema.integration.house_management.ImportHouseRSORequest.ApartmentHouse;
 import ru.gosuslugi.dom.schema.integration.house_management.ImportHouseRSORequest.ApartmentHouse.ApartmentHouseToCreate;
 import ru.gosuslugi.dom.schema.integration.house_management.ImportHouseRSORequest.ApartmentHouse.ApartmentHouseToUpdate;
+import ru.gosuslugi.dom.schema.integration.house_management.ImportHouseRSORequest.ApartmentHouse.EntranceToCreate;
+import ru.gosuslugi.dom.schema.integration.house_management.ImportHouseRSORequest.ApartmentHouse.EntranceToUpdate;
+import ru.gosuslugi.dom.schema.integration.house_management.ImportHouseRSORequest.ApartmentHouse.NonResidentialPremiseToCreate;
+import ru.gosuslugi.dom.schema.integration.house_management.ImportHouseRSORequest.ApartmentHouse.NonResidentialPremiseToUpdate;
 import ru.gosuslugi.dom.schema.integration.house_management.ImportHouseRSORequest.LivingHouse;
 import ru.gosuslugi.dom.schema.integration.house_management.ImportHouseRSORequest.LivingHouse.LivingHouseToCreate;
 import ru.gosuslugi.dom.schema.integration.house_management.ImportHouseRSORequest.LivingHouse.LivingHouseToUpdate;
 import ru.gosuslugi.dom.schema.integration.house_management.ImportResult;
 import ru.gosuslugi.dom.schema.integration.house_management.ImportResult.CommonResult;
+import ru.gosuslugi.dom.schema.integration.nsi_base.NsiRef;
 
 @MessageDriven(activationConfig = {
     @ActivationConfigProperty(propertyName = "destinationLookup", propertyValue = "mosgis.inSoapImportHouseRSOData")
@@ -62,6 +78,11 @@ public class ImportHouseRSOData extends WsMDB {
     protected JAXBContext getJAXBContext() throws JAXBException {
         return jc;
     }
+    
+    @Override
+	protected Class<GetStateResult> getGetStateResultClass() {
+		return GetStateResult.class;
+	}
 
     @Override
     protected BaseAsyncResponseType generateResponse (DB db, Map<String, Object> r, Object request) throws Exception {
@@ -86,53 +107,50 @@ public class ImportHouseRSOData extends WsMDB {
     	CommonResult commonResult = new CommonResult();
     	importResult.getCommonResult().add(commonResult);
     	try {
-	    	if (house.getApartmentHouseToCreate() != null) {
+	    	UUID houseId;
+    		if (house.getApartmentHouseToCreate() != null) {
 	    		isCreate = true;
 	    		ApartmentHouseToCreate houseToCreate = house.getApartmentHouseToCreate();
-	    		commonResult.setTransportGUID(houseToCreate.getTransportGUID());
-	    		
 	    		HouseBasicRSOType basicCharacteristic = houseToCreate.getBasicCharacteristicts();
 	        	
-	        	Map<String, Object> vocBuilding = getBuilding(db, basicCharacteristic.getFIASHouseGuid());
-	        	
-	        	Map<String, Object> houseDb = db.getMap(db.getModel().select(House.class, "*")
-	        			.where(House.c.FIASHOUSEGUID, vocBuilding.get("houseguid")));
-	        	if (houseDb != null && !houseDb.isEmpty())
-	        		throw new Fault("Дом уже размещен", Errors.INT004132);
-	        	
-	        	UUID houseId = (UUID) db.insertId(House.class, HASH(
-	        			House.c.FIASHOUSEGUID, vocBuilding.get("houseguid"),
-	        			House.c.ADDRESS,       vocBuilding.get("label"),
-	        			House.c.KAD_N.lc(),    basicCharacteristic.getCadastralNumber(),
-	        			House.c.IS_CONDO,      true
-	        			));
-	        	
-	        	commonResult.setGUID(houseId.toString());
-	        	commonResult.setUpdateDate(SOAPTools.xmlNow());
-	        } else if (house.getApartmentHouseToUpdate() != null) {
+	    		houseId = saveHouseData(db, commonResult, houseToCreate.getTransportGUID(),
+						basicCharacteristic.getFIASHouseGuid(), basicCharacteristic.getOKTMO(),
+						basicCharacteristic.getOlsonTZ(), basicCharacteristic, false, isCreate);
+	        } else {
 	        	ApartmentHouseToUpdate houseToUpdate = house.getApartmentHouseToUpdate();
-	        	commonResult.setTransportGUID(houseToUpdate.getTransportGUID());
-	        	
 	        	HouseBasicUpdateRSOType basicCharacteristic = houseToUpdate.getBasicCharacteristicts();
 	        	
-	        	Map<String, Object> vocBuilding = getBuilding(db, basicCharacteristic.getFIASHouseGuid());
-	        	
-	        	Map<String, Object> houseDb = db.getMap(db.getModel().select(House.class, "*")
-	        			.where(House.c.FIASHOUSEGUID, vocBuilding.get("houseguid")));
-	        	if (houseDb == null || houseDb.isEmpty())
-	        		throw new Fault("Дом еще не размещен");
-	        	
-	        	houseDb.put(House.c.KAD_N.lc(), basicCharacteristic.getCadastralNumber());
-	        	
-				commonResult.setGUID(((UUID) houseDb.get("uuid")).toString());
-	        	commonResult.setUpdateDate(SOAPTools.xmlNow());
+	        	houseId = saveHouseData(db, commonResult, houseToUpdate.getTransportGUID(),
+						basicCharacteristic.getFIASHouseGuid(), basicCharacteristic.getOKTMO(),
+						basicCharacteristic.getOlsonTZ(), basicCharacteristic, false, isCreate);
 	        }
 	        
+			for (EntranceToCreate entranceToCreate : house.getEntranceToCreate()) {
+				CommonResult entranceCommonResult = saveEntrance(db, TypeConverter.Map(entranceToCreate), houseId,
+						isCreate, r.get(WsMessages.c.UUID_ORG.lc()));
+				importResult.getCommonResult().add(entranceCommonResult);
+			}
 	        
-	        house.getEntranceToCreate();
-	        house.getEntranceToUpdate();
-	        house.getNonResidentialPremiseToCreate();
-	        house.getNonResidentialPremiseToUpdate();
+			for (EntranceToUpdate entranceToUpdate : house.getEntranceToUpdate()) {
+				CommonResult entranceCommonResult = saveEntrance(db, TypeConverter.Map(entranceToUpdate), houseId,
+						isCreate, r.get(WsMessages.c.UUID_ORG.lc()));
+				importResult.getCommonResult().add(entranceCommonResult);
+			}
+	        
+			for (NonResidentialPremiseToCreate nonResidentialPremiseToCreate : house.getNonResidentialPremiseToCreate()) {
+				CommonResult nonResidentialPremiseCommonResult = saveNonResidentialPremise(db,
+						TypeConverter.Map(nonResidentialPremiseToCreate), houseId, isCreate,
+						r.get(WsMessages.c.UUID_ORG.lc()));
+				importResult.getCommonResult().add(nonResidentialPremiseCommonResult);
+			}
+	        
+			for (NonResidentialPremiseToUpdate nonResidentialPremiseToUpdate : house.getNonResidentialPremiseToUpdate()) {
+				CommonResult nonResidentialPremiseCommonResult = saveNonResidentialPremise(db,
+						TypeConverter.Map(nonResidentialPremiseToUpdate), houseId, isCreate,
+						r.get(WsMessages.c.UUID_ORG.lc()));
+				importResult.getCommonResult().add(nonResidentialPremiseCommonResult);
+			}
+			
 	        house.getResidentialPremises();
     	} catch (Fault fault) {
     		commonResult.getError().add(fault.toCommonResultError());
@@ -149,48 +167,23 @@ public class ImportHouseRSOData extends WsMDB {
     	
     	CommonResult commonResult = new CommonResult();
     	try {
-	    	if (house.getLivingHouseToCreate() != null) {
+	    	UUID houseId;
+    		if (house.getLivingHouseToCreate() != null) {
 	    		isCreate = true;
 	    		LivingHouseToCreate houseToCreate = house.getLivingHouseToCreate();
-	    		commonResult.setTransportGUID(houseToCreate.getTransportGUID());
-	    		
 	    		HouseBasicRSOType basicCharacteristic = houseToCreate.getBasicCharacteristicts();
 	        	
-	        	Map<String, Object> vocBuilding = getBuilding(db, basicCharacteristic.getFIASHouseGuid());
-	        	
-	        	Map<String, Object> houseDb = db.getMap(db.getModel().select(House.class, "*")
-	        			.where(House.c.FIASHOUSEGUID, vocBuilding.get("houseguid")));
-	        	if (houseDb != null && !houseDb.isEmpty())
-	        		throw new Fault("Дом уже размещен", Errors.INT004132);
-	        	
-	        	UUID houseId = (UUID) db.insertId(House.class, HASH(
-	        			House.c.FIASHOUSEGUID, vocBuilding.get("houseguid"),
-	        			House.c.ADDRESS,       vocBuilding.get("label"),
-	        			House.c.KAD_N.lc(),    basicCharacteristic.getCadastralNumber(),
-	        			House.c.IS_CONDO,      true
-	        			));
-	        	
-	        	commonResult.setGUID(houseId.toString());
-	        	commonResult.setUpdateDate(SOAPTools.xmlNow());
-	        } else if (house.getLivingHouseToUpdate() != null) {
+	    		houseId = saveHouseData(db, commonResult, houseToCreate.getTransportGUID(),
+						basicCharacteristic.getFIASHouseGuid(), basicCharacteristic.getOKTMO(),
+						basicCharacteristic.getOlsonTZ(), basicCharacteristic, false, isCreate);
+	        } else {
 	        	LivingHouseToUpdate houseToUpdate = house.getLivingHouseToUpdate();
-	        	commonResult.setTransportGUID(houseToUpdate.getTransportGUID());
-	        	
 	        	HouseBasicUpdateRSOType basicCharacteristic = houseToUpdate.getBasicCharacteristicts();
-	        	
-	        	Map<String, Object> vocBuilding = getBuilding(db, basicCharacteristic.getFIASHouseGuid());
-	        	
-	        	Map<String, Object> houseDb = db.getMap(db.getModel().select(House.class, "*")
-	        			.where(House.c.FIASHOUSEGUID, vocBuilding.get("houseguid")));
-	        	if (houseDb == null || houseDb.isEmpty())
-	        		throw new Fault("Дом еще не размещен");
-	        	
-	        	houseDb.put(House.c.KAD_N.lc(), basicCharacteristic.getCadastralNumber());
-	        	
-				commonResult.setGUID(((UUID) houseDb.get("uuid")).toString());
-	        	commonResult.setUpdateDate(SOAPTools.xmlNow());
+				
+	        	houseId = saveHouseData(db, commonResult, houseToUpdate.getTransportGUID(),
+						basicCharacteristic.getFIASHouseGuid(), basicCharacteristic.getOKTMO(),
+						basicCharacteristic.getOlsonTZ(), basicCharacteristic, false, isCreate);
 	        }
-	        
 	        
 	        house.getBlocks();
 	        house.getLivingHouseToCreate();
@@ -202,6 +195,44 @@ public class ImportHouseRSOData extends WsMDB {
     		commonResult.getError().add(fault.toCommonResultError());
     	}
     	return importResult;
+    }
+    
+	private UUID saveHouseData(DB db, CommonResult commonResult, String transportGuid, String fiasHouseGuid,
+			OKTMORefType oktmo, NsiRef olsonTZ, GKNEGRPKeyRSOType egrpKey, boolean isCondo, boolean isCreate)
+			throws Fault, SQLException {
+    	commonResult.setTransportGUID(transportGuid);
+    	
+    	Map<String, Object> vocBuilding = getBuilding(db, fiasHouseGuid);
+    	
+    	Map<String, Object> houseDb = db.getMap(db.getModel().select(House.class, "*")
+    			.where(House.c.FIASHOUSEGUID, vocBuilding.get("houseguid")));
+    	UUID houseId;
+    	if (isCreate) {
+    		if (houseDb != null && !houseDb.isEmpty())
+        		throw new Fault("Дом уже размещен", Errors.INT004132);
+        	
+        	houseId = (UUID) db.insertId(House.class, HASH(
+        			House.c.FIASHOUSEGUID, vocBuilding.get("houseguid"),
+        			House.c.ADDRESS,       vocBuilding.get("label"),
+        			House.c.KAD_N.lc(),    egrpKey.getCadastralNumber(),
+        			House.c.IS_CONDO,      isCondo
+        			));
+    	} else {
+	    	if (houseDb == null || houseDb.isEmpty())
+	    		throw new Fault("Дом еще не размещен");
+	    	
+	    	houseId = (UUID) houseDb.get("uuid");
+	    	
+			db.update(House.class, HASH(
+					EnTable.c.UUID, houseId, 
+					House.c.KAD_N,  egrpKey.getCadastralNumber()
+					));
+    	}
+    	
+		commonResult.setGUID(houseId.toString());
+    	commonResult.setUpdateDate(SOAPTools.xmlNow());
+    	
+    	return houseId;
     }
     
     private Map<String, Object> getBuilding(DB db, String fiasHouseGuid) throws SQLException, Fault {
@@ -234,9 +265,167 @@ public class ImportHouseRSOData extends WsMDB {
     	return vocBuilding;
     }
     
-    @Override
-	protected Class<GetStateResult> getGetStateResultClass() {
-		return GetStateResult.class;
-	}
-
+	private CommonResult saveEntrance(DB db,  Map<String, Object> entranceData,
+			UUID houseId, boolean isHouseCreate, Object uuidOrg) {
+    	CommonResult commonResult = new CommonResult();
+    	commonResult.setTransportGUID(entranceData.get("transportguid").toString());
+    	
+    	String entranceGuid = entranceData.get("premisesguid").toString();
+    	try {
+	    	if (StringUtils.isNotBlank(entranceGuid) && isHouseCreate)
+	    		throw new Fault(Errors.INT004026);
+	    	
+	    	UUID entranceId;
+	    	
+	    	if (StringUtils.isNotBlank(entranceGuid)) {
+	    		Select select = db.getModel().select(Entrance.class, "*")
+	    				.where("uuid_house", houseId)
+	    				.and("is_deleted", 0);
+	    		select.andEither ("uuid", entranceGuid).or("entranceguid", entranceGuid);
+	    		
+	    		Map<String, Object> entrance = db.getMap(select);
+	    		if (entrance.isEmpty())
+	    			throw new Fault(Errors.INT002034);
+	    		
+	    		entranceId = (UUID)entrance.get("uuid");
+	    		
+	    		entranceData.remove("entranceguid");
+	    		entranceData.put("uuid", entranceId);
+	    		if (entranceData.containsKey("annulmentreason")) {
+	    			entranceData.put("code_vc_nsi_330",((NsiRef)entranceData.get("annulmentreason")).getCode());
+	    			entranceData.remove("annulmentreason");
+	    		}
+	    		
+	    		db.update(Entrance.class, entranceData);
+	    		
+	    	} else {
+	    		entranceData.putAll(HASH(
+	    				"uuid_org",    uuidOrg,
+	    				"uuid_house",  houseId
+	    				));
+	    		entranceId = (UUID) db.insertId(Entrance.class, entranceData);
+	    	}
+	    	
+	    	commonResult.setGUID(entranceId.toString());
+	    	commonResult.setUpdateDate(SOAPTools.xmlNow());
+    	
+    	} catch (Fault fault) {
+    		commonResult.getError().add(fault.toCommonResultError());
+    	} catch (Exception e) {
+    		Fault fault = new Fault(e);
+    		commonResult.getError().add(fault.toCommonResultError());
+    	} 
+    	return commonResult;
+    }
+	
+	private CommonResult saveNonResidentialPremise(DB db, Map<String, Object> premiseData, UUID houseId, boolean isHouseCreate, Object uuidOrg) {
+    	CommonResult commonResult = new CommonResult();
+    	commonResult.setTransportGUID(premiseData.get("transportguid").toString());
+    	String premiseGuid = premiseData.get("premisesguid").toString();
+    	try {
+	    	if (StringUtils.isNotBlank(premiseGuid) && isHouseCreate)
+	    		throw new Fault(Errors.INT004025);
+	    	
+	    	UUID premiseId;
+	    	
+	    	if (StringUtils.isNotBlank(premiseGuid)) {
+	    		Select select = db.getModel().select(NonResidentialPremise.class, "*")
+	    				.where("uuid_house", houseId)
+	    				.and("is_deleted", 0);
+	    		select.andEither ("uuid", premiseGuid).or("premisesguid", premiseGuid);
+	    		
+	    		Map<String, Object> premise = db.getMap(select);
+	    		if (premise.isEmpty())
+	    			throw new Fault(Errors.INT002034);
+	    		
+	    		premiseId = (UUID)premise.get("uuid");
+	    		
+	    		premiseData.remove("premisesguid");
+	    		premiseData.put("uuid", premiseId);
+	    		if (premiseData.containsKey("annulmentreason")) {
+	    			premiseData.put("code_vc_nsi_330",((NsiRef)premiseData.get("annulmentreason")).getCode());
+	    			premiseData.remove("annulmentreason");
+	    		}
+	    		
+	    		db.update(NonResidentialPremise.class, premiseData);
+	    		
+	    	} else {
+	    		premiseData.putAll(HASH(
+	    				"uuid_org",    uuidOrg,
+	    				"uuid_house",  houseId
+	    				));
+	    		premiseId = (UUID) db.insertId(NonResidentialPremise.class, premiseData);
+	    	}
+	    	
+	    	commonResult.setGUID(premiseId.toString());
+	    	commonResult.setUpdateDate(SOAPTools.xmlNow());
+    	
+    	} catch (Fault fault) {
+    		commonResult.getError().add(fault.toCommonResultError());
+    	} catch (Exception e) {
+    		Fault fault = new Fault(e);
+    		commonResult.getError().add(fault.toCommonResultError());
+    	} 
+    	return commonResult;
+    }
+	
+	private CommonResult saveResidentialPremise(DB db, Map<String, Object> premiseData, UUID houseId, boolean isHouseCreate, Object uuidOrg) {
+    	CommonResult commonResult = new CommonResult();
+    	commonResult.setTransportGUID(premiseData.get("transportguid").toString());
+    	String premiseGuid = premiseData.get("premisesguid").toString();
+    	try {
+	    	if (StringUtils.isNotBlank(premiseGuid) && isHouseCreate)
+	    		throw new Fault(Errors.INT004027);
+	    	
+	    	UUID premiseId;
+	    	
+	    	if (StringUtils.isNotBlank(premiseGuid)) {
+	    		Select select = db.getModel().select(ResidentialPremise.class, "*")
+	    				.where("uuid_house", houseId)
+	    				.and("is_deleted", 0);
+	    		select.andEither ("uuid", premiseGuid).or("premisesguid", premiseGuid);
+	    		
+	    		Map<String, Object> premise = db.getMap(select);
+	    		if (premise.isEmpty())
+	    			throw new Fault(Errors.INT002034);
+	    		
+	    		premiseId = (UUID)premise.get("uuid");
+	    		
+	    		premiseData.remove("premisesguid");
+	    		premiseData.put("uuid", premiseId);
+	    		if (premiseData.containsKey("annulmentreason")) {
+	    			premiseData.put("code_vc_nsi_330",((NsiRef)premiseData.get("annulmentreason")).getCode());
+	    			premiseData.remove("annulmentreason");
+	    		}
+	    		if (premiseData.containsKey("premisesсharacteristic")) {
+	    			premiseData.put("code_vc_nsi_30",((NsiRef)premiseData.get("premisesсharacteristic")).getCode());
+	    		}
+	    		if ((Boolean) premiseData.containsKey("hasnoentrance")) {
+	    			premiseData.put("uuid_entrance", null);
+	    		} else {
+	    			
+	    		}
+	    		
+	    		
+	    		db.update(NonResidentialPremise.class, premiseData);
+	    		
+	    	} else {
+	    		premiseData.putAll(HASH(
+	    				"uuid_org",    uuidOrg,
+	    				"uuid_house",  houseId
+	    				));
+	    		premiseId = (UUID) db.insertId(NonResidentialPremise.class, premiseData);
+	    	}
+	    	
+	    	commonResult.setGUID(premiseId.toString());
+	    	commonResult.setUpdateDate(SOAPTools.xmlNow());
+    	
+    	} catch (Fault fault) {
+    		commonResult.getError().add(fault.toCommonResultError());
+    	} catch (Exception e) {
+    		Fault fault = new Fault(e);
+    		commonResult.getError().add(fault.toCommonResultError());
+    	} 
+    	return commonResult;
+    }
 }
