411d9ba5b985c6904302277ae46cdb8b20cb440b
Dmitry Ovsyanko <do_@pochta.ru>
2019-04-29 16:30:26 +0300

    houses bulk import IN PROGRESS tasks/85d9a971-881b-4c4b-a406-cc775831613d

--- mosgis-ejb/src/main/java/ru/eludia/products/mosgis/db/model/voc/VocBuildingUnknown.java
+++ mosgis-ejb/src/main/java/ru/eludia/products/mosgis/db/model/voc/VocBuildingUnknown.java
@@ -29,7 +29,7 @@ public class VocBuildingUnknown extends View {
     @Override
     public String getSQL () {
         return 
-            "SELECT houseguid fiashouseguid FROM " + VocBuilding.TABLE_NAME
+            "SELECT houseguid fiashouseguid FROM " + VocBuilding.TABLE_NAME + " WHERE houseguid=upper('099EDD3AAA2044BC89FFF8FD26B1D402') "
                 + " MINUS " +
             "SELECT fiashouseguid FROM " + House.TABLE_NAME
                 + " MINUS " +
--- mosgis-ejb/src/main/java/ru/eludia/products/mosgis/jms/gis/poll/GisPollImportHouseByFiasHouseGuidMDB.java
+++ mosgis-ejb/src/main/java/ru/eludia/products/mosgis/jms/gis/poll/GisPollImportHouseByFiasHouseGuidMDB.java
@@ -3,15 +3,19 @@ package ru.eludia.products.mosgis.jms.gis.poll;
 import java.sql.SQLException;
 import java.util.Map;
 import java.util.UUID;
+import javax.annotation.Resource;
 import javax.ejb.ActivationConfigProperty;
 import javax.ejb.EJB;
 import javax.ejb.MessageDriven;
+import javax.jms.Queue;
 import ru.eludia.base.DB;
 import static ru.eludia.base.DB.HASH;
 import ru.eludia.base.db.sql.gen.Get;
 import ru.eludia.products.mosgis.db.model.tables.OutSoap;
 import static ru.eludia.products.mosgis.db.model.voc.VocAsyncRequestState.i.DONE;
 import ru.eludia.products.mosgis.db.ModelHolder;
+import ru.eludia.products.mosgis.db.model.tables.House;
+import ru.eludia.products.mosgis.db.model.tables.HouseLog;
 import ru.eludia.products.mosgis.jms.gis.poll.base.GisPollException;
 import ru.eludia.products.mosgis.jms.gis.poll.base.GisPollMDB;
 import ru.eludia.products.mosgis.jms.gis.poll.base.GisPollRetryException;
@@ -33,12 +37,21 @@ public class GisPollImportHouseByFiasHouseGuidMDB  extends GisPollMDB {
     @EJB
     private WsGisHouseManagementClient wsGisHouseManagementClient;
 
+    @Resource (mappedName = "mosgis.outExportHouseQueue")
+    private Queue exportQueue;
+    
     @Override
     protected Get get (UUID uuid) {
         return (Get) ModelHolder.getModel ().get (getTable (), uuid, "AS root", "*")                
             .toOne (VocBuildingLog.class,     "AS log", "uuid", "action").on ("log.uuid_out_soap=root.uuid")
-            .toOne (VocBuilding.class,        "AS r", "uuid").on ()
+            .toOne (VocBuilding.class,        "AS r", "uuid", "label").on ()
         ;
+    }
+    
+    private static int isCondo (ExportHouseResultType exportHouseResult) {
+        if (exportHouseResult.getApartmentHouse () != null) return 1;
+        if (exportHouseResult.getLivingHouse    () != null) return 0;
+        throw new IllegalArgumentException ("Cannot determine is it a condo or not.");
     }
 
     @Override
@@ -58,19 +71,30 @@ public class GisPollImportHouseByFiasHouseGuidMDB  extends GisPollMDB {
             
             if (exportHouseResult == null) throw new GisPollException ("0", "Сервис ГИСЖКХ вернул пустой результат");
             
-            String houseUniqueNumber = exportHouseResult.getHouseUniqueNumber ();
-logger.info ("houseUniqueNumber=" + houseUniqueNumber);
+            String uuidHouse = db.upsertId (House.class, 
+                    
+                HASH (
+                    House.c.FIASHOUSEGUID, r.get ("r.uuid"),
+                    House.c.ADDRESS,       r.get ("r.label"),
+                    House.c.IS_CONDO,      isCondo (exportHouseResult)
+                ),
 
-            ExportHouseResultType.ApartmentHouse apartmentHouse = exportHouseResult.getApartmentHouse ();
-logger.info ("apartmentHouse=" + apartmentHouse);
-
-            ExportHouseResultType.LivingHouse livingHouse = exportHouseResult.getLivingHouse ();
-logger.info ("livingHouse=" + livingHouse);
+                House.c.FIASHOUSEGUID.lc ()
+                    
+            );
             
+            Map<String, Object> logRecord = db.getMap (db.getModel ().get (VocBuildingLog.class, uuid, "uuid", "uuid_out_soap", "uuid_message", "action"));
+            
+            logRecord.put ("uuid_object", uuidHouse);
+            
+            db.insert (HouseLog.class, logRecord);
+                                                
             db.update (OutSoap.class, HASH (
                 "uuid", getUuid (),
                 "id_status", DONE.getId ()
             ));
+            
+            uuidPublisher.publish (exportQueue, uuid);
 
         }
         catch (GisPollRetryException ex) {
