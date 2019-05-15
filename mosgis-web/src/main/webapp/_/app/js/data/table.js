9d5188a022c47034abbaebd63e7288b046fb4ffa
Dmitry Ovsyanko <do_@pochta.ru>
2019-05-15 14:00:01 +0300

    db2xl FIX tasks/5218afa4-a8d7-cc1d-508f-587212ba7d98

--- mosgis-web/src/main/webapp/_/app/js/data/table.js
+++ mosgis-web/src/main/webapp/_/app/js/data/table.js
@@ -6,7 +6,15 @@ define ([], function () {
     
         var it = data.item
         
-        var $t = $('table.ttt').clone ()
+        $('span', $('table.ttt')).each (function () {
+
+            var $this = $(this)
+
+            if ($this.css ('display') == 'none') $this.text ('')
+
+        });
+        
+        var $t = $('table.ttt').clone ()        
         
         $t.prepend ('<tr><th colspan=8 align=left>&nbsp;Таблица ' + it.recid + ': ' + it.label)
                 
